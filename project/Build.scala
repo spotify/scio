7c1277dddd1e9a93db7c5595b3790a07e86dc179
Neville Li <neville@spotify.com>
2015-05-07 22:40:54 -0400

    clean up SBT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import xerial.sbt.Sonatype.SonatypeKeys._
 
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Sonatype.sonatypeSettings ++ Seq(
-    organization       := "com.spotify.data",
+    organization       := "com.spotify",
     version            := "0.1.0-SNAPSHOT",
 
     scalaVersion       := "2.10.5",
@@ -18,19 +18,26 @@ object BuildSettings {
 object DataflowScalaBuild extends Build {
   import BuildSettings._
 
+  val sdkVersion = "0.4.150414"
+
+  val chillVersion = "0.5.2"
+  val macrosVersion = "2.0.1"
+  val scalaTestVersion = "2.2.1"
+
   lazy val root: Project = Project(
     "root",
     file("."),
     settings = buildSettings ++ Seq(
       run <<= run in Compile in dataflowScalaExamples)
   ).settings(
-    publish         := {},
-    publishLocal    := {}
+    publish := {},
+    publishLocal := {}
   ).aggregate(
     dataflowScalaCore,
-    dataflowScalaExamples,
+    dataflowScalaTest,
+    bigqueryScala,
     dataflowScalaSchemas,
-    dataflowScalaTest
+    dataflowScalaExamples
   )
 
   lazy val dataflowScalaCore: Project = Project(
@@ -38,14 +45,10 @@ object DataflowScalaBuild extends Build {
     file("core"),
     settings = buildSettings ++ Seq(
       libraryDependencies ++= Seq(
-        "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % "latest.integration",
+        "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % sdkVersion,
         "com.twitter" %% "algebird-core" % "0.9.0",
-        "com.twitter" %% "chill" % "0.5.2",
-        "com.twitter" %% "chill-avro" % "0.5.2",
-        "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-        // DataFlow testing requires junit and hamcrest
-        "junit" % "junit" % "4.12" % "test",
-        "org.hamcrest" % "hamcrest-all" % "1.3" % "test"
+        "com.twitter" %% "chill" % chillVersion,
+        "com.twitter" %% "chill-avro" % chillVersion
       )
     )
   ).dependsOn(
@@ -54,16 +57,28 @@ object DataflowScalaBuild extends Build {
     dataflowScalaTest % "test"
   )
 
+  lazy val dataflowScalaTest: Project = Project(
+    "dataflow-scala-test",
+    file("test"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % sdkVersion,
+        "org.scalatest" %% "scalatest" % scalaTestVersion,
+        // DataFlow testing requires junit and hamcrest
+        "junit" % "junit" % "4.12",
+        "org.hamcrest" % "hamcrest-all" % "1.3"
+      )
+    )
+  )
+
   lazy val bigqueryScala: Project = Project(
     "bigquery-scala",
     file("bigquery"),
     settings = buildSettings ++ Seq(
       libraryDependencies ++= Seq(
-        "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % "latest.integration",
-        "org.scalatest" %% "scalatest" % "2.2.1",
-        // DataFlow testing requires junit and hamcrest
-        "junit" % "junit" % "4.12",
-        "org.hamcrest" % "hamcrest-all" % "1.3"
+        "com.google.apis" % "google-api-services-bigquery" % "v2-rev187-1.19.1"
+          exclude("com.google.guava", "guava-jdk5"),
+        "org.scalatest" %% "scalatest" % scalaTestVersion
       ),
       libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
       libraryDependencies := {
@@ -74,8 +89,8 @@ object DataflowScalaBuild extends Build {
           // in Scala 2.10, quasiquotes are provided by macro paradise
           case Some((2, 10)) =>
             libraryDependencies.value ++ Seq(
-              compilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full),
-              "org.scalamacros" %% "quasiquotes" % "2.0.1" cross CrossVersion.binary)
+              compilerPlugin("org.scalamacros" % "paradise" % macrosVersion cross CrossVersion.full),
+              "org.scalamacros" %% "quasiquotes" % macrosVersion cross CrossVersion.binary)
         }
       }
     )
@@ -86,38 +101,16 @@ object DataflowScalaBuild extends Build {
     file("schemas"),
     settings = buildSettings ++ sbtavro.SbtAvro.avroSettings
   ).settings(
-    publish         := {},
-    publishLocal    := {}
-  )
-
-  lazy val dataflowScalaTest: Project = Project(
-    "dataflow-scala-test",
-    file("test"),
-    settings = buildSettings ++ Seq(
-      libraryDependencies ++= Seq(
-        "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % "latest.integration",
-        "org.scalatest" %% "scalatest" % "2.2.1",
-        // DataFlow testing requires junit and hamcrest
-        "junit" % "junit" % "4.12",
-        "org.hamcrest" % "hamcrest-all" % "1.3"
-      )
-    )
+    publish := {},
+    publishLocal := {}
   )
 
   lazy val dataflowScalaExamples: Project = Project(
     "dataflow-scala-examples",
-    file("examples"),
-    settings = buildSettings ++ Seq(
-      libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-        // DataFlow testing requires junit and hamcrest
-        "junit" % "junit" % "4.12" % "test",
-        "org.hamcrest" % "hamcrest-all" % "1.3" % "test"
-      )
-    )
+    file("examples")
   ).settings(
-    publish         := {},
-    publishLocal    := {}
+    publish := {},
+    publishLocal := {}
   ).dependsOn(
     dataflowScalaCore,
     dataflowScalaSchemas,
