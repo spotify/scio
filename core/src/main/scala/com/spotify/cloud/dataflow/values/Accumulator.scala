255f6891cc7a1d266fc47da20b8095ea0914511e
Neville Li <neville@spotify.com>
2015-05-25 22:33:19 -0400

    add ScalaDoc for accumulators

--- core/src/main/scala/com/spotify/cloud/dataflow/values/Accumulator.scala
+++ core/src/main/scala/com/spotify/cloud/dataflow/values/Accumulator.scala
@@ -8,6 +8,7 @@ import com.google.cloud.dataflow.sdk.transforms.{Aggregator, Combine, DoFn}
 
 import scala.collection.mutable.{Map => MMap}
 
+/** Type class for `T` that can be used in an [[Accumulator]]. */
 sealed trait AccumulatorType[T] {
   type CF = CombineFn[T, Array[T], T]
   type BCF = Combine.BinaryCombineFn[T]
@@ -16,8 +17,13 @@ sealed trait AccumulatorType[T] {
   protected def minFnImpl(): Combine.BinaryCombineFn[_]
   protected def maxFnImpl(): Combine.BinaryCombineFn[_]
 
+  /** CombineFn for computing sum of the underlying values. */
   def sumFn(): CF = sumFnImpl().asInstanceOf[CF]
+
+  /** BinaryCombineFn for computing maximum of the underlying values. */
   def minFn(): BCF = minFnImpl().asInstanceOf[BCF]
+
+  /** BinaryCombineFn for computing minimum of the underlying values. */
   def maxFn(): BCF = maxFnImpl().asInstanceOf[BCF]
 }
 
@@ -39,6 +45,10 @@ private[dataflow] class DoubleAccumulatorType extends AccumulatorType[Double] {
   override protected def maxFnImpl() = new MaxDoubleFn()
 }
 
+/**
+ * Accumulator of values that can be updated via `add`, `max` or `min` operation, similar to
+ * Hadoop counters. Each accumulation must have a unique name.
+ */
 class Accumulator[T, U] private[dataflow]() {
 
   private val m: MMap[String, Aggregator[_]] = MMap.empty
@@ -52,16 +62,19 @@ class Accumulator[T, U] private[dataflow]() {
   private def aggregator[V, VA](name: String, f: CombineFn[V, VA, V]): Aggregator[V] =
     m.getOrElseUpdate(name, c.createAggregator(name, f)).asInstanceOf[Aggregator[V]]
 
+  /** Add value to the named accumulation. */
   def add[V](name: String, value: V)(implicit at: AccumulatorType[V]): Accumulator[T, U] = {
     aggregator(name, at.sumFn()).addValue(value)
     this
   }
 
+  /** Update maximum value of the named accumulation. */
   def max[V](name: String, value: V)(implicit at: AccumulatorType[V]): Accumulator[T, U] = {
     aggregator(name, at.maxFn()).addValue(value)
     this
   }
 
+  /** Update minimum value of the named accumulation. */
   def min[V](name: String, value: V)(implicit at: AccumulatorType[V]): Accumulator[T, U] = {
     aggregator(name, at.minFn()).addValue(value)
     this
