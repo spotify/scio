38f63a43764536fb84154e823b21ddfdb6369804
Neville Li <neville@spotify.com>
2015-05-27 18:04:47 -0400

    add ScalaDoc for DataflowContext

--- core/src/main/scala/com/spotify/cloud/dataflow/DataflowContext.scala
+++ core/src/main/scala/com/spotify/cloud/dataflow/DataflowContext.scala
@@ -30,25 +30,32 @@ import org.joda.time.Instant
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
+/** Convenience object for creating [[DataflowContext]] and [[Args]]. */
 object ContextAndArgs {
+  /** Create [[DataflowContext]] and [[Args]] for command line arguments. */
   def apply(args: Array[String]): (DataflowContext, Args) = {
     val context = DataflowContext(args)
     (context, context.args)
   }
 }
 
+/** Companion object for [[DataflowContext]]. */
 object DataflowContext {
+  /** Create a new [[DataflowContext]] instance. */
   def apply(args: Array[String]): DataflowContext = new DataflowContext(args)
 }
 
+/**
+ * Main entry point for Dataflow functionality. A DataflowContext represents a Dataflow pipeline,
+ * and can be used to create SCollections and distributed caches on that cluster.
+ */
 class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplicits {
 
   private var _options: DataflowPipelineOptions = null
-  def options: DataflowPipelineOptions = _options
 
   private var _sqlId: Int = 0
 
-  val (args, pipeline) = {
+  private val (_args, _pipeline) = {
     val dfPatterns = classOf[DataflowPipelineOptions].getMethods.flatMap { m =>
       val n = m.getName
       if ((!n.startsWith("get") && !n.startsWith("is")) ||
@@ -76,9 +83,21 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
 
   private lazy val bigQueryClient: BigQueryClient = BigQueryClient(this.options.getGcpCredential)
 
+  /** Dataflow pipeline specific options, e.g. project, zone, runner, stagingLocation. */
+  def options: DataflowPipelineOptions = _options
+
+  /** Application specific arguments. */
+  def args: Args = _args
+
+  /** Dataflow pipeline. */
+  def pipeline: Pipeline = _pipeline
+
+  /** Close the context. */
   def close(): Unit = pipeline.run()
 
-  /* Test wiring */
+  // =======================================================================
+  // Test wiring
+  // =======================================================================
 
   private implicit def context: DataflowContext = this
 
@@ -96,6 +115,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
   private def applyInternal[Output <: POutput](root: PTransform[_ >: PBegin, Output]): Output =
     pipeline.apply(root.withName(CallSites.getCurrent))
 
+  /** Get an SCollection of specific record type for an Avro file. */
   def avroFile[T <: IndexedRecord: ClassTag](path: String): SCollection[T] =
     if (this.isTest) {
       this.getTestInput(AvroIO[T](path))
@@ -105,6 +125,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(o).setName(path)
     }
 
+  /** Get an SCollection of generic record type for an Avro file. */
   def avroFile(path: String, schema: Schema): SCollection[GenericRecord] =
     if (this.isTest) {
       this.getTestInput(AvroIO[GenericRecord](path))
@@ -112,6 +133,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GAvroIO.Read.from(path).withSchema(schema))).setName(path)
     }
 
+  /** Get an SCollection of generic record type for an Avro file. */
   def avroFile(path: String, schemaString: String): SCollection[GenericRecord] =
     if (this.isTest) {
       this.getTestInput(AvroIO[GenericRecord](path))
@@ -119,6 +141,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GAvroIO.Read.from(path).withSchema(schemaString))).setName(path)
     }
 
+  /** Get an SCollection for a BigQuery SELECT query. */
   def bigQuerySelect(sqlQuery: String, stagingDataset: String): SCollection[TableRow] =
     if (this.isTest) {
       this.getTestInput(BigQueryIO(sqlQuery))
@@ -135,6 +158,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       bigQueryTable(table).setName(sqlQuery)
     }
 
+  /** Get an SCollection for a BigQuery table. */
   def bigQueryTable(table: TableReference): SCollection[TableRow] = {
     val tableSpec: String = GBigQueryIO.toTableSpec(table)
     if (this.isTest) {
@@ -144,9 +168,11 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
     }
   }
 
+  /** Get an SCollection for a BigQuery table. */
   def bigQueryTable(tableSpec: String): SCollection[TableRow] =
     this.bigQueryTable(GBigQueryIO.parseTableSpec(tableSpec))
 
+  /** Get an SCollection for a Datastore query. */
   def datastore(datasetId: String, query: Query): SCollection[Entity] =
     if (this.isTest) {
       this.getTestInput(DatastoreIO(datasetId, query))
@@ -154,6 +180,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GDatastoreIO.readFrom(datasetId, query)))
     }
 
+  /** Get an SCollection for a Pub/Sub subscription. */
   def pubsubSubscription(subscription: String, dropLateData: Boolean = true): SCollection[String] =
     if (this.isTest) {
       this.getTestInput(PubsubIO(subscription))
@@ -162,6 +189,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
         .setName(subscription)
     }
 
+  /** Get an SCollection for a Pub/Sub topic. */
   def pubsubTopic(topic: String, dropLateData: Boolean = true): SCollection[String] =
     if (this.isTest) {
       this.getTestInput(PubsubIO(topic))
@@ -169,6 +197,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GPubsubIO.Read.topic(topic).dropLateData(dropLateData))).setName(topic)
     }
 
+  /** Get an SCollection of TableRow for a JSON file. */
   def tableRowJsonFile(path: String): SCollection[TableRow] =
     if (this.isTest) {
       this.getTestInput(TableRowJsonIO(path))
@@ -176,6 +205,7 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GTextIO.Read.from(path).withCoder(TableRowJsonCoder.of()))).setName(path)
     }
 
+  /** Get an SCollection for a text file. */
   def textFile[T](path: String): SCollection[String] =
     if (this.isTest) {
       this.getTestInput(TextIO(path))
@@ -183,36 +213,44 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       SCollection(this.applyInternal(GTextIO.Read.from(path))).setName(path)
     }
 
-  /* In-memory collections */
+  // =======================================================================
+  // In-memory collections
+  // =======================================================================
 
+  /** Distribute local values to form an SCollection. */
   def parallelize[T: ClassTag](elems: T*): SCollection[T] = {
     val coder = pipeline.getCoderRegistry.getScalaCoder[T]
     SCollection(this.applyInternal(Create.of(elems: _*)).setCoder(coder)).setName(elems.toString())
   }
 
+  /** Distribute a local Scala Iterable to form an SCollection. */
   def parallelize[T: ClassTag](elems: Iterable[T]): SCollection[T] = {
     val coder = pipeline.getCoderRegistry.getScalaCoder[T]
     SCollection(this.applyInternal(Create.of(elems.asJava)).setCoder(coder)).setName(elems.toString())
   }
 
+  /** Distribute a local Scala Map to form an SCollection. */
   def parallelize[K: ClassTag, V: ClassTag](elems: Map[K, V]): SCollection[(K, V)] = {
     val coder = pipeline.getCoderRegistry.getScalaKvCoder[K, V]
     SCollection(this.applyInternal(Create.of(elems.asJava)).setCoder(coder)).map(kv => (kv.getKey, kv.getValue))
       .setName(elems.toString())
   }
 
+  /** Distribute local values with timestamps to form an SCollection. */
   def parallelizeTimestamped[T: ClassTag](elems: (T, Instant)*): SCollection[T] = {
     val coder = pipeline.getCoderRegistry.getScalaCoder[T]
     val v = elems.map(t => TimestampedValue.of(t._1, t._2))
     SCollection(this.applyInternal(Create.timestamped(v: _*)).setCoder(coder)).setName(elems.toString())
   }
 
+  /** Distribute a local Scala Iterable with timestamps to form an SCollection. */
   def parallelizeTimestamped[T: ClassTag](elems: Iterable[(T, Instant)]): SCollection[T] = {
     val coder = pipeline.getCoderRegistry.getScalaCoder[T]
     val v = elems.map(t => TimestampedValue.of(t._1, t._2))
     SCollection(this.applyInternal(Create.timestamped(v.asJava)).setCoder(coder)).setName(elems.toString())
   }
 
+  /** Distribute a local Scala Iterable with timestamps to form an SCollection. */
   def parallelizeTimestamped[T: ClassTag](elems: Iterable[T],
                                           timestamps: Iterable[Instant]): SCollection[T] = {
     val coder = pipeline.getCoderRegistry.getScalaCoder[T]
@@ -220,8 +258,29 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
     SCollection(this.applyInternal(Create.timestamped(v.asJava)).setCoder(coder)).setName(elems.toString())
   }
 
-  /* Distributed cache */
+  // =======================================================================
+  // Distributed cache
+  // =======================================================================
 
+  /**
+   * Create a new [[DistCache]] instance.
+   * @param uri Google Cloud Storage URI of the file to be distributed to all workers
+   * @param initFn function to initialized the distributed file
+   *
+   * {{{
+   * // Prepare distributed cache as Map[Int, String]
+   * val dc = context.distCache("gs://dataflow-samples/samples/misc/months.txt") { f =>
+   *   scala.io.Source.fromFile(f).getLines().map { s =>
+   *     val t = s.split(" ")
+   *     (t(0).toInt, t(1))
+   *   }.toMap
+   * }
+   *
+   * val p: SCollection[Int] = // ...
+   * // Extract distributed cache inside a transform
+   * p.map(x => dc.get().getOrElse(x, "unknown"))
+   * }}}
+   */
   def distCache[F](uri: String)(initFn: File => F): DistCache[F] =
     if (this.isTest) {
       new MockDistCache(testDistCache(DistCacheIO(uri)))
@@ -229,6 +288,11 @@ class DataflowContext private (cmdlineArgs: Array[String]) extends PrivateImplic
       new DistCacheSingle(new URI(uri), initFn, options)
     }
 
+  /**
+   * Create a new [[DistCache]] instance.
+   * @param uris Google Cloud Storage URIs of the files to be distributed to all workers
+   * @param initFn function to initialized the distributed files
+   */
   def distCache[F](uris: Seq[String])(initFn: Seq[File] => F): DistCache[F] =
     if (this.isTest) {
       new MockDistCache(testDistCache(DistCacheIO(uris.mkString("\t"))))
