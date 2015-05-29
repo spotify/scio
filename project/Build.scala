002180eecf4cb72f90c845a507ffa1d379210286
Neville Li <neville@spotify.com>
2015-05-28 23:00:22 -0400

    drop scala 2.10 build

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object BuildSettings {
     version            := "0.1.0-SNAPSHOT",
 
     scalaVersion       := "2.11.6",
-    crossScalaVersions := Seq("2.10.5", "2.11.6"),
+    crossScalaVersions := Seq("2.11.6"),
     scalacOptions      ++= Seq("-target:jvm-1.7", "-deprecation", "-feature", "-unchecked"),
     javacOptions in (Compile)          ++= Seq("-source", "1.7"),
     javacOptions in (Compile, compile) ++= Seq("-target", "1.7"),
