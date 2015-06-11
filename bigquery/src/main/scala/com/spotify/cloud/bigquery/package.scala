6643af9de76fc4590ba7c2474b33266dfe352953
Neville Li <neville@spotify.com>
2015-06-11 06:27:57 +0200

    improve doc for package objects

--- bigquery/src/main/scala/com/spotify/cloud/bigquery/package.scala
+++ bigquery/src/main/scala/com/spotify/cloud/bigquery/package.scala
@@ -2,14 +2,30 @@ package com.spotify.cloud
 
 import com.google.api.services.bigquery.model.{TableRow => GTableRow}
 
+/**
+ * Main package for BigQuery APIs. Import all.
+ *
+ * {{{
+ * import com.spotify.cloud.bigquery._
+ * }}}
+ */
 package object bigquery {
 
+  /**
+   * Create a TableRow with Map-like syntax. For example:
+   *
+   * {{{
+   * val r = TableRow("name" -> "Alice", "score" -> 100)
+   * }}}
+   */
   object TableRow {
     def apply(fields: (String, _)*): TableRow = fields.foldLeft(new GTableRow())((r, kv) => r.set(kv._1, kv._2))
   }
 
+  /** Alias for BigQuery TableRow. */
   type TableRow = GTableRow
 
+  /** Enhanced version of TableRow with typed getters. */
   implicit class RichTableRow(val r: TableRow) extends AnyVal {
 
     def getBoolean(name: AnyRef): Boolean = r.get(name).asInstanceOf[Boolean]
--- core/src/main/scala/com/spotify/cloud/dataflow/package.scala
+++ core/src/main/scala/com/spotify/cloud/dataflow/package.scala
@@ -2,15 +2,31 @@ package com.spotify.cloud
 
 import com.google.cloud.dataflow.sdk.io.BigQueryIO.Write
 
+/**
+ * Main package for public APIs. Import all.
+ *
+ * {{{
+ * import com.spotify.cloud.dataflow._
+ * }}}
+ */
 package object dataflow extends Implicits {
 
+  /** Alias for [[com.spotify.cloud.dataflow.values.WindowedValue]]. */
   type WindowedValue[T] = values.WindowedValue[T]
 
-  // BigQuery
+  /** Alias for BigQuery CreateDisposition. */
   val CREATE_IF_NEEDED = Write.CreateDisposition.CREATE_IF_NEEDED
+
+  /** Alias for BigQuery CreateDisposition. */
   val CREATE_NEVER = Write.CreateDisposition.CREATE_NEVER
+
+  /** Alias for BigQuery WriteDisposition. */
   val WRITE_APPEND = Write.WriteDisposition.WRITE_APPEND
+
+  /** Alias for BigQuery WriteDisposition. */
   val WRITE_EMPTY = Write.WriteDisposition.WRITE_EMPTY
+
+  /** Alias for BigQuery WriteDisposition. */
   val WRITE_TRUNCATE = Write.WriteDisposition.WRITE_TRUNCATE
 
 }
