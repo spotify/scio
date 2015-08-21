3551475e1443f061c052c12d21733a1fe1965115
Neville Li <neville@spotify.com>
2015-08-21 14:04:40 -0700

    clean up DoubleSCollectionFunctionsTest

--- core/src/test/scala/com/spotify/scio/values/DoubleSCollectionFunctionsTest.scala
+++ core/src/test/scala/com/spotify/scio/values/DoubleSCollectionFunctionsTest.scala
@@ -9,68 +9,43 @@ class DoubleSCollectionFunctionsTest extends PipelineTest {
   val longs = (1 to 100).map(_.toLong)
   val floats = (1 to 100).map(_.toFloat)
   val doubles = (1 to 100).map(_.toDouble)
-
   val expected = StatCounter((1 to 100).map(_.toDouble): _*)
 
-  def checkError(p: SCollection[Double], e: Double): Unit = {
-    p.count().internal should containSingleValue (1L)
-    p.map(x => math.abs(x - e) < 1e-10).internal should containSingleValue (true)
+  def test(s: Seq[Double], e: Double): Unit = {
+    s.size should equal (1L)
+    s.head should equal (e +- 1e-10)
   }
 
   "DoubleSCollection" should "support sampleStdev()" in {
-    runWithContext { sc =>
-      val e = expected.sampleStdev
-      val p1 = sc.parallelize(ints: _*).sampleStdev()
-      val p2 = sc.parallelize(longs: _*).sampleStdev()
-      val p3 = sc.parallelize(floats: _*).sampleStdev()
-      val p4 = sc.parallelize(doubles: _*).sampleStdev()
-      checkError(p1, e)
-      checkError(p2, e)
-      checkError(p3, e)
-      checkError(p4, e)
-    }
+    val e = expected.sampleStdev
+    test(runWithData(ints: _*)(_.sampleStdev()), e)
+    test(runWithData(longs: _*)(_.sampleStdev()), e)
+    test(runWithData(floats: _*)(_.sampleStdev()), e)
+    test(runWithData(doubles: _*)(_.sampleStdev()), e)
   }
 
   it should "support sampleVariance()" in {
-    runWithContext { sc =>
-      val e = expected.sampleVariance
-      val p1 = sc.parallelize(ints: _*).sampleVariance()
-      val p2 = sc.parallelize(longs: _*).sampleVariance()
-      val p3 = sc.parallelize(floats: _*).sampleVariance()
-      val p4 = sc.parallelize(doubles: _*).sampleVariance()
-      checkError(p1, e)
-      checkError(p2, e)
-      checkError(p3, e)
-      checkError(p4, e)
-    }
+    val e = expected.sampleVariance
+    test(runWithData(ints: _*)(_.sampleVariance()), e)
+    test(runWithData(longs: _*)(_.sampleVariance()), e)
+    test(runWithData(floats: _*)(_.sampleVariance()), e)
+    test(runWithData(doubles: _*)(_.sampleVariance()), e)
   }
 
   it should "support stdev()" in {
-    runWithContext { sc =>
-      val e = expected.stdev
-      val p1 = sc.parallelize(ints: _*).stdev()
-      val p2 = sc.parallelize(longs: _*).stdev()
-      val p3 = sc.parallelize(floats: _*).stdev()
-      val p4 = sc.parallelize(doubles: _*).stdev()
-      checkError(p1, e)
-      checkError(p2, e)
-      checkError(p3, e)
-      checkError(p4, e)
-    }
+    val e = expected.stdev
+    test(runWithData(ints: _*)(_.stdev()), e)
+    test(runWithData(longs: _*)(_.stdev()), e)
+    test(runWithData(floats: _*)(_.stdev()), e)
+    test(runWithData(doubles: _*)(_.stdev()), e)
   }
 
   it should "support variance()" in {
-    runWithContext { sc =>
-      val e = expected.variance
-      val p1 = sc.parallelize(ints: _*).variance()
-      val p2 = sc.parallelize(longs: _*).variance()
-      val p3 = sc.parallelize(floats: _*).variance()
-      val p4 = sc.parallelize(doubles: _*).variance()
-      checkError(p1, e)
-      checkError(p2, e)
-      checkError(p3, e)
-      checkError(p4, e)
-    }
+    val e = expected.variance
+    test(runWithData(ints: _*)(_.variance()), e)
+    test(runWithData(longs: _*)(_.variance()), e)
+    test(runWithData(floats: _*)(_.variance()), e)
+    test(runWithData(doubles: _*)(_.variance()), e)
   }
 
 }
