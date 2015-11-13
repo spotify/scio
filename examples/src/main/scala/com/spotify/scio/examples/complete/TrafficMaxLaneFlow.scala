4679042efafcf585842c0746963d775cddb4d372
Neville Li <neville@spotify.com>
2015-11-13 17:11:14 -0500

    optimize imports

--- examples/src/main/scala/com/spotify/scio/examples/complete/TrafficMaxLaneFlow.scala
+++ examples/src/main/scala/com/spotify/scio/examples/complete/TrafficMaxLaneFlow.scala
@@ -1,13 +1,13 @@
 package com.spotify.scio.examples.complete
 
-import com.google.api.services.bigquery.model.{TableSchema, TableFieldSchema}
+import com.google.api.services.bigquery.model.{TableFieldSchema, TableSchema}
 import com.google.cloud.dataflow.examples.common.DataflowExampleUtils
 import com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner
 import com.spotify.scio._
 import com.spotify.scio.bigquery._
 import com.spotify.scio.examples.common.{ExampleData, ExampleOptions}
-import org.joda.time.{Duration, Instant}
 import org.joda.time.format.DateTimeFormat
+import org.joda.time.{Duration, Instant}
 
 import scala.collection.JavaConverters._
 
