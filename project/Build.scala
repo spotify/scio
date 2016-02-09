8b9c3495ceccf2781b202dbabf36afe824785189
Neville Li <neville@spotify.com>
2016-02-09 11:49:29 -0500

    clean up SBT javacOptions

--- project/Build.scala
+++ project/Build.scala
@@ -17,9 +17,8 @@ object BuildSettings {
     scalaVersion       := "2.11.7",
     crossScalaVersions := Seq("2.10.6", "2.11.7"),
     scalacOptions      ++= Seq("-target:jvm-1.7", "-deprecation", "-feature", "-unchecked"),
-    scalacOptions in (Compile,doc)     ++= Seq("-groups", "-skip-packages", "com.google"),
-    javacOptions in (Compile)          ++= Seq("-source", "1.7"),
-    javacOptions in (Compile, compile) ++= Seq("-target", "1.7"),
+    scalacOptions in (Compile, doc)    ++= Seq("-groups", "-skip-packages", "com.google"),
+    javacOptions in (Compile, compile) ++= Seq("-source", "1.7", "-target", "1.7"),
 
     // Mappings from dependencies to external ScalaDoc/JavaDoc sites
     apiMappings ++= {
