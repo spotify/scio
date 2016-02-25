492f6ab21550754675728ff68b74d40c0dedb073
Neville Li <neville@spotify.com>
2016-02-25 18:13:52 -0500

    refactor FileStorage

--- scio-core/src/main/scala/com/spotify/scio/io/FileStorage.scala
+++ scio-core/src/main/scala/com/spotify/scio/io/FileStorage.scala
@@ -19,20 +19,22 @@ package com.spotify.scio.io
 
 import java.io.{File, FileInputStream, InputStream, SequenceInputStream}
 import java.net.URI
+import java.nio.channels.Channels
+import java.nio.file.Path
 import java.util.Collections
 
 import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.Charsets
-import com.google.api.services.storage.Storage
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory
+import com.google.cloud.dataflow.sdk.util.GcsUtil.GcsUtilFactory
+import com.google.cloud.dataflow.sdk.util.gcsfs.GcsPath
 import com.spotify.scio.bigquery.TableRow
 import com.spotify.scio.util.ScioUtil
 import org.apache.avro.Schema
 import org.apache.avro.file.DataFileStream
 import org.apache.avro.generic.GenericDatumReader
 import org.apache.avro.specific.{SpecificDatumReader, SpecificRecordBase}
+import org.apache.commons.io.filefilter.WildcardFileFilter
 import org.apache.commons.io.{FileUtils, IOUtils}
 
 import scala.collection.JavaConverters._
@@ -47,9 +49,9 @@ private trait FileStorage {
 
   protected val path: String
 
-  protected def list: Seq[String]
+  protected def list: Seq[Path]
 
-  protected def getObjectInputStream(path: String): InputStream
+  protected def getObjectInputStream(path: Path): InputStream
 
   def avroFile[T: ClassTag](schema: Schema = null): Iterator[T] = {
     val cls = ScioUtil.classOf[T]
@@ -81,38 +83,12 @@ private class GcsStorage(protected val path: String) extends FileStorage {
   private val uri = new URI(path)
   require(ScioUtil.isGcsUri(uri), s"Not a GCS path: $path")
 
-  lazy private val storage = new Storage.Builder(
-    GoogleNetHttpTransport.newTrustedTransport(),
-    JacksonFactory.getDefaultInstance,
-    GoogleCredential.getApplicationDefault).build()
+  private lazy val gcs = new GcsUtilFactory().create(PipelineOptionsFactory.create())
 
-  override protected def list: Seq[String] = {
-    val bucket = uri.getHost
-    val prefix = uri.getPath.replaceAll("^/", "") + (if (uri.getPath.endsWith("/")) "" else "/")
-    val pDelimiters = prefix.count(isDelimiter)
-    storage.objects()
-      .list(bucket)
-      .setPrefix(prefix)
-      .execute()
-      .getItems.asScala
-      .filter { o =>
-        val n = o.getName
-        val nDelimiters: Int = n.count(isDelimiter)
-        BigInt(o.getSize) > 0 && !isHidden(n) && nDelimiters == pDelimiters && n != prefix
-      }
-      .map(o => s"gs://$bucket/${o.getName}")
-  }
+  override protected def list: Seq[Path] = gcs.expand(GcsPath.fromUri(uri)).asScala
 
-  override protected def getObjectInputStream(path: String): InputStream = {
-    val uri = new URI(path)
-    val bucket = uri.getHost
-    val obj = uri.getPath.replaceAll("^/", "")
-    storage.objects().get(bucket, obj).executeMediaAsInputStream()
-  }
-
-  private def isDelimiter(char: Char) = char == '/'
-
-  private def isHidden(path: String) = !path.endsWith("/") && Set('_', '.')(path.charAt(path.lastIndexOf('/') + 1))
+  override protected def getObjectInputStream(path: Path): InputStream =
+    Channels.newInputStream(gcs.open(GcsPath.fromUri(path.toUri)))
 
 }
 
@@ -121,9 +97,15 @@ private class LocalStorage(protected val path: String)  extends FileStorage {
   private val uri = new URI(path)
   require(ScioUtil.isLocalUri(uri), s"Not a local path: $path")
 
-  override protected def list: Seq[String] =
-    FileUtils.listFiles(new File(path), null, false).asScala.map(_.getCanonicalPath).toSeq
+  override protected def list: Seq[Path] = {
+    val p = path.lastIndexOf("/")
+    FileUtils
+      .listFiles(new File(path.substring(0, p)), new WildcardFileFilter(path.substring(p + 1)), null)
+      .asScala
+      .toSeq
+      .map(_.toPath)
+  }
 
-  override protected def getObjectInputStream(path: String): InputStream = new FileInputStream(path)
+  override protected def getObjectInputStream(path: Path): InputStream = new FileInputStream(path.toFile)
 
 }
