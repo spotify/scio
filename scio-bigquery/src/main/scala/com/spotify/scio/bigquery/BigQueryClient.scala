5c841ff0cbd40d4d186e2a41a8bea7ba0c9bb7af
Neville Li <neville@spotify.com>
2016-02-27 11:04:05 -0500

    clean up BigQueryClient

--- scio-bigquery/src/main/scala/com/spotify/scio/bigquery/BigQueryClient.scala
+++ scio-bigquery/src/main/scala/com/spotify/scio/bigquery/BigQueryClient.scala
@@ -17,7 +17,7 @@
 
 package com.spotify.scio.bigquery
 
-import java.io.{StringReader, FileInputStream, File}
+import java.io.{File, FileInputStream, StringReader}
 import java.util.UUID
 import java.util.regex.Pattern
 
@@ -28,20 +28,20 @@ import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonObjectParser
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.bigquery.Bigquery.Builder
-import com.google.api.services.bigquery.{BigqueryScopes, Bigquery}
 import com.google.api.services.bigquery.model._
-import com.google.cloud.dataflow.sdk.options.{PipelineOptionsFactory, GcpOptions}
-import com.google.cloud.dataflow.sdk.util.{Credentials, BigQueryTableRowIterator}
+import com.google.api.services.bigquery.{Bigquery, BigqueryScopes}
+import com.google.cloud.dataflow.sdk.io.BigQueryIO
+import com.google.cloud.dataflow.sdk.util.BigQueryTableRowIterator
 import com.google.common.base.Charsets
 import com.google.common.hash.Hashing
 import com.google.common.io.Files
 import org.apache.commons.io.FileUtils
-import org.joda.time.{Period, Instant}
-import org.joda.time.format.{PeriodFormatterBuilder, DateTimeFormat}
+import org.joda.time.format.{DateTimeFormat, PeriodFormatterBuilder}
+import org.joda.time.{Instant, Period}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.JavaConverters._
-import scala.util.{Try, Random}
+import scala.util.{Random, Try}
 
 /** Utility for BigQuery data types. */
 object BigQueryUtil {
@@ -53,22 +53,7 @@ object BigQueryUtil {
   private val TABLE_REGEXP = "[-\\w$@]{1,1024}"
   private val DATASET_TABLE_REGEXP =
     s"((?<PROJECT>$PROJECT_ID_REGEXP):)?(?<DATASET>$DATASET_REGEXP)\\.(?<TABLE>$TABLE_REGEXP)"
-  private val TABLE_SPEC = Pattern.compile(DATASET_TABLE_REGEXP)
-  private val QUERY_TABLE_SPEC = Pattern.compile(s"(?<=\\[)$DATASET_REGEXP(?=\\])")
-
-  /** Parse a table specification string. */
-  def parseTableSpec(tableSpec: String): TableReference = {
-    val m = TABLE_SPEC.matcher(tableSpec)
-    require(m.matches(), s"Table reference is not in [project_id]:[dataset_id].[table_id] format: $tableSpec")
-    new TableReference()
-      .setProjectId(m.group("PROJECT"))
-      .setDatasetId(m.group("DATASET"))
-      .setTableId(m.group("TABLE"))
-  }
-
-  /** Convert a table reference to string. */
-  def toTableSpec(table: TableReference): String =
-    (if (table.getProjectId != null) table.getProjectId + ":" else "") + table.getDatasetId + "."  + table.getTableId
+  private val QUERY_TABLE_SPEC = Pattern.compile(s"(?<=\\[)$DATASET_TABLE_REGEXP(?=\\])")
 
   /** Parse a schema string. */
   def parseSchema(schemaString: String): TableSchema =
@@ -79,7 +64,7 @@ object BigQueryUtil {
     val matcher = BigQueryUtil.QUERY_TABLE_SPEC.matcher(sqlQuery)
     val b = Set.newBuilder[TableReference]
     while (matcher.find()) {
-      b += parseTableSpec(matcher.group())
+      b += BigQueryIO.parseTableSpec(matcher.group())
     }
     b.result()
   }
@@ -118,13 +103,13 @@ class BigQueryClient private (private val projectId: String, credential: Credent
 
     // Create temporary table view and get schema
     val table = temporaryTable(TABLE_PREFIX)
-    logger.info(s"Creating temporary view ${BigQueryUtil.toTableSpec(table)}")
+    logger.info(s"Creating temporary view ${BigQueryIO.toTableSpec(table)}")
     val view = new ViewDefinition().setQuery(sqlQuery)
     val viewTable = new Table().setView(view).setTableReference(table)
     val schema = bigquery.tables().insert(table.getProjectId, table.getDatasetId, viewTable).execute().getSchema
 
     // Delete temporary table
-    logger.info(s"Deleting temporary view ${BigQueryUtil.toTableSpec(table)}")
+    logger.info(s"Deleting temporary view ${BigQueryIO.toTableSpec(table)}")
     bigquery.tables().delete(table.getProjectId, table.getDatasetId, table.getTableId).execute()
 
     schema
@@ -138,17 +123,17 @@ class BigQueryClient private (private val projectId: String, credential: Credent
   }
 
   /** Get rows from a table. */
-  def getTableRows(tableSpec: String): Iterator[TableRow] = getTableRows(BigQueryUtil.parseTableSpec(tableSpec))
+  def getTableRows(tableSpec: String): Iterator[TableRow] = getTableRows(BigQueryIO.parseTableSpec(tableSpec))
 
   /** Get rows from a table. */
   def getTableRows(table: TableReference): Iterator[TableRow] =
     BigQueryTableRowIterator.fromTable(table, bigquery).asScala
 
   /** Get schema from a table. */
-  def getTableSchema(tableSpec: String): TableSchema = getTableSchema(BigQueryUtil.parseTableSpec(tableSpec))
+  def getTableSchema(tableSpec: String): TableSchema = getTableSchema(BigQueryIO.parseTableSpec(tableSpec))
 
   /** Get schema from a table. */
-  def getTableSchema(table: TableReference): TableSchema = withCacheKey(BigQueryUtil.toTableSpec(table)) {
+  def getTableSchema(table: TableReference): TableSchema = withCacheKey(BigQueryIO.toTableSpec(table)) {
     getTable(table).getSchema
   }
 
@@ -163,18 +148,18 @@ class BigQueryClient private (private val projectId: String, credential: Credent
       val table = getCacheDestinationTable(sqlQuery).get
       val time = BigInt(getTable(table).getLastModifiedTime)
       if (sourceTimes.forall(_ < time)) {
-        logger.info(s"Cache hit, existing destination table: ${BigQueryUtil.toTableSpec(table)}")
+        logger.info(s"Cache hit, existing destination table: ${BigQueryIO.toTableSpec(table)}")
         (table, None)
       } else {
         val temp = temporaryTable(TABLE_PREFIX)
-        logger.info(s"Cache invalid, new destination table: ${BigQueryUtil.toTableSpec(temp)}")
+        logger.info(s"Cache invalid, new destination table: ${BigQueryIO.toTableSpec(temp)}")
         setCacheDestinationTable(sqlQuery, temp)
         (temp, Some(makeQuery(sqlQuery, temp)))
       }
     } catch {
       case _: Throwable =>
         val temp = temporaryTable(TABLE_PREFIX)
-        logger.info(s"Cache miss, new destination table: ${BigQueryUtil.toTableSpec(temp)}")
+        logger.info(s"Cache miss, new destination table: ${BigQueryIO.toTableSpec(temp)}")
         setCacheDestinationTable(sqlQuery, temp)
         (temp, Some(makeQuery(sqlQuery, temp)))
     }
@@ -294,10 +279,10 @@ class BigQueryClient private (private val projectId: String, credential: Credent
   }.toOption
 
   private def setCacheDestinationTable(key: String, table: TableReference): Unit =
-    Files.write(BigQueryUtil.toTableSpec(table), tableCacheFile(key), Charsets.UTF_8)
+    Files.write(BigQueryIO.toTableSpec(table), tableCacheFile(key), Charsets.UTF_8)
 
   private def getCacheDestinationTable(key: String): Option[TableReference] = Try {
-    BigQueryUtil.parseTableSpec(scala.io.Source.fromFile(tableCacheFile(key)).mkString)
+    BigQueryIO.parseTableSpec(scala.io.Source.fromFile(tableCacheFile(key)).mkString)
   }.toOption
 
   private def cacheFile(key: String, suffix: String): File = {
