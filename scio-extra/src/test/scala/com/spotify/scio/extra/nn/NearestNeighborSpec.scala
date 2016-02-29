07deaef75be0925d956d199dfdc37b4ad04aed69
Neville Li <neville@spotify.com>
2016-02-29 11:38:46 -0500

    lower LSHNN coverage threshold

--- scio-extra/src/test/scala/com/spotify/scio/extra/nn/NearestNeighborSpec.scala
+++ scio-extra/src/test/scala/com/spotify/scio/extra/nn/NearestNeighborSpec.scala
@@ -39,7 +39,7 @@ object NearestNeighborSpec extends Properties("NearestNeighbor") {
   property("LSHNN") = forAll(vectors, maxResult, minSimilarity) { (vectors, maxResult, minSimilarity) =>
     val b = NearestNeighbor.newLSHBuilder[String, Double](dimension, 5, max(vectors.size / 100, 10))
     // TODO: figure out coverage expectation
-    verify(b, vectors, maxResult, minSimilarity, 0.5, 0.5, 0.5)
+    verify(b, vectors, maxResult, minSimilarity, 0.25, 0.25, 0.25)
   }
 
   def verify(builder: NearestNeighborBuilder[String, Double],
