e3d96092f334c1a7cc7520d05059224cb2ee0320
Rafal Wojdyla <rav@spotify.com>
2016-03-02 21:28:31 -0500

    Add async closing of context to repl

--- scio-repl/src/main/scala/com/spotify/scio/repl/ReplScioContext.scala
+++ scio-repl/src/main/scala/com/spotify/scio/repl/ReplScioContext.scala
@@ -1,10 +1,40 @@
 package com.spotify.scio.repl
 
+import java.io.{OutputStream, PrintStream}
+
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions
 import com.spotify.scio.ScioResult
 
+import scala.concurrent.Future
+
 class ReplScioContext (options: DataflowPipelineOptions, private var artifacts: List[String], testId: Option[String])
   extends com.spotify.scio.ScioContext (options, artifacts, testId) {
+
+  private val nullout = new PrintStream(new OutputStream() {
+    override def write(b: Int) = {}
+    override def write(b: Array[Byte]) = {}
+    override def write(b: Array[Byte], off: Int, len: Int) = {}
+  })
+
+  /**
+   * Async close/run Scio context.
+   * NOTE: by default no std{out,err} is available as feedback
+   * NOTE: to turn off or reduce logging noise - use sl4j properties - for example:
+   *  `-Dorg.slf4j.simpleLogger.logFile=/dev/null`
+   */
+  def asyncClose(stdout: PrintStream = nullout, stderr: PrintStream = nullout): Future[ScioResult] = {
+    import scala.language.reflectiveCalls
+    this.getClass.getClassLoader.asInstanceOf[ {def createReplCodeJar: String} ].createReplCodeJar
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    /* Console.with* affects worker thread - dataflow version/job id manages to get printed still */
+    Future(Console.withOut(stdout){
+      Console.withErr(stderr) {
+        super.close()
+      }
+    })
+  }
+
   /** Enhance original close method with dumping REPL session jar */
   override def close(): ScioResult = {
     import scala.language.reflectiveCalls
