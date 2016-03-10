ef8ee1f54fafb56b3c4d31cb7abb7ccf5ebdcc8f
Neville Li <neville@spotify.com>
2016-03-09 23:05:12 -0500

    remove ReplScioContext#asyncClose()

--- scio-repl/src/main/scala/com/spotify/scio/repl/ReplScioContext.scala
+++ scio-repl/src/main/scala/com/spotify/scio/repl/ReplScioContext.scala
@@ -20,12 +20,10 @@ package com.spotify.scio.repl
 import java.io.{OutputStream, PrintStream}
 
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions
-import com.spotify.scio.ScioResult
+import com.spotify.scio.{ScioContext, ScioResult}
 
-import scala.concurrent.Future
-
-class ReplScioContext (options: DataflowPipelineOptions, private var artifacts: List[String], testId: Option[String])
-  extends com.spotify.scio.ScioContext(options, artifacts, testId) {
+class ReplScioContext(options: DataflowPipelineOptions, artifacts: List[String], testId: Option[String])
+  extends ScioContext(options, artifacts, testId) {
 
   private lazy val nullout = new PrintStream(new OutputStream() {
     override def write(b: Int) = {}
@@ -33,27 +31,8 @@ class ReplScioContext (options: DataflowPipelineOptions, private var artifacts: 
     override def write(b: Array[Byte], off: Int, len: Int) = {}
   })
 
-  /**
-   * Async close/run Scio context.
-   * NOTE: by default no std{out,err} is available as feedback.
-   * NOTE: to turn off or reduce logging noise - use sl4j properties - for example:
-   *  `-Dorg.slf4j.simpleLogger.logFile=/dev/null`
-   */
-  def asyncClose(stdout: PrintStream = nullout, stderr: PrintStream = nullout): Future[ScioResult] = {
-    createJar()
-
-    import scala.concurrent.ExecutionContext.Implicits.global
-    // Console.with* affects worker thread - dataflow version/job id manages to get printed still
-    Future(Console.withOut(stdout){
-      Console.withErr(stderr) {
-        super.close()
-      }
-    })
-  }
-
-  /** Enhance original close method with dumping REPL session jar */
+  /** Enhanced version that dumps REPL session jar. */
   override def close(): ScioResult = {
-    // TODO: add notification if distributed mode, that it may take minutes for DF to start
     createJar()
     super.close()
   }
