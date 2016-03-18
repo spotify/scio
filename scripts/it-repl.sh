6fe84f31a76f21d40151b85f10c0045e3ae3b93d
Rafal Wojdyla <rav@spotify.com>
2016-03-18 18:36:53 -0400

    Adjust it-repl script to travis env

--- scripts/it-repl.sh
+++ scripts/it-repl.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-SCALA_VERSION=${1?"provide scala version as first argument"}
+if [ $# != 1 ]; then
+  echo "usage: $0 [full-scala-version]"
+  echo "example: $0 2.11.7"
+  exit 1
+fi
+
+SCALA_VERSION=$1
+
+echo "Will test scio REPL for scala ${SCALA_VERSION}"
 
 sbt ++$SCALA_VERSION "project scio-repl" assembly
-find scio-repl/src/it/resources -type file -exec sh -c "cat {} | java -jar scio-repl/target/scala-${SCALA_VERSION%.*}/scio-repl-*.jar" \;
+
+echo "Test scripts:"
+find ./scio-repl/src/it/resources -type f
+
+find ./scio-repl/src/it/resources -type f -exec sh -c "cat {} | java -jar ./scio-repl/target/scala-${SCALA_VERSION%.*}/scio-repl-*.jar" \;
