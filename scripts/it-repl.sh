2037e86ea0250188e98ebed5a915548429bb0686
Rafal Wojdyla <rav@spotify.com>
2016-03-18 19:26:37 -0400

    moar fixes for it script

--- scripts/it-repl.sh
+++ scripts/it-repl.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-SCALA_VERSION=${1}
+if [ $# != 1 ]; then
+  echo "usage: $0 [full-scala-version]"
+  echo "example: $0 2.11.7"
+  exit 1
+fi
+
+SCALA_VERSION=$1
 
 echo "Will test scio REPL for scala ${SCALA_VERSION}"
 
 sbt ++$SCALA_VERSION "project scio-repl" assembly
 
 echo "Test scripts:"
-find ./scio-repl/src/it/resources -type file
+find ./scio-repl/src/it/resources -type f
 
-find ./scio-repl/src/it/resources -type file -exec sh -c "cat {} | java -jar ./scio-repl/target/scala-${SCALA_VERSION%.*}/scio-repl-*.jar" \;
+find ./scio-repl/src/it/resources -type f -exec sh -c "cat {} | java -jar ./scio-repl/target/scala-${SCALA_VERSION%.*}/scio-repl-*.jar" \;
