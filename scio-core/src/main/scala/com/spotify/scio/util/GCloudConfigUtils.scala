b572c67f0723788161f73926191a93a3868a7586
Neville Li <neville@spotify.com>
2016-03-19 15:05:13 +0800

    make GCloudConfigUtils private and clean up comments

--- scio-core/src/main/scala/com/spotify/scio/util/GCloudConfigUtils.scala
+++ scio-core/src/main/scala/com/spotify/scio/util/GCloudConfigUtils.scala
@@ -4,17 +4,18 @@ import java.io.File
 import org.ini4j.Ini
 import org.slf4j.LoggerFactory
 
-object GCloudConfigUtils {
+private[scio] object GCloudConfigUtils {
+
   val logger = LoggerFactory.getLogger(GCloudConfigUtils.getClass)
   val PROJECT_ENV_NAME = "GCLOUD_PROJECT"
   val CLOUD_CONFIG_ENV_NAME = "CLOUDSDK_CONFIG"
 
-  /**
-   * Check:
-   * 1. system property for project name
-   * 2. environmental variable for project name
-   * 3. configuration files for project name
-   */
+  /*
+  Check:
+  1. system property for project name
+  2. environmental variable for project name
+  3. configuration files for project name
+  */
   def getGCloudProjectId: Option[String] = {
     val projectId = sys.props.getOrElse(PROJECT_ENV_NAME,
                                         sys.env.getOrElse(PROJECT_ENV_NAME, null))
@@ -36,14 +37,14 @@ object GCloudConfigUtils {
     }
   }
 
-  /**
-   * Search for GCloud config file (and return first), in the order of:
-   * 1. [gcloud-config-dir]/configurations/config_scio
-   * 2. [gcloud-config-dir]/configurations/config_default
-   * 3. [gcloud-config-dir]/properties
-   *
-   * where gcloud-config-dir can be for example: `~/.config/gcloud`
-   */
+  /*
+  Search for GCloud config file (and return first), in the order of:
+  1. [gcloud-config-dir]/configurations/config_scio
+  2. [gcloud-config-dir]/configurations/config_default
+  3. [gcloud-config-dir]/properties
+
+  where gcloud-config-dir can be for example: `~/.config/gcloud`
+  */
   private def getConfigFile(configDir: File): File = {
     val possibleConfigs = List("configurations/config_scio",
                                "configurations/config_default",
@@ -69,4 +70,5 @@ object GCloudConfigUtils {
       None
     }
   }
+
 }
