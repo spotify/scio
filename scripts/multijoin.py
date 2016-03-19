7112f553d55b67f4176634fa544942c8bffbfb26
Neville Li <neville@spotify.com>
2016-03-20 07:02:36 +0800

    reduce transforms in MultiJoin

--- scripts/multijoin.py
+++ scripts/multijoin.py
@@ -72,52 +72,83 @@ def cogroup(out, n):
 
 
 def join(out, n):
+    vals = mkVals(n)
+
     print >> out, '  def apply[%s](%s): %s = {' % (
         mkClassTags(n), mkFnArgs(n), mkFnRetVal(n))
-    print >> out, '    cogroup(%s)' % mkArgs(n)
-    print >> out, '      .flatMap { kv =>'
-    print >> out, '        for {'
-    for (i, x) in enumerate(mkVals(n)):
-        print >> out, '          %s <- kv._2._%d' % (x.lower(), i + 1)
-    print >> out, '        } yield (kv._1, (%s))' % mkArgs(n)
-    print >> out, '      }'
+
+    print >> out, '    val (%s) = (%s)' % (
+        ', '.join('tag' + x for x in vals),
+        ', '.join('new TupleTag[%s]()' % x for x in vals))
+
+    print >> out, '    val keyed = KeyedPCollectionTuple'
+    print >> out, '      .of(tagA, a.toKV.internal)'
+    for x in vals[1:]:
+        print >> out, '      .and(tag%s, %s.toKV.internal)' % (x, x.lower())
+    print >> out, '      .apply(CallSites.getCurrent, CoGroupByKey.create())'
+
+    print >> out, '    a.context.wrap(keyed).flatMap { kv =>'
+    print >> out, '      val (k, r) = (kv.getKey, kv.getValue)'
+    print >> out, '      for {'
+    for x in vals:
+        print >> out, '        %s <- r.getAll(tag%s).asScala' % (x.lower(), x)
+    print >> out, '      } yield (k, (%s))' % mkArgs(n)
+    print >> out, '    }'
     print >> out, '  }'
     print >> out
 
 
 def left(out, n):
+    vals = mkVals(n)
     print >> out, '  def left[%s](%s): %s = {' % (
         mkClassTags(n), mkFnArgs(n), mkFnRetVal(n, None, 'Option'))
-    print >> out, '    cogroup(%s)' % mkArgs(n)
-    print >> out, '      .flatMap { kv =>'
-    print >> out, '        for {'
-    for (i, x) in enumerate(mkVals(n)):
-        y = x.lower()
-        j = i + 1
+
+    print >> out, '    val (%s) = (%s)' % (
+        ', '.join('tag' + x for x in vals),
+        ', '.join('new TupleTag[%s]()' % x for x in vals))
+
+    print >> out, '    val keyed = KeyedPCollectionTuple'
+    print >> out, '      .of(tagA, a.toKV.internal)'
+    for x in vals[1:]:
+        print >> out, '      .and(tag%s, %s.toKV.internal)' % (x, x.lower())
+    print >> out, '      .apply(CallSites.getCurrent, CoGroupByKey.create())'
+
+    print >> out, '    a.context.wrap(keyed).flatMap { kv =>'
+    print >> out, '      val (k, r) = (kv.getKey, kv.getValue)'
+    print >> out, '      for {'
+    for (i, x) in enumerate(vals):
         if (i == 0):
-            print >> out, '          %s <- kv._2._%d' % (y, j)
+            print >> out, '        %s <- r.getAll(tag%s).asScala' % (x.lower(), x)
         else:
-            print >> out, '          %s <- if (kv._2._%d.isEmpty) Iterable(None) else kv._2._%d.map(Option(_))' % (  # NOQA
-                y, j, j)
-    print >> out, '        } yield (kv._1, (%s))' % mkArgs(n)
-    print >> out, '      }'
+            print >> out, '        %s <- toOptions(r.getAll(tag%s).asScala)' % (x.lower(), x)
+    print >> out, '      } yield (k, (%s))' % mkArgs(n)
+    print >> out, '    }'
     print >> out, '  }'
     print >> out
 
 
 def outer(out, n):
+    vals = mkVals(n)
     print >> out, '  def outer[%s](%s): %s = {' % (
         mkClassTags(n), mkFnArgs(n), mkFnRetVal(n, 'Option', 'Option'))
-    print >> out, '    cogroup(%s)' % mkArgs(n)
-    print >> out, '      .flatMap { kv =>'
-    print >> out, '        for {'
-    for (i, x) in enumerate(mkVals(n)):
-        y = x.lower()
-        j = i + 1
-        print >> out, '          %s <- if (kv._2._%d.isEmpty) Iterable(None) else kv._2._%d.map(Option(_))' % (  # NOQA
-            y, j, j)
-    print >> out, '        } yield (kv._1, (%s))' % mkArgs(n)
-    print >> out, '      }'
+
+    print >> out, '    val (%s) = (%s)' % (
+        ', '.join('tag' + x for x in vals),
+        ', '.join('new TupleTag[%s]()' % x for x in vals))
+
+    print >> out, '    val keyed = KeyedPCollectionTuple'
+    print >> out, '      .of(tagA, a.toKV.internal)'
+    for x in vals[1:]:
+        print >> out, '      .and(tag%s, %s.toKV.internal)' % (x, x.lower())
+    print >> out, '      .apply(CallSites.getCurrent, CoGroupByKey.create())'
+
+    print >> out, '    a.context.wrap(keyed).flatMap { kv =>'
+    print >> out, '      val (k, r) = (kv.getKey, kv.getValue)'
+    print >> out, '      for {'
+    for (i, x) in enumerate(vals):
+        print >> out, '        %s <- toOptions(r.getAll(tag%s).asScala)' % (x.lower(), x)
+    print >> out, '      } yield (k, (%s))' % mkArgs(n)
+    print >> out, '    }'
     print >> out, '  }'
     print >> out
 
@@ -159,9 +190,11 @@ def main(out):
         import scala.reflect.ClassTag
 
         object MultiJoin {
+
+          def toOptions[T](xs: Iterable[T]): Iterable[Option[T]] = if (xs.isEmpty) Iterable(None) else xs.map(Option(_))
         ''').replace('  # NOQA', '')
 
-    N = 22
+    N = 4
     for i in xrange(2, N + 1):
         cogroup(out, i)
     for i in xrange(2, N + 1):
