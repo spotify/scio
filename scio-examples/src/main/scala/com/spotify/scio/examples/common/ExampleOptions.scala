12a8e908ad4b6ea9fefa5338d390294569746944
Neville Li <neville@spotify.com>
2016-03-23 13:35:05 +0800

    line length fixes for ExampleOptions

--- scio-examples/src/main/scala/com/spotify/scio/examples/common/ExampleOptions.scala
+++ scio-examples/src/main/scala/com/spotify/scio/examples/common/ExampleOptions.scala
@@ -18,11 +18,15 @@
 package com.spotify.scio.examples.common
 
 import com.google.api.services.bigquery.model.TableReference
-import com.google.cloud.dataflow.examples.common.{DataflowExampleOptions, ExampleBigQueryTableOptions, ExamplePubsubTopicOptions}
+import com.google.cloud.dataflow.examples.common._
 import com.google.cloud.dataflow.sdk.io.BigQueryIO
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions
 
-trait ExampleOptions extends DataflowPipelineOptions with DataflowExampleOptions with ExampleBigQueryTableOptions with ExamplePubsubTopicOptions
+trait ExampleOptions
+  extends DataflowPipelineOptions
+  with DataflowExampleOptions
+  with ExampleBigQueryTableOptions
+  with ExamplePubsubTopicOptions
 
 object ExampleOptions {
   def bigQueryTable(options: ExampleOptions): String =
