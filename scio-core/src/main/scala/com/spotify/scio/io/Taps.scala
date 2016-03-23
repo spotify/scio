a74050abb4ad298abfe5129de71714381d2f894a
Neville Li <neville@spotify.com>
2016-03-23 12:44:33 -0700

    line length fixes for Taps.scala

--- scio-core/src/main/scala/com/spotify/scio/io/Taps.scala
+++ scio-core/src/main/scala/com/spotify/scio/io/Taps.scala
@@ -3,7 +3,10 @@ package com.spotify.scio.io
 import com.google.api.client.util.{BackOffUtils, Sleeper, BackOff}
 import com.google.api.services.bigquery.model.TableReference
 import com.google.cloud.dataflow.sdk.io.BigQueryIO
-import com.google.cloud.dataflow.sdk.util.{AttemptBoundedExponentialBackOff, IntervalBoundedExponentialBackOff}
+import com.google.cloud.dataflow.sdk.util.{
+  AttemptBoundedExponentialBackOff,
+  IntervalBoundedExponentialBackOff
+}
 import com.spotify.scio.bigquery.{BigQueryClient, BigQueryUtil, TableRow}
 import org.apache.avro.Schema
 import org.slf4j.{LoggerFactory, Logger}
