436305b30f311a417248de093a476e4c3965fc98
Neville Li <neville@spotify.com>
2016-03-28 13:56:50 -0400

    suppress warnings for non-Spotify license headers

--- scio-core/src/main/scala/com/spotify/scio/util/StatCounter.scala
+++ scio-core/src/main/scala/com/spotify/scio/util/StatCounter.scala
@@ -1,3 +1,4 @@
+// scalastyle:off header.matches
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -14,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// scalastyle:on header.matches
 
 /* Ported from org.apache.spark.util.StatCounter */
 
