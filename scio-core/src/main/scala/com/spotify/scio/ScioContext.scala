9426259a4245ec72c5460b5bde368f6f56bfc44b
Neville Li <neville@spotify.com>
2016-03-30 17:05:20 -0400

    add ScioContext#applyTransform

--- scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
+++ scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
@@ -307,6 +307,13 @@ class ScioContext private[scio] (val options: DataflowPipelineOptions,
     pipeline.apply(CallSites.getCurrent, root)
 
   /**
+   * Apply a [[com.google.cloud.dataflow.sdk.transforms.PTransform PTransform]] and wrap the output
+   * in an [[SCollection]].
+   */
+  def applyTransform[T: ClassTag](root: PTransform[_ >: PBegin, PCollection[T]]): SCollection[T] =
+    this.wrap(this.applyInternal(root))
+
+  /**
    * Get an SCollection for an object file.
    * @group input
    */
---
+++ scio-test/src/test/scala/com/spotify/scio/ScioContextTest.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio
+
+import com.google.cloud.dataflow.sdk.testing.DataflowAssert
+import com.google.cloud.dataflow.sdk.transforms.Create
+import com.google.common.collect.Lists
+import com.spotify.scio.testing.PipelineSpec
+
+import scala.collection.JavaConverters._
+
+class ScioContextTest extends PipelineSpec {
+
+  "ScioContext" should "support applyTransform()" in {
+    val sc = ScioContext()
+    sc.applyTransform(Create.of((1 to 10).asJava)) should containInAnyOrder (1 to 10)
+    sc.close()
+  }
+
+  it should "support pipeline" in {
+    val pipeline = ScioContext().pipeline
+    val p = pipeline.apply(Create.of(Lists.newArrayList(1, 2, 3)))
+    DataflowAssert.that(p).containsInAnyOrder(Lists.newArrayList(1, 2, 3))
+    pipeline.run()
+  }
+
+}
