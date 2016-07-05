986b47a476b42957ee56561a4c083e0ddf01c368
Neville Li <neville.lyh@gmail.com>
2016-07-05 10:46:45 -0400

    [BEAM-371] Backport HDFS IO enhancements (#192)
    * update JobID handling in HadoopFileSink
    
    * backport HDFS IO changes from Beam

--- scio-hdfs/src/main/java/com/google/cloud/dataflow/sdk/io/hdfs/AvroHDFSFileSource.java
+++ scio-hdfs/src/main/java/com/google/cloud/dataflow/sdk/io/hdfs/AvroHDFSFileSource.java
@@ -1,20 +1,21 @@
 /*
- * Copyright (C) 2015 The Google Cloud Dataflow Hadoop Library Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-package com.google.cloud.dataflow.contrib.hadoop;
+package com.google.cloud.dataflow.sdk.io.hdfs;
 
 import com.google.cloud.dataflow.sdk.coders.AvroCoder;
 import com.google.cloud.dataflow.sdk.coders.Coder;
@@ -45,18 +46,19 @@ import java.util.List;
  *
  * @param <T> The type of the Avro records to be read from the source.
  */
-public class AvroHadoopFileSource<T> extends HadoopFileSource<AvroKey<T>, NullWritable> {
-  private static final Logger LOG = LoggerFactory.getLogger(AvroHadoopFileSource.class);
+public class AvroHDFSFileSource<T> extends HDFSFileSource<AvroKey<T>, NullWritable> {
   private static final long serialVersionUID = 0L;
 
   protected final AvroCoder<T> avroCoder;
   private final String schemaStr;
 
-  public AvroHadoopFileSource(String filepattern, AvroCoder<T> avroCoder) {
+  public AvroHDFSFileSource(String filepattern, AvroCoder<T> avroCoder) {
     this(filepattern, avroCoder, null);
   }
 
-  public AvroHadoopFileSource(String filepattern, AvroCoder<T> avroCoder, HadoopFileSource.SerializableSplit serializableSplit) {
+  public AvroHDFSFileSource(String filepattern,
+                            AvroCoder<T> avroCoder,
+                            HDFSFileSource.SerializableSplit serializableSplit) {
     super(filepattern,
         ClassUtil.<AvroKeyInputFormat<T>>castClass(AvroKeyInputFormat.class),
         ClassUtil.<AvroKey<T>>castClass(AvroKey.class),
@@ -72,46 +74,48 @@ public class AvroHadoopFileSource<T> extends HadoopFileSource<AvroKey<T>, NullWr
   }
 
   @Override
-  public List<? extends AvroHadoopFileSource<T>> splitIntoBundles(long desiredBundleSizeBytes,
-                                                                  PipelineOptions options) throws Exception {
+  public List<? extends AvroHDFSFileSource<T>> splitIntoBundles(
+      long desiredBundleSizeBytes, PipelineOptions options) throws Exception {
     if (serializableSplit == null) {
-      LOG.info("Splitting '" + filepattern + "' with desired bundle size " +
-          desiredBundleSizeBytes + " bytes.");
       return Lists.transform(computeSplits(desiredBundleSizeBytes),
-              new Function<InputSplit, AvroHadoopFileSource<T>>() {
-                @Nullable
-                @Override
-                public AvroHadoopFileSource<T> apply(@Nullable InputSplit inputSplit) {
-                  return new AvroHadoopFileSource<T>(filepattern, avroCoder, new SerializableSplit(inputSplit));
-                }
-              });
+          new Function<InputSplit, AvroHDFSFileSource<T>>() {
+            @Override
+            public AvroHDFSFileSource<T> apply(@Nullable InputSplit inputSplit) {
+              return new AvroHDFSFileSource<>(
+                  filepattern, avroCoder, new SerializableSplit(inputSplit));
+            }
+          });
     } else {
       return ImmutableList.of(this);
     }
   }
 
   @Override
-  public BoundedReader<KV<AvroKey<T>, NullWritable>> createReader(PipelineOptions options) throws IOException {
+  public BoundedReader<KV<AvroKey<T>, NullWritable>> createReader(PipelineOptions options)
+      throws IOException {
     this.validate();
 
     Schema schema = new Schema.Parser().parse(schemaStr);
     if (serializableSplit == null) {
-      return new AvroHadoopFileReader<>(this, filepattern, formatClass, schema);
+      return new AvroHDFSFileReader<>(this, filepattern, formatClass, schema);
     } else {
-      return new AvroHadoopFileReader<>(this, filepattern, formatClass, schema, serializableSplit.getSplit());
+      return new AvroHDFSFileReader<>(this, filepattern, formatClass, schema,
+          serializableSplit.getSplit());
     }
   }
 
-  static class AvroHadoopFileReader<T> extends HadoopFileReader<AvroKey<T>, NullWritable> {
-    public AvroHadoopFileReader(BoundedSource<KV<AvroKey<T>, NullWritable>> source,
-                                String filepattern, Class<? extends FileInputFormat<?, ?>> formatClass,
-                                Schema schema) throws IOException {
+  static class AvroHDFSFileReader<T> extends HDFSFileReader<AvroKey<T>, NullWritable> {
+    public AvroHDFSFileReader(BoundedSource<KV<AvroKey<T>, NullWritable>> source,
+                              String filepattern,
+                              Class<? extends FileInputFormat<?, ?>> formatClass,
+                              Schema schema) throws IOException {
       this(source, filepattern, formatClass, schema, null);
     }
 
-    public AvroHadoopFileReader(BoundedSource<KV<AvroKey<T>, NullWritable>> source,
-                                String filepattern, Class<? extends FileInputFormat<?, ?>> formatClass,
-                                Schema schema, InputSplit split) throws IOException {
+    public AvroHDFSFileReader(BoundedSource<KV<AvroKey<T>, NullWritable>> source,
+                              String filepattern,
+                              Class<? extends FileInputFormat<?, ?>> formatClass,
+                              Schema schema, InputSplit split) throws IOException {
       super(source, filepattern, formatClass, split);
       AvroJob.setInputKeySchema(job, schema);
     }
@@ -123,7 +127,7 @@ public class AvroHadoopFileSource<T> extends HadoopFileSource<AvroKey<T>, NullWr
       NullWritable value = currentReader.getCurrentValue();
 
       // clone the record to work around identical element issue due to object reuse
-      Coder<T> avroCoder = ((AvroHadoopFileSource<T>) this.getCurrentSource()).avroCoder;
+      Coder<T> avroCoder = ((AvroHDFSFileSource<T>) this.getCurrentSource()).avroCoder;
       key = new AvroKey(CoderUtils.clone(avroCoder, key.datum()));
 
       return KV.of(key, value);
@@ -134,7 +138,7 @@ public class AvroHadoopFileSource<T> extends HadoopFileSource<AvroKey<T>, NullWr
   static class ClassUtil {
     @SuppressWarnings("unchecked")
     static <T> Class<T> castClass(Class<?> aClass) {
-      return (Class<T>)aClass;
+      return (Class<T>) aClass;
     }
   }
 
