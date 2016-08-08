ce83f63fa0be1e7e29ae80bed0f8f8947ad5e54d
Rémy-Christophe Schermesser <git@schermesser.com>
2016-08-08 14:53:09 +0200

    Allow usage of different ScalaTest specs (#208)

--- scio-test/src/main/scala/com/spotify/scio/testing/PipelineSpec.scala
+++ scio-test/src/main/scala/com/spotify/scio/testing/PipelineSpec.scala
@@ -17,131 +17,20 @@
 
 package com.spotify.scio.testing
 
-import com.spotify.scio._
-import com.spotify.scio.values.SCollection
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.reflect.ClassTag
-
 /**
- * Trait for unit testing pipelines.
- *
- * A simple test might look like this:
- * {{{
- * class SimplePipelineTest extends PipelineSpec {
- *   "A simple pipeline" should "sum integers" in {
- *     runWithContext { sc =>
- *       sc.parallelize(Seq(1, 2, 3)).sum should containSingleValue (6)
- *     }
- *   }
- * }
- * }}}
- */
-trait PipelineSpec extends FlatSpec with Matchers with SCollectionMatcher {
-
-  /**
-   * Test pipeline components with a [[ScioContext]].
-   * @param fn code that tests the components and verifies the result
-   *
-   * {{{
-   * runWithContext { sc =>
-   *   sc.parallelize(Seq(1, 2, 3)).sum should containSingleValue (6)
-   * }
-   * }}}
-   */
-  def runWithContext[T](fn: ScioContext => T): ScioResult = {
-    val sc = ScioContext.forTest()
-    fn(sc)
-    sc.close()
-  }
-
-  /**
-   * Test pipeline components with in-memory data.
-   *
-   * Input data is passed to `fn` as an [[SCollection]] and the result [[SCollection]] from `fn`
-   * is extracted and to be verified.
-   *
-   * @param data input data
-   * @param fn transform to be tested
-   * @return output data
-   *
-   * {{{
-   * runWithData(Seq(1, 2, 3)) { p =>
-   *   p.sum
-   * } should equal (Seq(6))
-   * }}}
-   */
-  def runWithData[T: ClassTag, U: ClassTag](data: Iterable[T])
-                                           (fn: SCollection[T] => SCollection[U]): Seq[U] = {
-    runWithLocalOutput { sc => fn(sc.parallelize(data)) }
-  }
-
-  /**
-   * Test pipeline components with in-memory data.
-   *
-   * Input data is passed to `fn` as [[SCollection]]s and the result [[SCollection]] from `fn` is
-   * extracted and to be verified.
-   *
-   * @param data1 input data
-   * @param data2 input data
-   * @param fn transform to be tested
-   * @return output data
-   */
-  def runWithData[T1: ClassTag, T2: ClassTag, U: ClassTag]
-  (data1: Iterable[T1], data2: Iterable[T2])
-  (fn: (SCollection[T1], SCollection[T2]) => SCollection[U]): Seq[U] = {
-    runWithLocalOutput { sc =>
-      fn(sc.parallelize(data1), sc.parallelize(data2))
-    }
-  }
-
-  /**
-   * Test pipeline components with in-memory data.
-   *
-   * Input data is passed to `fn` as [[SCollection]]s and the result [[SCollection]] from `fn` is
-   * extracted and to be verified.
-   *
-   * @param data1 input data
-   * @param data2 input data
-   * @param data3 input data
-   * @param fn transform to be tested
-   * @return output data
-   */
-  def runWithData[T1: ClassTag, T2: ClassTag, T3: ClassTag, U: ClassTag]
-  (data1: Iterable[T1], data2: Iterable[T2], data3: Iterable[T3])
-  (fn: (SCollection[T1], SCollection[T2], SCollection[T3]) => SCollection[U]): Seq[U] = {
-    runWithLocalOutput { sc =>
-      fn(sc.parallelize(data1), sc.parallelize(data2), sc.parallelize(data3))
-    }
-  }
-
-  /**
-   * Test pipeline components with in-memory data.
-   *
-   * Input data is passed to `fn` as [[SCollection]]s and the result [[SCollection]] from `fn` is
-   * extracted and to be verified.
-   *
-   * @param data1 input data
-   * @param data2 input data
-   * @param data3 input data
-   * @param data4 input data
-   * @param fn transform to be tested
-   * @return output data
-   */
-  def runWithData[T1: ClassTag, T2: ClassTag, T3: ClassTag, T4: ClassTag, U: ClassTag]
-  (data1: Iterable[T1], data2: Iterable[T2], data3: Iterable[T3], data4: Iterable[T4])
-  (fn: (SCollection[T1], SCollection[T2], SCollection[T3], SCollection[T4]) => SCollection[U])
-  : Seq[U] = {
-    runWithLocalOutput { sc =>
-      fn(sc.parallelize(data1), sc.parallelize(data2), sc.parallelize(data3), sc.parallelize(data4))
-    }
-  }
-
-  private def runWithLocalOutput[U](fn: ScioContext => SCollection[U]): Seq[U] = {
-    val sc = ScioContext()
-    val f = fn(sc).materialize
-    sc.close()
-    f.waitForResult().value.toSeq
-  }
-
-}
+  * Trait for unit testing pipelines.
+  *
+  * A simple test might look like this:
+  * {{{
+  * class SimplePipelineTest extends PipelineSpec {
+  *   "A simple pipeline" should "sum integers" in {
+  *     runWithContext { sc =>
+  *       sc.parallelize(Seq(1, 2, 3)).sum should containSingleValue (6)
+  *     }
+  *   }
+  * }
+  * }}}
+  */
+trait PipelineSpec extends FlatSpec with Matchers with PipelineSpecHelper
