efe12ca86dab52d75dd94f72197015e5e9afd018
Rafal Wojdyla <rav@spotify.com>
2016-09-16 17:45:21 -0400

    Add serialization cycle on actual on tests
    Dataflow testing framework does serde cycle on expected values which
    in case of TableRow/TableRowJsonCoder/Jackson from BigQuery may lead
    to inconsistency of Long/Integer:
     * http://stackoverflow.com/questions/33622227/dataflow-mixing-integer-long-types
     * http://stackoverflow.com/questions/7316689/forcing-jackson-to-deserialize-to-specific-primitive-type

--- scio-test/src/main/scala/com/spotify/scio/testing/SCollectionMatchers.scala
+++ scio-test/src/main/scala/com/spotify/scio/testing/SCollectionMatchers.scala
@@ -21,6 +21,7 @@ import java.lang.{Iterable => JIterable}
 
 import com.google.cloud.dataflow.sdk.testing.DataflowAssert
 import com.google.cloud.dataflow.sdk.transforms.SerializableFunction
+import com.google.cloud.dataflow.sdk.util.CoderUtils
 import com.spotify.scio.util.ClosureCleaner
 import com.spotify.scio.values.SCollection
 import org.scalatest.matchers.{MatchResult, Matcher}
@@ -37,15 +38,24 @@ trait SCollectionMatchers {
     MatchResult(r, "", "")
   }
 
-  def containInAnyOrder[T](value: Iterable[T])
-  : Matcher[SCollection[T]] = new Matcher[SCollection[T]] {
-    override def apply(left: SCollection[T]): MatchResult =
-      m(() => DataflowAssert.that(left.internal).containsInAnyOrder(value.asJava))
+  // Due to  https://github.com/GoogleCloudPlatform/DataflowJavaSDK/issues/434
+  // SerDe cycle on each element to keep consistent with values on the expected side
+  private def serDeCycle[T: ClassTag](scollection: SCollection[T]): SCollection[T] = {
+    val coder = scollection.internal.getCoder
+    scollection
+      .map(e => CoderUtils.decodeFromByteArray(coder, CoderUtils.encodeToByteArray(coder, e)))
   }
 
-  def containSingleValue[T](value: T): Matcher[SCollection[T]] = new Matcher[SCollection[T]] {
+  def containInAnyOrder[T: ClassTag](value: Iterable[T])
+  : Matcher[SCollection[T]] = new Matcher[SCollection[T]] {
     override def apply(left: SCollection[T]): MatchResult =
-      m(() => DataflowAssert.thatSingleton(left.internal).isEqualTo(value))
+      m(() => DataflowAssert.that(serDeCycle(left).internal).containsInAnyOrder(value.asJava))
+  }
+
+  def containSingleValue[T: ClassTag](value: T)
+  : Matcher[SCollection[T]] = new Matcher[SCollection[T]] {
+    override def apply(left: SCollection[T]): MatchResult =
+      m(() => DataflowAssert.thatSingleton(serDeCycle(left).internal).isEqualTo(value))
   }
 
   val beEmpty = new Matcher[SCollection[_]] {
@@ -70,20 +80,20 @@ trait SCollectionMatchers {
   def equalMapOf[K: ClassTag, V: ClassTag](value: Map[K, V])
   : Matcher[SCollection[(K, V)]] = new Matcher[SCollection[(K, V)]] {
     override def apply(left: SCollection[(K, V)]): MatchResult = {
-      m(() => DataflowAssert.thatMap(left.toKV.internal).isEqualTo(value.asJava))
+      m(() => DataflowAssert.thatMap(serDeCycle(left).toKV.internal).isEqualTo(value.asJava))
     }
   }
 
   def notEqualMapOf[K: ClassTag, V: ClassTag](value: Map[K, V])
   : Matcher[SCollection[(K, V)]] = new Matcher[SCollection[(K, V)]] {
     override def apply(left: SCollection[(K, V)]): MatchResult = {
-      m(() => DataflowAssert.thatMap(left.toKV.internal).notEqualTo(value.asJava))
+      m(() => DataflowAssert.thatMap(serDeCycle(left).toKV.internal).notEqualTo(value.asJava))
     }
   }
 
   // TODO: investigate why multi-map doesn't work
 
-  def satisfy[T](predicate: Iterable[T] => Boolean)
+  def satisfy[T: ClassTag](predicate: Iterable[T] => Boolean)
   : Matcher[SCollection[T]] = new Matcher[SCollection[T]] {
     override def apply(left: SCollection[T]): MatchResult = {
       val f = ClosureCleaner(predicate)
@@ -93,12 +103,14 @@ trait SCollectionMatchers {
           null
         }
       }
-      m(() => DataflowAssert.that(left.internal).satisfies(g))
+      m(() => DataflowAssert.that(serDeCycle(left).internal).satisfies(g))
     }
   }
 
-  def forAll[T](predicate: T => Boolean): Matcher[SCollection[T]] = satisfy(_.forall(predicate))
+  def forAll[T: ClassTag](predicate: T => Boolean): Matcher[SCollection[T]] =
+    satisfy(_.forall(predicate))
 
-  def exist[T](predicate: T => Boolean): Matcher[SCollection[T]] = satisfy(_.exists(predicate))
+  def exist[T: ClassTag](predicate: T => Boolean): Matcher[SCollection[T]] =
+    satisfy(_.exists(predicate))
 
 }
