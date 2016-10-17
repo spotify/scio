321abd7f190e487d6d63a54db80e061cf7fe76ab
Neville Li <neville@spotify.com>
2016-10-17 14:35:12 -0400

    add BigQuery utilities for Date, Time, DateTime

--- scio-bigquery/src/main/scala/com/spotify/scio/bigquery/package.scala
+++ scio-bigquery/src/main/scala/com/spotify/scio/bigquery/package.scala
@@ -18,11 +18,10 @@
 package com.spotify.scio
 
 import com.google.api.services.bigquery.model.{TableRow => GTableRow}
-import org.joda.time.Instant
-import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatterBuilder}
+import org.joda.time.{Instant, LocalDate, LocalDateTime, LocalTime}
 
 import scala.collection.JavaConverters._
-import scala.util.{Try, Failure}
 
 /**
  * Main package for BigQuery APIs. Import all.
@@ -80,50 +79,82 @@ package object bigquery {
 
   }
 
-  /** Utility for bigQuery time stamps. */
+  /** Utility for BigQuery TIMESTAMP type. */
   object Timestamp {
 
-    private val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS").withZoneUTC()
+    // FIXME: verify that these match BigQuery specification
+    // YYYY-[M]M-[D]D[ [H]H:[M]M:[S]S[.DDDDDD]][time zone]
+    private val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSS ZZZ")
+    private val parser = new DateTimeFormatterBuilder()
+      .append(DateTimeFormat.forPattern("yyyy-MM-dd"))
+      .appendOptional(new DateTimeFormatterBuilder()
+        .append(DateTimeFormat.forPattern(" HH:mm:ss").getParser)
+        .appendOptional(DateTimeFormat.forPattern(".SSSSSS").getParser)
+        .toParser)
+      .appendOptional(new DateTimeFormatterBuilder()
+        .append(null, Array(" ZZZ", "ZZ").map(p => DateTimeFormat.forPattern(p).getParser))
+        .toParser)
+      .toFormatter
+      .withZoneUTC()
 
-    /** Convert Instant to BigQuery time stamp string. */
-    def apply(instant: Instant): String = formatter.print(instant) + " UTC"
+    /** Convert Instant to BigQuery TIMESTAMP string. */
+    def apply(instant: Instant): String = formatter.print(instant)
 
-    /** Convert millisecond instant to BigQuery time stamp string. */
-    def apply(instant: Long): String = formatter.print(instant) + " UTC"
+    /** Convert millisecond instant to BigQuery TIMESTAMP string. */
+    def apply(instant: Long): String = formatter.print(instant)
 
-    private val hasMillisRx = """\.[0-9]+$""".r
+    /** Convert BigQuery TIMESTAMP string to Instant. */
+    def parse(timestamp: String): Instant = parser.parseDateTime(timestamp).toInstant
 
-    private def tryParse(
-      timestamp: String,
-      priorException: Option[Throwable] = None
-    ): Try[Instant] = {
-      // try to parse with DateTime formatter; if failure, check timestamp
-      // for missing fractional seconds and potentially try again
-      Try(formatter.parseDateTime(timestamp).toInstant).recoverWith {
-        case _ if priorException.isDefined =>
-          // Pass original exception for the unmodified timestamp, since its error
-          // string will reflect the raw timestamp in the user's data
-          Failure(priorException.get)
+  }
 
-        case ex: IllegalArgumentException if hasMillisRx.findFirstIn(timestamp).isEmpty =>
-          // Failure due to missing fractional seconds can be retried,
-          // given that we've already checked for earlier failures.  Pass the
-          // current exception to be thrown if we fail again, in which case it
-          // likely reflects some failure other than missing fractional seconds.
-          tryParse(timestamp.stripSuffix(".") + ".000", Some(ex))
+  /** Utility for BigQuery DATE type. */
+  object Date {
+    // YYYY-[M]M-[D]D
+    private val formatter = DateTimeFormat.forPattern("yyyy-MM-dd").withZoneUTC()
 
-        case ex =>
-          // Unknown cause, so get out of here
-          Failure(ex)
-      }
-    }
+    /** Convert LocalDate to BigQuery DATE string. */
+    def apply(date: LocalDate): String = formatter.print(date)
 
-    /** Convert BigQuery time stamp string to Instant. */
-    def parse(timestamp: String): Instant = {
-      // Erase trailing ' UTC' before parsing
-      tryParse(timestamp.stripSuffix(" UTC")).get
-    }
+    /** Convert BigQuery DATE string to LocalDate. */
+    def parse(date: String): LocalDate = LocalDate.parse(date, formatter)
+  }
 
+  /** Utility for BigQuery TIME type. */
+  object Time {
+    // [H]H:[M]M:[S]S[.DDDDDD]
+    private val formatter = DateTimeFormat.forPattern("HH:mm:ss.SSSSSS").withZoneUTC()
+    private val parser = new DateTimeFormatterBuilder()
+      .append(DateTimeFormat.forPattern("HH:mm:ss").getParser)
+      .appendOptional(DateTimeFormat.forPattern(".SSSSSS").getParser)
+      .toFormatter
+      .withZoneUTC()
+
+    /** Convert LocalTime to BigQuery TIME string. */
+    def apply(time: LocalTime): String = formatter.print(time)
+
+    /** Convert BigQuery TIME string to LocalTime. */
+    def parse(time: String): LocalTime = parser.parseLocalTime(time)
+  }
+
+  /** Utility for BigQuery DATETIME type. */
+  object DateTime {
+    // YYYY-[M]M-[D]D[ [H]H:[M]M:[S]S[.DDDDDD]]
+    private val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+    private val parser = new DateTimeFormatterBuilder()
+      .append(DateTimeFormat.forPattern("yyyy-MM-dd"))
+      .appendOptional(new DateTimeFormatterBuilder()
+        .append(DateTimeFormat.forPattern(" HH:mm:ss").getParser)
+        .appendOptional(DateTimeFormat.forPattern(".SSSSSS").getParser)
+        .toParser)
+      .toFormatter
+      .withZoneUTC()
+
+    /** Convert LocalDateTime to BigQuery DATETIME string. */
+    def apply(datetime: LocalDateTime): String = formatter.print(datetime)
+
+    /** Convert BigQuery DATETIME string to LocalDateTime. */
+    def parse(datetime: String): LocalDateTime = parser.parseLocalDateTime(datetime)
   }
 
 }
