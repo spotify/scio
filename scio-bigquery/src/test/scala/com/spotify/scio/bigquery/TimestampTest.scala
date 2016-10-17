4f0c787a4ffefe8d88446387985febeb6206876a
Neville Li <neville@spotify.com>
2016-10-17 14:35:12 -0400

    add BigQuery utilities for Date, Time, DateTime

--- scio-bigquery/src/test/scala/com/spotify/scio/bigquery/TimestampTest.scala
+++ scio-bigquery/src/test/scala/com/spotify/scio/bigquery/TimestampTest.scala
@@ -17,27 +17,64 @@
 
 package com.spotify.scio.bigquery
 
-import org.joda.time.{Instant, DateTime, DateTimeZone}
+import org.joda.time._
 import org.joda.time.format.DateTimeFormat
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 
 class TimestampTest extends FlatSpec with Matchers {
 
   "Timestamp" should "round trip Instant" in {
     val now = Instant.now()
-    Timestamp.parse(Timestamp(now)) shouldBe now
+    Timestamp.parse(Timestamp(now)) should equal (now)
   }
 
   it should "round trip Long" in {
     val now = Instant.now()
-    Timestamp.parse(Timestamp(now.getMillis)) shouldBe now
+    Timestamp.parse(Timestamp(now.getMillis)) should equal (now)
   }
 
-  it should "correctly parse timestamps with no fractional seconds" in {
-    val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+  it should "parse different formats" in {
+    val t = Timestamp.parse("2016-01-01 00:00:00.000000 UTC")
+    Timestamp.parse("2016-01-01 00:00:00.000000 UTC") should equal (t)
+    Timestamp.parse("2016-01-01 08:00:00.000000+08:00") should equal (t)
+    Timestamp.parse("2016-01-01 08:00:00.000000+0800") should equal (t)
+    Timestamp.parse("2016-01-01 08:00:00.000000+08") should equal (t)
+    Timestamp.parse("2016-01-01 00:00:00.000 UTC") should equal (t)
+    Timestamp.parse("2016-01-01 00:00:00.000") should equal (t)
+    Timestamp.parse("2016-01-01 00:00:00 UTC") should equal (t)
+    Timestamp.parse("2016-01-01 00:00:00") should equal (t)
+    Timestamp.parse("2016-01-01 UTC") should equal (t)
+    Timestamp.parse("2016-01-01") should equal (t)
+  }
 
-    val now = new DateTime(DateTimeZone.UTC)
-    Timestamp.parse(formatter.print(now)) shouldBe now.withMillisOfSecond(0)
+  "Date" should "round trip LocalDate" in {
+    val now = LocalDate.now()
+    Date.parse(Date(now)) should equal (now)
+  }
+
+  "Time" should "round trip LocalTime" in {
+    val now = LocalTime.now()
+    Time.parse(Time(now)) should equal (now)
+  }
+
+  it should "parse different formats" in {
+    val t = Time.parse("00:00:00.000000")
+    Time.parse("00:00:00.000000") should equal (t)
+    Time.parse("00:00:00.000") should equal (t)
+    Time.parse("00:00:00") should equal (t)
+  }
+
+  "DateTime" should "round trip LocalDateTime" in {
+    val now = LocalDateTime.now()
+    DateTime.parse(DateTime(now)) should equal (now)
+  }
+
+  it should "parse different formats" in {
+    val t = DateTime.parse("2016-01-01 00:00:00.000000")
+    DateTime.parse("2016-01-01 00:00:00.000000") should equal (t)
+    DateTime.parse("2016-01-01 00:00:00.000") should equal (t)
+    DateTime.parse("2016-01-01 00:00:00") should equal (t)
+    DateTime.parse("2016-01-01") should equal (t)
   }
 
 }
