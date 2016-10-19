98945300ff48e72ec85149b736654d67f01eb536
Andrew Martin <amartin@spotify.com>
2016-10-19 14:06:51 -0400

    Use path.toString to get full path

--- scio-hdfs/src/main/scala/com/spotify/scio/hdfs/HdfsFileStorage.scala
+++ scio-hdfs/src/main/scala/com/spotify/scio/hdfs/HdfsFileStorage.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.spotify.scio.hdfs
 
 import java.io.{File, InputStream}
@@ -29,7 +46,7 @@ private class HdfsFileStorage(protected val path: String) extends FileStorage {
 
     fs
       .listStatus(new org.apache.hadoop.fs.Path(path), pathFilter)
-      .map(status => new File(path + "/" + status.getPath.getName).toPath).toSeq
+      .map(status => new File(status.getPath.toString).toPath).toSeq
   }
 
   override protected def getObjectInputStream(path: Path): InputStream = {
