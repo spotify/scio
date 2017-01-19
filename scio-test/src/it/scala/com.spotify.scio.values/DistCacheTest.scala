247c50eba1349a2fe648e334c698276745150827
Andrew Martin <amartin@spotify.com>
2017-01-19 13:27:07 -0500

    Move cache reading logic to runWithDistCache

--- scio-test/src/it/scala/com.spotify.scio.values/DistCacheTest.scala
+++ scio-test/src/it/scala/com.spotify.scio.values/DistCacheTest.scala
@@ -6,7 +6,7 @@ import com.google.cloud.dataflow.sdk.util.GcsUtil.GcsUtilFactory
 import com.google.cloud.dataflow.sdk.util.MimeTypes
 import com.google.cloud.dataflow.sdk.util.gcsfs.GcsPath
 import com.spotify.scio.{ScioContext, ScioResult}
-import com.spotify.scio.testing.{ITPipelineSpec, PipelineSpec}
+import com.spotify.scio.testing.PipelineSpec
 
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
@@ -17,11 +17,7 @@ class DistCacheTest extends PipelineSpec {
   val cacheData = Seq("name1", "name2")
 
   "GCS DistCache" should "work" in {
-    runWithDistCache(cacheData, distCacheUri) { sc =>
-      val names = sc.distCache(distCacheUri) { f =>
-        scala.io.Source.fromFile(f).getLines().toList
-      }
-
+    runWithDistCache(cacheData, distCacheUri) { (sc, names) =>
       val p = sc.parallelize(0 to 1)
         .map(i => (i, names()(i)))
 
@@ -30,15 +26,18 @@ class DistCacheTest extends PipelineSpec {
   }
 
   def runWithDistCache[T: ClassTag](data: Iterable[String], path: String)
-                                                  (fn: ScioContext => T)
+                                   (fn: (ScioContext, DistCache[List[String]]) => T)
   : ScioResult = {
     val sc = ScioContext()
+    val cache = sc.distCache(distCacheUri) { f =>
+      scala.io.Source.fromFile(f).getLines().toList
+    }
     val gcsUtil = new GcsUtilFactory().create(sc.options)
     try {
       val channel = gcsUtil.create(GcsPath.fromUri(path), MimeTypes.TEXT)
       channel.write(ByteBuffer.wrap(data.mkString("\n").getBytes))
       channel.close()
-      fn(sc)
+      fn(sc, cache)
       sc.close()
     }
     finally {
