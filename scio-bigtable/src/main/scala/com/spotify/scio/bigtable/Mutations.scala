9bc411673c6c77196d1d98ae2763b282cbeef40e
Neville Li <neville@spotify.com>
2017-02-13 16:39:21 -0500

    add convenience implicits to scio-bigtable

---
+++ scio-bigtable/src/main/scala/com/spotify/scio/bigtable/Mutations.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.bigtable
+
+import java.util.concurrent.TimeUnit
+
+import com.google.bigtable.v2.Mutation.{DeleteFromColumn, DeleteFromFamily, DeleteFromRow, SetCell}
+import com.google.bigtable.v2._
+import com.google.protobuf.ByteString
+
+/** Helper methods for [[Mutation]]. */
+object Mutations {
+
+  def newSetCell(familyName: String, columnQualifier: ByteString, value: ByteString): Mutation =
+    Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+        .setFamilyName(familyName)
+        .setColumnQualifier(columnQualifier)
+        .setValue(value)
+        .setTimestampMicros(TimeUnit.MILLISECONDS.toMicros(System.currentTimeMillis())))
+      .build()
+
+  def newSetCell(familyName: String, columnQualifier: ByteString, value: ByteString,
+                 timestampMicros: Long): Mutation =
+    Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+        .setFamilyName(familyName)
+        .setColumnQualifier(columnQualifier)
+        .setValue(value)
+        .setTimestampMicros(timestampMicros))
+      .build()
+
+  def newDeleteFromColumn(familyName: String, columnQualifier: ByteString,
+                          startTimestampMicros: Long, endTimestampMicros: Long): Mutation =
+    Mutation.newBuilder()
+      .setDeleteFromColumn(DeleteFromColumn.newBuilder()
+        .setFamilyName(familyName)
+        .setColumnQualifier(columnQualifier)
+        .setTimeRange(TimestampRange.newBuilder()
+          .setStartTimestampMicros(startTimestampMicros)
+          .setEndTimestampMicros(endTimestampMicros)))
+      .build()
+
+  def newDeleteFromFamily(familyName: String): Mutation =
+    Mutation.newBuilder()
+      .setDeleteFromFamily(DeleteFromFamily.newBuilder()
+        .setFamilyName(familyName))
+      .build()
+
+  def newDeleteFromRow: Mutation =
+    Mutation.newBuilder().setDeleteFromRow(DeleteFromRow.getDefaultInstance).build()
+
+}
---
+++ scio-bigtable/src/main/scala/com/spotify/scio/bigtable/Rows.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.bigtable
+
+import com.google.bigtable.v2.{Cell, Column, Family, Row}
+import com.google.protobuf.ByteString
+
+/** Helper methods for [[Row]]. */
+object Rows {
+
+  private def newCell(value: ByteString): Cell = Cell.newBuilder().setValue(value).build()
+
+  private def newCell(value: ByteString, timestampMicros: Long): Cell =
+    Cell.newBuilder().setValue(value).setTimestampMicros(timestampMicros).build()
+
+  private def newRow(key: ByteString, familyName: String, columnQualifier: ByteString,
+                     cell: Cell): Row =
+    Row.newBuilder()
+      .setKey(key)
+      .addFamilies(Family.newBuilder()
+        .setName(familyName)
+        .addColumns(Column.newBuilder()
+          .setQualifier(columnQualifier)
+          .addCells(cell)))
+      .build()
+
+  def newRow(key: ByteString, familyName: String, columnQualifier: ByteString,
+             value: ByteString): Row =
+    newRow(key, familyName, columnQualifier, newCell(value))
+
+  def newRow(key: ByteString, familyName: String, columnQualifier: ByteString,
+             value: ByteString, timestampMicros: Long): Row =
+    newRow(key, familyName, columnQualifier, newCell(value, timestampMicros))
+
+}
