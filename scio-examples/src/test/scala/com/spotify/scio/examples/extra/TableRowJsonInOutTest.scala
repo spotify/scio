8299a69c6cc939c6990a310f1f51f2b8ee908c09
Andrew Martin <andrewsmartin.mg@gmail.com>
2017-02-14 10:01:26 -0500

    Add missing tests for extra examples. (#445)
    * Add missing tests for extra examples.
    
    * Make sure no exception thrown in AccumulatorExampleTest

---
+++ scio-examples/src/test/scala/com/spotify/scio/examples/extra/TableRowJsonInOutTest.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.examples.extra
+
+import com.spotify.scio.bigquery.TableRow
+import com.spotify.scio.testing.{JobTest, PipelineSpec, TableRowJsonIO}
+
+class TableRowJsonInOutTest extends PipelineSpec {
+
+  val input = Seq(
+    TableRow("field1" -> "str1", "field2" -> 100),
+    TableRow("field1" -> "str2", "field2" -> 200)
+  )
+
+  "TableRowJsonInOut" should "work" in {
+    JobTest[com.spotify.scio.examples.extra.TableRowJsonInOut.type]
+      .args("--input=in.json", "--output=out.json")
+      .input(TableRowJsonIO("in.json"), input)
+      .output(TableRowJsonIO("out.json"))(_ should containInAnyOrder(input))
+      .run()
+  }
+
+}
