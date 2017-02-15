108816098e7d71637fe4fa520d53fe5478c9d1e9
Rafal Wojdyla <rav@spotify.com>
2017-02-15 16:41:17 -0500

    Fix #449. Warn on scala.App

--- scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
+++ scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
@@ -170,6 +170,16 @@ class ScioContext private[scio] (val options: PipelineOptions,
     o.setScioVersion(scioVersion)
   }
 
+  {
+    // Check if running within scala.App. See https://github.com/spotify/scio/issues/449
+    if (Thread.currentThread()
+      .getStackTrace.toList.map(_.getClassName.split('$').head)
+      .exists(_.equals(classOf[App].getName))) {
+      logger.warn(
+        "Applications defined within scala.App might not work properly. Please use main method!")
+    }
+  }
+
   private[scio] val testId: Option[String] =
     Try(optionsAs[ApplicationNameOptions]).toOption.flatMap { o =>
       if (JobTest.isTestId(o.getAppName)) {
