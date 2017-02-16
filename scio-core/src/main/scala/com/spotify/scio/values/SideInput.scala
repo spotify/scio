7e2e89c5adee47a53322935628bbae2f3428ab60
Neville Li <neville@spotify.com>
2017-02-15 22:51:26 -0500

    make side input maps lazy #453

--- scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
@@ -24,7 +24,6 @@ import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.values.PCollectionView
 
 import scala.collection.JavaConverters._
-import scala.collection.breakOut
 
 /** Encapsulate an SCollection when it is being used as a side input. */
 trait SideInput[T] extends Serializable {
@@ -35,6 +34,7 @@ trait SideInput[T] extends Serializable {
 
   private[values] def getCache[I, O](context: DoFn[I, O]#ProcessContext): T = {
     if (cache == null) {
+      // this is called once per DoFn instance, which means once per CPU core on Dataflow
       cache = get(context)
     }
     cache
@@ -64,17 +64,49 @@ private[values] class IterableSideInput[T](val view: PCollectionView[JIterable[T
 private[values] class MapSideInput[K, V](val view: PCollectionView[JMap[K, V]])
   extends SideInput[Map[K, V]] {
   override def get[I, O](context: DoFn[I, O]#ProcessContext): Map[K, V] =
-    context.sideInput(view).asScala.toMap
+    new SideInputMap(context.sideInput(view))
 }
 
 private[values] class MultiMapSideInput[K, V](val view: PCollectionView[JMap[K, JIterable[V]]])
   extends SideInput[Map[K, Iterable[V]]] {
   override def get[I, O](context: DoFn[I, O]#ProcessContext): Map[K, Iterable[V]] =
-    context.sideInput(view).asScala.map(kv => (kv._1, kv._2.asScala))(breakOut)
+    new SideInputMultiMap(context.sideInput(view))
 }
 
 /** Encapsulate context of one or more [[SideInput]]s in an [[SCollectionWithSideInput]]. */
 class SideInputContext[T] private[scio] (val context: DoFn[T, AnyRef]#ProcessContext) {
   /** Extract the value of a given [[SideInput]]. */
   def apply[S](side: SideInput[S]): S = side.getCache(context)
+}
+
+// Immutable wrapper for j.u.Map[A, B] because .asScala returns a s.c.mutable.Map
+private class SideInputMap[A, B](self: JMap[A, B]) extends Map[A, B] {
+
+  // make eager copies when necessary
+  // scalastyle:off method.name
+  override def +[B1 >: B](kv: (A, B1)): Map[A, B1] = self.asScala.toMap + kv
+  override def -(key: A): Map[A, B] = self.asScala.toMap - key
+  // scalastyle:on method.name
+
+  // lazy transform underlying j.l.Map
+  override def get(key: A): Option[B] = Option(self.get(key))
+  override def iterator: Iterator[(A, B)] = self.asScala.iterator
+
+}
+
+// Immutable wrapper for j.u.Map[A, j.l.terable[B]] because .asScala returns a s.c.mutable.Map
+private class SideInputMultiMap[A, B](self: JMap[A, JIterable[B]]) extends Map[A, Iterable[B]] {
+
+  // make eager copies when necessary
+  // scalastyle:off method.name
+  override def +[B1 >: Iterable[B]](kv: (A, B1)): Map[A, B1] =
+    self.asScala.mapValues(_.asScala).toMap + kv
+  override def -(key: A): Map[A, Iterable[B]] = self.asScala.mapValues(_.asScala).toMap - key
+  // scalastyle:on method.name
+
+  // lazy transform underlying j.l.Map
+  override def get(key: A): Option[Iterable[B]] = Option(self.get(key)).map(_.asScala)
+  override def iterator: Iterator[(A, Iterable[B])] =
+    self.asScala.iterator.map(kv => (kv._1, kv._2.asScala))
+
 }
