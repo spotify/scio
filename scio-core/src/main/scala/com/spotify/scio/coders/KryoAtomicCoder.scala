c7032757a628a296afad11f519cb1185db9a2411
Neville Li <neville@spotify.com>
2017-03-04 19:31:37 -0500

    add buffered prefix input and output steams

--- scio-core/src/main/scala/com/spotify/scio/coders/KryoAtomicCoder.scala
+++ scio-core/src/main/scala/com/spotify/scio/coders/KryoAtomicCoder.scala
@@ -17,7 +17,8 @@
 
 package com.spotify.scio.coders
 
-import java.io.{ByteArrayOutputStream, IOException, InputStream, OutputStream}
+import java.io.{ByteArrayInputStream, InputStream, OutputStream}
+import java.nio.ByteBuffer
 
 import com.google.common.io.{ByteStreams, CountingOutputStream}
 import com.google.common.reflect.ClassPath
@@ -105,13 +106,11 @@ private[scio] class KryoAtomicCoder[T] extends AtomicCoder[T] {
       kryo.get().writeClassAndObject(output, value)
       output.flush()
     } else {
-      val s = new ByteArrayOutputStream()
-      val output = new Output(s)
+      val os = new BufferedPrefixOutputStream(outStream)
+      val output = new Output(os)
       kryo.get().writeClassAndObject(output, value)
       output.flush()
-
-      VarInt.encode(s.size(), outStream)
-      outStream.write(s.toByteArray)
+      os.finish()
     }
   }
 
@@ -119,14 +118,10 @@ private[scio] class KryoAtomicCoder[T] extends AtomicCoder[T] {
     val o = if (context.isWholeStream) {
       kryo.get().readClassAndObject(new Input(inStream))
     } else {
-      val length = VarInt.decodeInt(inStream)
-      if (length < 0) {
-        throw new IOException("invalid length " + length)
-      }
-
-      val value = Array.ofDim[Byte](length)
-      ByteStreams.readFully(inStream, value)
-      kryo.get().readClassAndObject(new Input(value))
+      val is = new BufferedPrefixInputStream(inStream)
+      val obj = kryo.get().readClassAndObject(new Input(is))
+      is.finish()
+      obj
     }
     o.asInstanceOf[T]
   }
@@ -160,4 +155,107 @@ private[scio] class KryoAtomicCoder[T] extends AtomicCoder[T] {
 
 private[scio] object KryoAtomicCoder {
   def apply[T]: Coder[T] = new KryoAtomicCoder[T]
+}
+
+/**
+ * Buffered output stream that adds length prefix to each buffer block. Useful for large objects
+ * or collections. Based on [[org.apache.beam.sdk.util.BufferedElementCountingOutputStream]].
+ */
+private class BufferedPrefixOutputStream(private val os: OutputStream)
+  extends OutputStream {
+
+  private val buffer = ByteBuffer.allocate(64 * 1024)
+  private var finished = false
+
+  VarInt.encode(-1, os)
+
+  override def write(b: Int): Unit = {
+    require(!finished, "Stream has been finished. Can not add any more data.")
+    if (!buffer.hasRemaining) {
+      outputBuffer()
+    }
+    buffer.put(b.toByte)
+  }
+
+  override def write(b: Array[Byte], off: Int, len: Int): Unit = {
+    require(!finished, "Stream has been finished. Can not add any more data.")
+    if (buffer.remaining() >= len) {
+      buffer.put(b, off, len)
+    } else {
+      outputBuffer()
+      if (len < buffer.capacity()) {
+        buffer.put(b, off, len)
+      } else {
+        VarInt.encode(len, os)
+        os.write(b, off, len)
+      }
+    }
+  }
+
+  override def flush(): Unit = if (!finished) {
+    outputBuffer()
+    os.flush()
+  }
+
+  def finish(): Unit = if (!finished) {
+    flush()
+    VarInt.encode(0, os)
+    finished = true
+  }
+
+  private def outputBuffer(): Unit = if (buffer.position() > 0) {
+    VarInt.encode(buffer.position(), os)
+    os.write(buffer.array(), buffer.arrayOffset(), buffer.position())
+    buffer.clear()
+  }
+
+}
+
+/** Counterpart for [[BufferedPrefixOutputStream]]. */
+private class BufferedPrefixInputStream(private val is: InputStream) extends InputStream {
+
+  require(VarInt.decodeInt(is) == -1, "Invalid input stream")
+  inputBuffer()
+
+  private var buffer: Array[Byte] = _
+  private var bais: ByteArrayInputStream = _
+  private var finished = false
+
+  override def read(): Int = {
+    inputBuffer()
+    bais.read()
+  }
+
+  override def read(b: Array[Byte], off: Int, len: Int): Int = {
+    var bytesRead = 0
+    var n = 0
+    do {
+      inputBuffer()
+      n = bais.read(b, off + bytesRead, len - bytesRead)
+      if (n > 0) {
+        bytesRead += n
+      }
+    } while (n > 0)
+    bytesRead
+  }
+
+  private def inputBuffer(): Unit = if (!finished) {
+    if (bais == null || bais.available() <= 0) {
+      val len = VarInt.decodeInt(is)
+      if (len == 0) {
+        finished = true
+      } else {
+        if (buffer == null || buffer.length < len) {
+          buffer = Array.ofDim[Byte](math.max(len, 64 * 1024))
+        }
+        is.read(buffer, 0, len)
+        bais = new ByteArrayInputStream(buffer, 0, len)
+      }
+    }
+  }
+
+  def finish(): Unit = if (!finished) {
+    require(VarInt.decodeInt(is) == 0, "Invalid end of input stream")
+  }
+
 }
