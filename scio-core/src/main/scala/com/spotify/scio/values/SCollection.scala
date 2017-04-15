0941ab2aade4f376a8bde276a234d8fa68f38c73
Sam Schlegel <sam.s@discordapp.com>
2017-04-14 17:37:19 -0700

    Remove extra space

--- scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
@@ -1065,7 +1065,7 @@ sealed trait SCollection[T] extends PCollectionWrapper[T] {
    * Save this SCollection as a Pub/Sub topic.
    * @group output
    */
-  def saveAsPubsub(topic: String) : Future[Tap[T]] = {
+  def saveAsPubsub(topic: String): Future[Tap[T]] = {
     if (context.isTest) {
       context.testOut(PubsubIO(topic))(this)
     } else {
