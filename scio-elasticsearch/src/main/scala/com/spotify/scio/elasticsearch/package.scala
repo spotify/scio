d24496a8bd55da9f4fb1aa32c922e326c2b87f0a
Mayumi <mayumi@spotify.com>
2017-05-07 10:52:58 -0400

    Comforming Bound factory to Beam convention to get fields separately

--- scio-elasticsearch/src/main/scala/com/spotify/scio/elasticsearch/package.scala
+++ scio-elasticsearch/src/main/scala/com/spotify/scio/elasticsearch/package.scala
@@ -1,5 +1,6 @@
 package com.spotify.scio
 
+import java.net.InetSocketAddress
 import java.time.Duration
 
 import com.spotify.scio.io.Tap
@@ -15,31 +16,21 @@ import scala.concurrent.Future
   * }}}
   */
 package object elasticsearch extends AnyVal {
-  implicit class ElasticsearchSCollection(val self: SCollection[IndexRequestWrapper]) {
-    /**
-      * Save this SCollection into Elasticsearch. Default flushInterval is set to 1 secnod.
-      * Note that the elements must be of a type 'IndexRequestWrapper'
-      * @param elasticsearchOptions provides clusterName and server endpoints
-      */
-    def saveAsElasticsearch(elasticsearchOptions: ElasticsearchOptions)
-    :Future[Tap[IndexRequestWrapper]] = {
-      self.saveAsCustomOutput("Write to Elasticsearch",
-        ElasticsearchIO.Write.withElasticsearchOptions(elasticsearchOptions))
-    }
+  case class ElasticsearchOptions(clusterName: String, servers: Array[InetSocketAddress])
 
+  implicit class ElasticsearchSCollection(val self: SCollection[IndexRequestWrapper]) {
     /**
       * Save this SCollection into Elasticsearch.
       * Note that the elements must be of a type 'IndexRequestWrapper'
       * @param elasticsearchOptions provides clusterName and server endpoints
-      * @param flushInterval delayed applied to rate limit writes to Elasticsearch cluster
       */
     def saveAsElasticsearch(elasticsearchOptions: ElasticsearchOptions,
-                            flushInterval: Duration)
-    : Future[Tap[IndexRequestWrapper]] = {
+                            flushInterval: Duration = Duration.ofSeconds(1))
+    :Future[Tap[IndexRequestWrapper]] = {
       self.saveAsCustomOutput("Write to Elasticsearch",
-        ElasticsearchIO.Write
-        .withElasticsearchOptions(elasticsearchOptions)
-        .withFlushInterval(flushInterval))
+        ElasticsearchIO.Write.withClusterName(elasticsearchOptions.clusterName)
+        .withServers(elasticsearchOptions.servers))
     }
   }
 }
+
