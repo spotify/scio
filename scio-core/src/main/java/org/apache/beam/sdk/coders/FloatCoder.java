c729fc14bb04918b5460538af04960523421eb48
Neville Li <neville@spotify.com>
2017-05-14 17:34:58 -0400

    fix broken FloatCoder

--- scio-core/src/main/java/org/apache/beam/sdk/coders/FloatCoder.java
+++ scio-core/src/main/java/org/apache/beam/sdk/coders/FloatCoder.java
@@ -1,25 +1,21 @@
 /*
- * Copyright 2016 Spotify AB.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-/* Ported com.google.cloud.dataflow.sdk.coders.DoubleCoder */
-
-package com.spotify.scio.coders;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
+package org.apache.beam.sdk.coders;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -28,18 +24,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UTFDataFormatException;
-
-// package hack
-import org.apache.beam.sdk.coders.AtomicCoder;
-import org.apache.beam.sdk.coders.Coder.Context;
-import org.apache.beam.sdk.coders.CoderException;
+import org.apache.beam.sdk.values.TypeDescriptor;
 
 /**
  * A {@link FloatCoder} encodes {@link Float} values in 4 bytes using Java serialization.
  */
 public class FloatCoder extends AtomicCoder<Float> {
 
-  @JsonCreator
   public static FloatCoder of() {
     return INSTANCE;
   }
@@ -47,20 +38,21 @@ public class FloatCoder extends AtomicCoder<Float> {
   /////////////////////////////////////////////////////////////////////////////
 
   private static final FloatCoder INSTANCE = new FloatCoder();
+  private static final TypeDescriptor<Float> TYPE_DESCRIPTOR = new TypeDescriptor<Float>() {};
 
   private FloatCoder() {}
 
   @Override
-  public void encode(Float value, OutputStream outStream, Context context)
+  public void encode(Float value, OutputStream outStream)
       throws IOException, CoderException {
     if (value == null) {
-      throw new CoderException("cannot encode a null float");
+      throw new CoderException("cannot encode a null Float");
     }
     new DataOutputStream(outStream).writeFloat(value);
   }
 
   @Override
-  public Float decode(InputStream inStream, Context context)
+  public Float decode(InputStream inStream)
       throws IOException, CoderException {
     try {
       return new DataInputStream(inStream).readFloat();
@@ -101,8 +93,13 @@ public class FloatCoder extends AtomicCoder<Float> {
    * @return {@code true}. {@link FloatCoder#getEncodedElementByteSize} returns a constant.
    */
   @Override
-  public boolean isRegisterByteSizeObserverCheap(Float value, Context context) {
+  public boolean isRegisterByteSizeObserverCheap(Float value) {
     return true;
+  }
+
+  @Override
+  public TypeDescriptor<Float> getEncodedTypeDescriptor() {
+    return TYPE_DESCRIPTOR;
   }
 
   /**
@@ -111,7 +108,7 @@ public class FloatCoder extends AtomicCoder<Float> {
    * @return {@code 4}, the byte size of a {@link Float} encoded using Java serialization.
    */
   @Override
-  protected long getEncodedElementByteSize(Float value, Context context)
+  protected long getEncodedElementByteSize(Float value)
       throws Exception {
     if (value == null) {
       throw new CoderException("cannot encode a null Float");
