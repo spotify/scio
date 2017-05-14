49e9f42e60918f047ae9d23214b2fdfaaeb94cdf
Neville Li <neville@spotify.com>
2017-05-14 17:35:40 -0400

    fix breaking BigQuery changes

---
+++ scio-bigquery/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryServicesWrapper.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.beam.sdk.io.gcp.bigquery;
+
+import com.google.api.services.bigquery.model.Table;
+import com.google.api.services.bigquery.model.TableReference;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Wrap {@link BigQueryServices} and expose package private methods.
+ */
+public class BigQueryServicesWrapper {
+
+  private final BigQueryServices bqServices;
+  private final BigQueryOptions bqOptions;
+
+  public BigQueryServicesWrapper(BigQueryOptions bqOptions) {
+    this.bqServices = new BigQueryServicesImpl();
+    this.bqOptions = bqOptions;
+  }
+
+  public void createTable(TableReference ref, TableSchema schema)
+      throws IOException, InterruptedException {
+    Table table = new Table()
+        .setTableReference(ref)
+        .setSchema(schema);
+    bqServices.getDatasetService(bqOptions).createTable(table);
+  }
+
+  public long insertAll(TableReference ref, List<TableRow> rowList)
+      throws IOException, InterruptedException {
+    return bqServices.getDatasetService(bqOptions).insertAll(ref, rowList, null);
+  }
+
+}
