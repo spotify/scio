c8d5e03e5aa6d2032113b8b6919b7330db1d59c4
Rafal Wojdyla <rav@spotify.com>
2017-05-25 16:08:01 -0400

    Add example of checkpoints

---
+++ scio-examples/src/main/scala/com/spotify/scio/examples/extra/CheckpointExample.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.examples.extra
+
+import com.spotify.scio.ContextAndArgs
+import com.spotify.scio.extra.checkpoint._
+
+object CheckpointExample {
+  def main(cmdlineArgs: Array[String]): Unit = {
+    val (sc, args) = ContextAndArgs(cmdlineArgs)
+
+    val words = sc.checkpoint(args("checkpoint") + "-src"){
+      sc.textFile(args("input"))
+        .map(_.trim)
+        .flatMap(_.split("[^a-zA-Z']+").filter(_.nonEmpty))
+      }
+    val count = sc.checkpoint(args("checkpoint") + "-count2")(words.countByValue)
+
+    words.saveAsTextFile(args("output") + "-words")
+    count.max(Ordering.by(_._2)).saveAsTextFile(args("output") + "-max")
+    count.map(t => t._1 + ": " + t._2).saveAsTextFile(args("output"))
+    sc.close()
+  }
+}
