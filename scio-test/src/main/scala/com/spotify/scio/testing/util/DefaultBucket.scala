59c2900a2a2bef6059fba949b0322f0fb2b6bb3a
Andrew Martin <andrewsmartin.mg@gmail.com>
2017-05-31 13:03:55 -0400

    Add default bucket utils. (#642)
    Note: logic copied from https://github.com/apache/beam/pull/2803 and https://github.com/apache/beam/pull/2747

---
+++ scio-test/src/main/scala/com/spotify/scio/testing/util/DefaultBucket.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.testing.util
+
+import java.io.IOException
+import java.nio.file.FileAlreadyExistsException
+
+import com.google.api.client.util.{BackOff => GBackOff, Sleeper => GSleeper}
+import com.google.api.services.cloudresourcemanager.CloudResourceManager
+import com.google.api.services.storage.model.Bucket
+import com.google.cloud.hadoop.util.{ResilientOperation, RetryDeterminer}
+import com.google.common.base.Strings.isNullOrEmpty
+import org.apache.beam.sdk.extensions.gcp.options.{GcpOptions, GcsOptions}
+import org.apache.beam.sdk.options.PipelineOptions
+import org.apache.beam.sdk.util.{BackOff, FluentBackoff, Sleeper}
+import org.apache.beam.sdk.util.gcsfs.GcsPath
+import org.joda.time.Duration
+import org.slf4j.{Logger, LoggerFactory}
+
+object DefaultBucket {
+
+  def tryCreateDefaultBucket(options: PipelineOptions, crmClient: CloudResourceManager): String = {
+    val gcpOptions = options.as(classOf[GcsOptions])
+    val projectId = gcpOptions.getProject
+    require(!isNullOrEmpty(projectId), "--project is a required option.")
+    // Look up the project number, to create a default bucket with a stable
+    // name with no special characters.
+    var projectNumber = 0L
+    try
+      projectNumber = getProjectNumber(projectId, crmClient)
+    catch {
+      case e: IOException =>
+        throw new RuntimeException("Unable to verify project with ID " + projectId, e)
+    }
+    var region = DEFAULT_REGION
+    if (!isNullOrEmpty(gcpOptions.getZone)) region = getRegionFromZone(gcpOptions.getZone)
+    val bucketName = "dataflow-staging-" + region + "-" + projectNumber
+    LOG.info("No staging location provided, attempting to use default bucket: {}", bucketName)
+    val bucket = new Bucket().setName(bucketName).setLocation(region)
+    // Always try to create the bucket before checking access, so that we do not
+    // race with other pipelines that may be attempting to do the same thing.
+    try
+      gcpOptions.getGcsUtil.createBucket(projectId, bucket)
+    catch {
+      case e: FileAlreadyExistsException =>
+        LOG.debug("Bucket '{}'' already exists, verifying access.", bucketName)
+      case e: IOException =>
+        throw new RuntimeException("Unable create default bucket.", e)
+    }
+    // Once the bucket is expected to exist, verify that it is correctly owned
+    // by the project executing the job.
+    try {
+      val owner = gcpOptions.getGcsUtil.bucketOwner(GcsPath.fromComponents(bucketName, ""))
+      require(owner == projectNumber,
+        "Bucket owner does not match the project from --project:" +
+          s" ${owner} vs. ${projectNumber}")
+    } catch {
+      case e: IOException =>
+        throw new RuntimeException("Unable to determine the owner of the default bucket at gs://" +
+          bucketName, e)
+    }
+    "gs://" + bucketName
+  }
+
+  private val BACKOFF_FACTORY = FluentBackoff.DEFAULT.withMaxRetries(3)
+    .withInitialBackoff(Duration.millis(200))
+
+  private val DEFAULT_REGION = "us-central1"
+  private val LOG: Logger = LoggerFactory.getLogger(classOf[GcpOptions.GcpTempLocationFactory])
+
+  private def getProjectNumber(projectId: String, crmClient: CloudResourceManager): Long =
+    getProjectNumber(projectId, crmClient, BACKOFF_FACTORY.backoff(), Sleeper.DEFAULT)
+
+  private def getProjectNumber(projectId: String,
+                               crmClient: CloudResourceManager,
+                               backoff: BackOff,
+                               sleeper: Sleeper): Long = {
+    val getProject = crmClient.projects.get(projectId)
+    try {
+      val project = ResilientOperation.retry(
+        ResilientOperation.getGoogleRequestCallable(getProject),
+        backoff.asInstanceOf[GBackOff],
+        RetryDeterminer.SOCKET_ERRORS,
+        classOf[IOException],
+        sleeper.asInstanceOf[GSleeper])
+      project.getProjectNumber
+    } catch {
+      case e: Exception =>
+        throw new IOException("Unable to get project number", e)
+    }
+  }
+
+  private def getRegionFromZone(zone: String): String = {
+    val zoneParts = zone.split("-")
+    require(zoneParts.length >= 2, s"Invalid zone provided: ${zone}")
+    zoneParts(0) + "-" + zoneParts(1)
+  }
+}
---
+++ scio-test/src/main/scala/com/spotify/scio/testing/util/ItUtils.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.testing.util
+
+import java.util.UUID
+
+import com.google.api.client.http.HttpRequestInitializer
+import com.google.api.services.cloudresourcemanager.CloudResourceManager
+import com.google.auth.Credentials
+import com.google.auth.http.HttpCredentialsAdapter
+import com.google.cloud.hadoop.util.ChainingHttpRequestInitializer
+import com.google.common.collect.ImmutableList
+import com.spotify.scio.bigquery.BigQueryClient
+import org.apache.beam.sdk.extensions.gcp.auth.NullCredentialInitializer
+import org.apache.beam.sdk.extensions.gcp.options.GcpOptions.DefaultProjectFactory
+import org.apache.beam.sdk.extensions.gcp.options.{CloudResourceManagerOptions, GcpOptions, GcsOptions}
+import org.apache.beam.sdk.options.PipelineOptionsFactory
+import org.apache.beam.sdk.util.{GcsUtil, RetryHttpRequestInitializer, Transport}
+
+/** Integration test utilities. */
+private[scio] object ItUtils {
+
+  /** Get project for integration test. */
+  def project: String =
+    if (sys.props(BigQueryClient.PROJECT_KEY) != null) {
+      // this is usually set by CI script
+      sys.props(BigQueryClient.PROJECT_KEY)
+    } else {
+      // fallback to local setting
+      new DefaultProjectFactory().create(null)
+    }
+
+  /** Get [[org.apache.beam.sdk.util.GcsUtil GcsUtil]] for integration test. */
+  def gcsUtil: GcsUtil = {
+    val opts = PipelineOptionsFactory.as(classOf[GcsOptions])
+    opts.setProject(project)
+    opts.getGcsUtil
+  }
+
+  /** Get GCP temp location for integration test. */
+  def gcpTempLocation(prefix: String): String = {
+    val opts = PipelineOptionsFactory.as(classOf[GcpOptions])
+    opts.setProject(project)
+    val bucket = DefaultBucket.tryCreateDefaultBucket(opts,
+      newCloudResourceManagerClient(opts.as(classOf[CloudResourceManagerOptions]))
+    )
+    val uuid = UUID.randomUUID().toString
+    s"$bucket/$prefix-$uuid"
+  }
+
+  private def newCloudResourceManagerClient(options: CloudResourceManagerOptions):
+  CloudResourceManager = {
+    val credentials = options.getGcpCredential()
+    if (credentials == null) {
+      NullCredentialInitializer.throwNullCredentialException()
+    }
+    new CloudResourceManager.Builder(Transport.getTransport(), Transport.getJsonFactory(),
+      chainHttpRequestInitializer(
+        credentials,
+        // Do not log 404. It clutters the output and is possibly even required by the caller.
+        new RetryHttpRequestInitializer(ImmutableList.of(404))))
+      .setApplicationName(options.getAppName())
+      .setGoogleClientRequestInitializer(options.getGoogleApiTrace())
+    .build()
+  }
+
+  private def chainHttpRequestInitializer(credential: Credentials,
+                                          httpRequestInitializer: HttpRequestInitializer):
+  HttpRequestInitializer = {
+    if (credential == null) {
+      new ChainingHttpRequestInitializer(new NullCredentialInitializer(), httpRequestInitializer)
+    } else {
+      new ChainingHttpRequestInitializer(new HttpCredentialsAdapter(credential),
+        httpRequestInitializer);
+    }
+  }
+}
