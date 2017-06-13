cdf5bfb5cc9c2ef5252f53d3bdbd16b3e02f4ca4
Neville Li <neville@spotify.com>
2017-06-13 09:41:52 -0400

    fix sys.props handling for SBT 0.13.15

--- scripts/circleci_test.sh
+++ scripts/circleci_test.sh
@@ -23,8 +23,8 @@ if [ "$CI_PULL_REQUEST" = "" ]; then
   echo "Running test for branch: $CIRCLE_BRANCH"
   JSON_KEY=$(basename $GOOGLE_APPLICATION_CREDENTIALS)
   openssl aes-256-cbc -d -in "$DIR_OF_SCRIPT/$JSON_KEY.enc" -out "$DIR_OF_SCRIPT/$JSON_KEY" -k $ENCRYPTION_KEY
-  "$DIR_OF_SCRIPT/circleci_parallel_run.sh" 'sbt ++$CI_SCALA_VERSION -Dbigquery.project=data-integration-test '"-Dbigquery.secret=$DIR_OF_SCRIPT/$JSON_KEY"' scalastyle coverage test it:test coverageReport coverageAggregate'
+  "$DIR_OF_SCRIPT/circleci_parallel_run.sh" 'sbt -Dbigquery.project=data-integration-test '"-Dbigquery.secret=$DIR_OF_SCRIPT/$JSON_KEY"' ++$CI_SCALA_VERSION scalastyle coverage test it:test coverageReport coverageAggregate'
 else
   echo "Running test for PR: $CI_PULL_REQUEST"
-  "$DIR_OF_SCRIPT/circleci_parallel_run.sh" 'sbt ++$CI_SCALA_VERSION -Dbigquery.project=dummy-project scalastyle coverage test coverageReport coverageAggregate'
+  "$DIR_OF_SCRIPT/circleci_parallel_run.sh" 'sbt -Dbigquery.project=dummy-project ++$CI_SCALA_VERSION scalastyle coverage test coverageReport coverageAggregate'
 fi
