01b180d2a664e40a4726634c7948b6169b93ace7
Rafal Wojdyla <ravwojdyla@gmail.com>
2017-08-11 13:27:58 -0400

    Add example of DoFn usage (#766)

---
+++ scio-examples/src/test/scala/com/spotify/scio/examples/extra/DoFnExampleTest.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.examples.extra
+
+import com.spotify.scio.testing.{PipelineSpec, TextIO}
+
+class DoFnExampleTest extends PipelineSpec {
+
+  private val inData = Seq("a bbb", "cc")
+  private val outData = Seq(1, 3, 2).map(_.toString)
+
+  "DoFnExample" should "work" in {
+    JobTest[DoFnExample.type]
+      .args("--input=in.txt", "--output=out.txt")
+      .input(TextIO("in.txt"), inData)
+      .output(TextIO("out.txt"))(_ should containInAnyOrder (outData))
+      .run()
+  }
+
+}
