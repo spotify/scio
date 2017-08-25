8efd5838285069672d098119357d0a864b385858
Neville Li <neville@spotify.com>
2017-08-25 13:29:29 -0400

    make ArraySemigroup a Monoid

--- scio-core/src/main/scala/com/spotify/scio/package.scala
+++ scio-core/src/main/scala/com/spotify/scio/package.scala
@@ -18,7 +18,7 @@
 package com.spotify
 
 import com.spotify.scio.io.Tap
-import com.twitter.algebird.Semigroup
+import com.twitter.algebird.Monoid
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
@@ -61,21 +61,23 @@ package object scio {
     """version in .+"([^"]+)"""".r.findFirstMatchIn(line).get.group(1)
   }
 
-  /** [[com.twitter.algebird.Semigroup Semigroup]] for `Array[Int]`. */
-  implicit val intArraySg: Semigroup[Array[Int]] = new ArraySemigroup[Int]
+  /** [[com.twitter.algebird.Monoid Monoid]] for `Array[Int]`. */
+  implicit val intArrayMon: Monoid[Array[Int]] = new ArrayMonoid[Int]
 
-  /** [[com.twitter.algebird.Semigroup Semigroup]] for `Array[Long]`. */
-  implicit val longArraySg: Semigroup[Array[Long]] = new ArraySemigroup[Long]
+  /** [[com.twitter.algebird.Monoid Monoid]] for `Array[Long]`. */
+  implicit val longArrayMon: Monoid[Array[Long]] = new ArrayMonoid[Long]
 
-  /** [[com.twitter.algebird.Semigroup Semigroup]] for `Array[Float]`. */
-  implicit val floatArraySg: Semigroup[Array[Float]] = new ArraySemigroup[Float]
+  /** [[com.twitter.algebird.Monoid Monoid]] for `Array[Float]`. */
+  implicit val floatArrayMon: Monoid[Array[Float]] = new ArrayMonoid[Float]
 
-  /** [[com.twitter.algebird.Semigroup Semigroup]] for `Array[Double]`. */
-  implicit val doubleArraySg: Semigroup[Array[Double]] = new ArraySemigroup[Double]
+  /** [[com.twitter.algebird.Monoid Monoid]] for `Array[Double]`. */
+  implicit val doubleArrayMon: Monoid[Array[Double]] = new ArrayMonoid[Double]
 
-  private class ArraySemigroup[@specialized(Int, Long, Float, Double) T : ClassTag : Numeric]
-    extends Semigroup[Array[T]]{
+  private class ArrayMonoid[@specialized(Int, Long, Float, Double) T : ClassTag : Numeric]
+    extends Monoid[Array[T]]{
+
     private val num = implicitly[Numeric[T]]
+
     private def plusI(l: Array[T], r: Array[T]): Array[T] = {
       require(l.length == r.length, "Array lengths do not match")
       import num.mkNumericOps
@@ -86,24 +88,36 @@ package object scio {
       }
       l
     }
+
+    override val zero: Array[T] = Array.empty[T]
     override def plus(l: Array[T], r: Array[T]): Array[T] = {
-      val s = Array.fill[T](l.length)(num.zero)
-      plusI(s, l)
-      plusI(s, r)
-      s
+      if (l.length == 0) {
+        r
+      } else if (r.length == 0) {
+        l
+      } else {
+        val s = Array.fill[T](l.length)(num.zero)
+        plusI(s, l)
+        plusI(s, r)
+        s
+      }
     }
+
     override def sumOption(xs: TraversableOnce[Array[T]]): Option[Array[T]] = {
       var s: Array[T] = null
       val i = xs.toIterator
       while (i.hasNext) {
         val a = i.next()
-        if (s == null) {
-          s = Array.fill[T](a.length)(num.zero)
+        if (a.length > 0) {
+          if (s == null) {
+            s = Array.fill[T](a.length)(num.zero)
+          }
+          plusI(s, a)
         }
-        plusI(s, a)
       }
       Option(s)
     }
+
   }
 
 }
