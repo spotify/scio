8898671b0f792881fe38b03025fca2e041e0c5b0
Filipe Regadas <regadas@spotify.com>
2017-11-16 13:23:17 -0800

    Add PubSub admin helper methods

---
+++ scio-core/src/main/scala/com/spotify/scio/pubsub/PubSubAdmin.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2017 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.pubsub
+
+import java.util.concurrent.Executors
+
+import com.google.pubsub.v1.PublisherGrpc.PublisherBlockingStub
+import com.google.pubsub.v1.SubscriberGrpc.SubscriberBlockingStub
+import com.google.pubsub.v1.{PublisherGrpc, SubscriberGrpc, Subscription, Topic}
+import io.grpc.ManagedChannel
+import io.grpc.auth.ClientAuthInterceptor
+import io.grpc.netty.{GrpcSslContexts, NegotiationType, NettyChannelBuilder}
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubOptions
+
+import scala.util.Try
+
+object PubSubAdmin {
+
+  private object GrpcClient {
+    private def newChannel(pubsubOptions: PubsubOptions): ManagedChannel = {
+      val interceptor =
+        new ClientAuthInterceptor(pubsubOptions.getGcpCredential, Executors.newSingleThreadExecutor)
+      NettyChannelBuilder
+        .forAddress("pubsub.googleapis.com", 443)
+        .negotiationType(NegotiationType.TLS)
+        .sslContext(GrpcSslContexts.forClient.ciphers(null).build)
+        .intercept(interceptor)
+        .build
+    }
+
+    def subscriber[A](pubsubOptions: PubsubOptions)(f: SubscriberBlockingStub => A): Try[A] = {
+      val channel = newChannel(pubsubOptions)
+      val client = SubscriberGrpc.newBlockingStub(channel)
+
+      val result = Try(f(client))
+      channel.shutdownNow()
+      result
+    }
+
+    def publisher[A](pubsubOptions: PubsubOptions)(f: PublisherBlockingStub => A): Try[A] = {
+      val channel = newChannel(pubsubOptions)
+      val client = PublisherGrpc.newBlockingStub(channel)
+
+      val result = Try(f(client))
+      channel.shutdownNow()
+      result
+    }
+  }
+
+  /**
+   * Ensure PubSub topic exists.
+   *
+   * @param name Topic path. Needs to follow `projects/projectId/topics/topicName`
+   * @return
+   */
+  def ensureTopic(pubsubOptions: PubsubOptions, name: String): Try[Topic] =
+    GrpcClient.publisher(pubsubOptions) { client =>
+      val topic = Topic.newBuilder().setName(name).build()
+      client.createTopic(topic)
+    }
+
+  /**
+   * Ensure PubSub subscription exists.
+   *
+   * @param topic Topic path. Needs to follow `projects/projectId/topics/topicName`
+   * @param name  Subscription path. Needs to follow `projects/projectId/topics/topicName`
+   * @return
+   */
+  def ensureSubscription(pubsubOptions: PubsubOptions,
+                         topic: String,
+                         name: String): Try[Subscription] =
+    GrpcClient.subscriber(pubsubOptions) { client =>
+      val sub = Subscription.newBuilder().setTopic(topic).setName(name).build()
+      client.createSubscription(sub)
+    }
+}
