e397b208805c3011aa1479329e8c660bd3142932
Neville Li <neville@spotify.com>
2017-11-28 22:28:56 -0500

    remove ProtoCoder precedence workaround for BEAM-2658

--- scio-core/src/main/scala/com/spotify/scio/Implicits.scala
+++ scio-core/src/main/scala/com/spotify/scio/Implicits.scala
@@ -22,7 +22,6 @@ import java.lang.{Float => JFloat}
 import com.spotify.scio.coders.KryoAtomicCoder
 import com.spotify.scio.util.ScioUtil
 import org.apache.beam.sdk.coders._
-import org.apache.beam.sdk.extensions.protobuf.ProtoCoder
 import org.apache.beam.sdk.values.KV
 
 import scala.language.implicitConversions
@@ -33,9 +32,6 @@ private[scio] object Implicits {
   private[scio] implicit class RichCoderRegistry(val r: CoderRegistry) extends AnyVal {
 
     def registerScalaCoders(): Unit = {
-      // FIXME: BEAM-2658 make sure ProtoCoder has higher precedence than SerializableCoder
-      r.registerCoderProvider(ProtoCoder.getCoderProvider)
-
       // Missing Coders from DataFlowJavaSDK
       r.registerCoderForClass(classOf[JFloat], FloatCoder.of())
 
