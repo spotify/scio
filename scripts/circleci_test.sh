2a0b117081dbce8f296dcc249c31e29dfa011298
Neville Li <neville@spotify.com>
2018-01-03 13:29:29 -0500

    fix test script for forked PR builds

--- scripts/circleci_test.sh
+++ scripts/circleci_test.sh
@@ -9,14 +9,14 @@ if [ -n "$ENCRYPTION_KEY" ]; then
   openssl aes-256-cbc -d -in "$DIR_OF_SCRIPT/$JSON_KEY.enc" -out "$DIR_OF_SCRIPT/$JSON_KEY" -k $ENCRYPTION_KEY
 fi
 
-if [ "$CIRCLE_PULL_REQUESTS" = "" ]; then
+if [ -n "$CIRCLE_PR_USERNAME" ]; then
+  echo "Running tests for Scala $SCALA_VERSION, forked PR #$CIRCLE_PR_NUMBER from $CIRCLE_PR_USERNAME/$CIRCLE_PR_REPONAME"
+  PROPS="-Dbigquery.project=dummy-project"
+  TESTS="test"
+else
   echo "Running tests for Scala $SCALA_VERSION, branch: $CIRCLE_BRANCH"
   PROPS="-Dbigquery.project=data-integration-test -Dbigquery.secret=$DIR_OF_SCRIPT/$JSON_KEY"
   TESTS="test it:test"
-else
-  echo "Running tests for Scala $SCALA_VERSION, PR: $CIRCLE_PULL_REQUESTS"
-  PROPS="-Dbigquery.project=dummy-project"
-  TESTS="test"
 fi
 
 "$DIR_OF_SCRIPT/gen_schemas.sh"
