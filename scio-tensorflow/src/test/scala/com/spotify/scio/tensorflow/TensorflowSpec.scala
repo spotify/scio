6f7e33efbe7c8ee318bb03d30583724f38c43acc
Rafal Wojdyla <rav@spotify.com>
2018-01-11 17:11:21 +0100

    Wip upgrade tf 140

--- scio-tensorflow/src/test/scala/com/spotify/scio/tensorflow/TensorflowSpec.scala
+++ scio-tensorflow/src/test/scala/com/spotify/scio/tensorflow/TensorflowSpec.scala
@@ -28,7 +28,7 @@ private object TFJob {
     val (sc, args) = ContextAndArgs(argv)
     sc.parallelize(1L to 10)
       .predict(args("graphURI"), Seq("multiply"))
-      {e => Map("input" -> Tensor.create(e))}
+      {e => Map("input" -> Tensors.create(e))}
       {(r, o) => (r, o.map{case (_, t) => t.longValue()}.head)}
       .saveAsTextFile(args("output"))
     sc.close()
@@ -40,8 +40,8 @@ private object TFJob2Inputs {
     val (sc, args) = ContextAndArgs(argv)
     sc.parallelize(1L to 10)
       .predict(args("graphURI"), Seq("multiply"))
-      {e => Map("input" -> Tensor.create(e),
-                "input2" -> Tensor.create(3L))}
+      {e => Map("input" -> Tensors.create(e),
+                "input2" -> Tensors.create(3L))}
       {(r, o) => (r, o.map{case (_, t) => t.longValue()}.head)}
       .saveAsTextFile(args("output"))
     sc.close()
@@ -54,8 +54,9 @@ class TensorflowSpec extends PipelineSpec {
     val const = "MyConst"
     val graph = new Graph()
     val helloworld = s"Hello from ${TensorFlow.version()}"
-    val t = Tensor.create(helloworld.getBytes("UTF-8"))
+    val t = Tensors.create(helloworld)
     graph.opBuilder("Const", const).setAttr("dtype", t.dataType()).setAttr("value", t).build()
+    t.close()
     (graph, const, helloworld)
   }
 
@@ -63,46 +64,73 @@ class TensorflowSpec extends PipelineSpec {
     val (graph, const, helloworld) = createHelloWorldGraph
     val session = new Session(graph)
     val r = session.runner().fetch(const).run().get(0)
-    new String(r.bytesValue()) should be (helloworld)
+    try {
+      new String(r.bytesValue()) should be(helloworld)
+    } finally {
+      session.close()
+      graph.close()
+      r.close()
+    }
   }
 
   it should "work for serde model" in {
     val (graph, const, helloworld) = createHelloWorldGraph
-    val graphFile = Files.createTempFile("tf-grap", ".bin")
-    Files.write(graphFile, graph.toGraphDef)
-    val newGraph = new Graph()
-    newGraph.importGraphDef(Files.readAllBytes(graphFile))
     val session = new Session(graph)
     val r = session.runner().fetch(const).run().get(0)
-    new String(r.bytesValue()) should be (helloworld)
+    val newGraph = new Graph()
+    try {
+      val graphFile = Files.createTempFile("tf-grap", ".bin")
+      Files.write(graphFile, graph.toGraphDef)
+      newGraph.importGraphDef(Files.readAllBytes(graphFile))
+      new String(r.bytesValue()) should be(helloworld)
+    } finally {
+      session.close()
+      graph.close()
+      newGraph.close()
+      r.close()
+    }
   }
 
   it should "allow to predict" in {
     val g = new Graph()
-    val t3 = Tensor.create(3L)
-    val input = g.opBuilder("Placeholder", "input").setAttr("dtype", t3.dataType).build.output(0)
-    val c3 = g.opBuilder("Const", "c3")
-      .setAttr("dtype", t3.dataType)
-      .setAttr("value", t3).build.output(0)
-    g.opBuilder("Mul", "multiply").addInput(c3).addInput(input).build()
-
-
+    val t3 = Tensors.create(4L)
+    try {
+      val input = g.opBuilder("Placeholder", "input").setAttr("dtype", t3.dataType).build.output(0)
+      val c3 = g.opBuilder("Const", "c3")
+        .setAttr("dtype", t3.dataType)
+        .setAttr("value", t3).build.output(0)
+      g.opBuilder("Mul", "multiply").addInput(c3).addInput(input).build()
+      JobTest[TFJob.type]
+        .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
+        .args("--graphURI=tf-graph.bin", "--output=output")
+        .output(TextIO("output")) {
+          _ should containInAnyOrder((1L to 10).map(x => (x, x * 4)).map(_.toString))
+        }
+        .run()
+    } finally {
+      g.close()
+      t3.close()
+    }
   }
 
   it should "allow to predict with 2 inputs" in {
     val g = new Graph()
-    val input = g.opBuilder("Placeholder", "input")
-      .setAttr("dtype", DataType.INT64).build.output(0)
-    val input2 = g.opBuilder("Placeholder", "input2")
-      .setAttr("dtype", DataType.INT64).build.output(0)
-    g.opBuilder("Mul", "multiply").addInput(input2).addInput(input).build()
-    JobTest[TFJob2Inputs.type]
-      .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
-      .args("--graphURI=tf-graph.bin", "--output=output")
-      .output(TextIO("output")){
-        _ should containInAnyOrder((1L to 10).map(x => (x, x * 3)).map(_.toString))
-      }
-      .run()
+    try {
+      val input = g.opBuilder("Placeholder", "input")
+        .setAttr("dtype", DataType.INT64).build.output(0)
+      val input2 = g.opBuilder("Placeholder", "input2")
+        .setAttr("dtype", DataType.INT64).build.output(0)
+      g.opBuilder("Mul", "multiply").addInput(input2).addInput(input).build()
+      JobTest[TFJob2Inputs.type]
+        .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
+        .args("--graphURI=tf-graph.bin", "--output=output")
+        .output(TextIO("output")) {
+          _ should containInAnyOrder((1L to 10).map(x => (x, x * 3)).map(_.toString))
+        }
+        .run()
+    } finally {
+      g.close()
+    }
   }
 
 }
