19562ce45d47a6eca10be298ca7a4bf6273afea3
Shameera R Yodage <shameera@spotify.com>
2018-01-12 12:43:31 -0500

    Refactor code to aline with review

--- scio-core/src/main/scala/com/spotify/scio/nio/TextIO.scala
+++ scio-core/src/main/scala/com/spotify/scio/nio/TextIO.scala
@@ -1,104 +1,61 @@
-/*
- * Copyright 2016 Spotify AB.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package com.spotify.scio.nio
 
 import com.spotify.scio.ScioContext
 import com.spotify.scio.io.{FileStorage, Tap}
-import com.spotify.scio.testing.TestIO
 import com.spotify.scio.util.ScioUtil
 import com.spotify.scio.values.SCollection
-import org.apache.beam.sdk.io.{Compression, FileBasedSink}
-import org.apache.beam.sdk.{io => bio}
+import org.apache.beam.sdk.io.{Compression, FileBasedSink, TextIO => BTextIO}
 
 import scala.concurrent.Future
-import scala.language.implicitConversions
 
-case class TextIO(path: String) extends TestIO[String](path) with Tap[String] {
+case class TextIO(name: String, path: String) extends ScioIO[String] with Tap[String] {
+
+  case class ReadParams(compression: Compression = Compression.AUTO)
+
+  case class WriteParams(suffix: String = ".txt",
+                         numShards: Int = 0,
+                         compression: Compression = Compression.UNCOMPRESSED)
+
+  type ReadP = ReadParams
+  type WriteP = WriteParams
+
+  def read(sc: ScioContext, params: ReadParams): SCollection[String] = sc.requireNotClosed {
+    if(sc.isTest) {
+      // TODO: support test
+      throw new UnsupportedOperationException("TextIO test is not yet supported")
+    } else {
+      sc.wrap(sc.applyInternal(BTextIO.read().from(path)
+        .withCompression(params.compression))).setName(name)
+    }
+  }
+
+  def write(pipeline: SCollection[String], params: WriteParams): Future[Tap[String]] = {
+    if (pipeline.context.isTest) {
+      // TODO: support test
+      throw new UnsupportedOperationException("TextIO test is not yet supported")
+    } else {
+      pipeline.applyInternal(textOut(path, params))
+      pipeline.context.makeFuture(TextIO(name, ScioUtil.addPartSuffix(path)))
+    }
+  }
 
   /** Read data set into memory. */
   def value: Iterator[String] = FileStorage(path).textFile
 
   /** Open data set as an [[com.spotify.scio.values.SCollection SCollection]]. */
-  def open(sc: ScioContext): SCollection[String] = sc.textFile(path)
-}
+  def open(sc: ScioContext): SCollection[String] = read(sc, ReadParams())
 
-object TextIO extends ScioIO[String] {
-  /** Implicitly convert ScioContext to have all TextIO read helper functions. */
-  implicit def read(sc: ScioContext): TextScioContext = new TextScioContext(sc)
-
-  /**
-   * Implicitly convert [[com.spotify.scio.values.SCollection SCollection[String]]] to
-   * have all TextIO write helper functions
-   */
-  implicit def write(scol: SCollection[String]): TextSCollection = new TextSCollection(scol)
+  private[scio] def textOut(path: String,
+                            params: WriteParams) = {
+    BTextIO.write()
+      .to(pathWithShards(path))
+      .withSuffix(params.suffix)
+      .withNumShards(params.numShards)
+      .withWritableByteChannelFactory(
+        FileBasedSink.CompressionType.fromCanonical(params.compression))
+  }
 
   private[scio] def pathWithShards(path: String) = path.replaceAll("\\/+$", "") + "/part"
 
-  class TextScioContext(@transient val self: ScioContext) extends Serializable {
-
-    /**
-     * Get an SCollection for a text file.
-     *
-     * @group input
-     */
-    def textFile(path: String,
-                 compression: Compression = Compression.AUTO)
-    : SCollection[String] = self.requireNotClosed {
-      if (self.isTest) {
-        self.getTestInput(TextIO(path))
-      } else {
-        self.wrap(self.applyInternal(bio.TextIO.read().from(path)
-          .withCompression(compression))).setName(path)
-      }
-    }
-  }
-
-  class TextSCollection(@transient val self: SCollection[String]) extends Serializable {
-
-    /**
-     * Save this SCollection as a text file. Note that elements must be of type `String`.
-     *
-     * @group output
-     */
-    def saveAsTextFile(path: String,
-                       suffix: String = ".txt",
-                       numShards: Int = 0,
-                       compression: Compression = Compression.UNCOMPRESSED)
-    : Future[Tap[String]] = {
-      if (self.context.isTest) {
-        self.context.testOut(TextIO(path))(self)
-        self.saveAsInMemoryTap
-      } else {
-        self.applyInternal(textOut(path, suffix, numShards, compression))
-        self.context.makeFuture(TextIO(ScioUtil.addPartSuffix(path)))
-      }
-    }
-
-    private[scio] def textOut(path: String,
-                              suffix: String,
-                              numShards: Int,
-                              compression: Compression) = {
-      bio.TextIO.write()
-        .to(pathWithShards(path))
-        .withSuffix(suffix)
-        .withNumShards(numShards)
-        .withWritableByteChannelFactory(FileBasedSink.CompressionType.fromCanonical(compression))
-    }
-  }
-
 }
+
