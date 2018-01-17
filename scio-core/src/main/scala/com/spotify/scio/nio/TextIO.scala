24607e7f885fe03c699a17f2a6ef3873fc265b81
Shameera R Yodage <shameera@spotify.com>
2018-01-16 20:38:02 -0500

    ScioIO mixin with Tap, moved FileStorage code to TextIO object

--- scio-core/src/main/scala/com/spotify/scio/nio/TextIO.scala
+++ scio-core/src/main/scala/com/spotify/scio/nio/TextIO.scala
@@ -17,15 +17,25 @@
 
 package com.spotify.scio.nio
 
+import java.io.{BufferedInputStream, InputStream, SequenceInputStream}
+import java.nio.channels.Channels
+import java.util.Collections
+
+import com.google.api.client.util.Charsets
 import com.spotify.scio.ScioContext
-import com.spotify.scio.io.{FileStorage, InMemorySink, InMemoryTap, Tap}
+import com.spotify.scio.io.Tap
 import com.spotify.scio.util.ScioUtil
 import com.spotify.scio.values.SCollection
-import org.apache.beam.sdk.io.{Compression, FileBasedSink, TextIO => BTextIO}
+import org.apache.beam.sdk.io.fs.MatchResult.Metadata
+import org.apache.beam.sdk.io.{Compression, FileBasedSink, FileSystems, TextIO => BTextIO}
+import org.apache.commons.compress.compressors.CompressorStreamFactory
+import org.apache.commons.io.IOUtils
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
+import scala.util.Try
 
-case class TextIO(path: String) extends ScioIO[String] with Tap[String] {
+case class TextIO(path: String) extends ScioIO[String] {
 
   case class ReadParams(compression: Compression = Compression.AUTO)
 
@@ -57,7 +67,7 @@ case class TextIO(path: String) extends ScioIO[String] with Tap[String] {
   }
 
   /** Read data set into memory. */
-  def value: Iterator[String] = FileStorage(path).textFile
+  def value: Iterator[String] = TextIO.textFile(path)
 
   /** Open data set as an [[com.spotify.scio.values.SCollection SCollection]]. */
   def open(sc: ScioContext): SCollection[String] = read(sc, ReadParams())
@@ -74,5 +84,33 @@ case class TextIO(path: String) extends ScioIO[String] with Tap[String] {
 
   private[scio] def pathWithShards(path: String) = path.replaceAll("\\/+$", "") + "/part"
 
+}
+
+object TextIO {
+
+  /** Read all files in the path line by line and return it as `Iterator[String]` */
+  def textFile(path: String): Iterator[String] = {
+    val factory = new CompressorStreamFactory()
+
+    def wrapInputStream(in: InputStream) = {
+      val buffered = new BufferedInputStream(in)
+      Try(factory.createCompressorInputStream(buffered)).getOrElse(buffered)
+    }
+
+    val input = getDirectoryInputStream(path, wrapInputStream)
+    IOUtils.lineIterator(input, Charsets.UTF_8).asScala
+  }
+
+  private[scio] def getDirectoryInputStream(path: String,
+                                            wrapperFn: InputStream => InputStream = identity)
+  : InputStream = {
+    val inputs = listFiles(path).map(getObjectInputStream).map(wrapperFn).asJava
+    new SequenceInputStream(Collections.enumeration(inputs))
+  }
+
+  private def listFiles(path: String): Seq[Metadata] = FileSystems.`match`(path).metadata().asScala
+
+  private def getObjectInputStream(meta: Metadata): InputStream =
+    Channels.newInputStream(FileSystems.open(meta.resourceId()))
 }
 
