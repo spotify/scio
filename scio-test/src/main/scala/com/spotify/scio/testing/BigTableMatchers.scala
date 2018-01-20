93ecf3d355ed2201dea9571b02fe62ed69457b25
Claire McGinty <cmcginty@linkedin.com>
2018-01-19 21:43:17 -0500

    Add BigTable-specific SCollectionMatchers to scio-test (#1019)
    * Add BigTable-specific SCollectionMatchers to scio-test
    
    * BigTable keys should be ByteStrings + small style fixes

---
+++ scio-test/src/main/scala/com/spotify/scio/testing/BigTableMatchers.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.testing
+
+import java.lang.{Iterable => JIterable}
+import java.util.{Map => JMap}
+
+import com.google.bigtable.v2.Mutation
+import com.google.bigtable.v2.Mutation.MutationCase
+import com.google.protobuf.ByteString
+import com.spotify.scio.values.SCollection
+import org.scalatest.matchers.{MatchResult, Matcher}
+
+import scala.language.implicitConversions
+import scala.reflect.ClassTag
+
+/**
+ * Trait with ScalaTest [[org.scalatest.matchers.Matcher Matcher]]s for
+ * [[com.spotify.scio.values.SCollection SCollection]]s specific to BigTable output.
+ */
+trait BigTableMatchers extends SCollectionMatchers {
+  type BTRow = (ByteString, Iterable[Mutation])
+  type BTCollection = SCollection[BTRow]
+
+  /** Provide an implicit BT serializer for common cell value type String. */
+  implicit def stringBTSerializer(s: String): ByteString = ByteString.copyFromUtf8(s)
+
+  /** Check that the BT collection contains only the given keys, in any order. */
+  def containRowKeys(expectedKeys: ByteString*): Matcher[BTCollection] =
+    new Matcher[BTCollection] {
+      override def apply(left: BTCollection): MatchResult = {
+        containInAnyOrder(expectedKeys).apply(left.keys)
+      }
+    }
+
+  /**
+   * Check that the BT collection contains only the given column families, unique, in any order.
+   */
+  def containColumnFamilies(expectedCFs: String*): Matcher[BTCollection] =
+    new Matcher[BTCollection] {
+      override def apply(left: BTCollection): MatchResult = {
+        val foundCFs = left.flatMap { case (key, cells) =>
+          cells.map(_.getSetCell.getFamilyName)
+        }
+
+        containInAnyOrder(expectedCFs).apply(foundCFs.distinct)
+      }
+    }
+
+  /**
+   * Check that the BT collection contains a cell with the given row key, column family, and
+   * deserialized cell value. Column qualifier defaults to the same as column family.
+   */
+  def containSetCellValue[V: ClassTag](key: ByteString, cf: String, value: V)
+                                      (implicit ser: V => ByteString): Matcher[BTCollection] =
+    containSetCellValue(key, cf, cf, value)
+
+  /**
+   * Check that the BT collection contains a cell with the given row key, column family, column
+   * qualifier, and deserialized cell value.
+   * @param key Row key the cell should be in
+   * @param cf Column family the cell should have
+   * @param cq Column qualifier the cell should have
+   * @param value Deserialized value of the set cell
+   * @param ser Serializer to convert value type V to ByteString for BT format
+   * @tparam V Class of expected value
+   */
+  def containSetCellValue[V: ClassTag](key: ByteString, cf: String, cq: String, value: V)
+                                      (implicit ser: V => ByteString): Matcher[BTCollection] =
+    new Matcher[BTCollection] {
+      override def apply(left: BTCollection): MatchResult = {
+        val flattenedRows = left.flatMap { case (rowKey, rowValue) =>
+          rowValue.map { cell =>
+            (
+              rowKey,
+              cell.getSetCell.getFamilyName,
+              cell.getSetCell.getColumnQualifier,
+              cell.getSetCell.getValue
+            )
+          }
+        }
+
+        containValue((
+          key,
+          cf,
+          ByteString.copyFromUtf8(cq),
+          ser.apply(value)
+          )).apply(flattenedRows)
+      }
+    }
+
+  /**
+   * Check that the BT collection contains a cell with the given row key and enumerated
+   * MutationCase, making no assumptions about the contents of the rest of the collection.
+   */
+  def containCellMutationCase[V: ClassTag](key: ByteString, mutation: MutationCase):
+  Matcher[BTCollection] =
+    new Matcher[BTCollection] {
+      override def apply(left: BTCollection): MatchResult = {
+        val flattenedRows = left.flatMap { case (rowKey, rowValue) =>
+          rowValue.map(cell => (rowKey, cell.getMutationCase))
+        }
+
+        containValue((key, mutation)).apply(flattenedRows)
+      }
+    }
+}
---
+++ scio-test/src/test/scala/com/spotify/scio/testing/BigTableMatchersTest.scala
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.testing
+
+import com.google.bigtable.v2.Mutation.{MutationCase, SetCell}
+import com.google.bigtable.v2._
+import com.google.protobuf.ByteString
+
+// scalastyle:off no.whitespace.before.left.bracket
+class BigTableMatchersTest extends PipelineSpec with BigTableMatchers {
+  private lazy val key1 = ByteString.copyFromUtf8("k1")
+  private lazy val key2 = ByteString.copyFromUtf8("k2")
+  private lazy val emptyCell = Seq(Mutation.newBuilder().build())
+
+  "BigTableMatchers" should "support containRowKeys" in {
+    val tableData: Seq[BTRow] = Seq(
+      (key1, emptyCell),
+      (key2, emptyCell)
+    )
+
+    // should cases
+    runWithContext {
+      _.parallelize(tableData) should containRowKeys(key1, key2)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) should containRowKeys (key1)
+      }
+    }
+
+    // should not cases
+    runWithContext {
+      _.parallelize(tableData) shouldNot containRowKeys(key1)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) shouldNot containRowKeys(key1, key2)
+      }
+    }
+  }
+
+  private lazy val columnFamily1 = "cf1"
+  private lazy val columnFamily2 = "cf2"
+
+  it should "support containColumnFamilies" in {
+    val cell1 = Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+      .setFamilyName(columnFamily1))
+      .build()
+
+    val cell2 = Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+        .setFamilyName(columnFamily2))
+      .build()
+
+    val tableData: Seq[BTRow] = Seq(
+      (key1, Seq(cell1)),
+      (key2, Seq(cell2))
+    )
+
+    // should cases
+    runWithContext {
+      _.parallelize(tableData) should containColumnFamilies(columnFamily1, columnFamily2)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) should containColumnFamilies (columnFamily1)
+      }
+    }
+
+    // should not cases
+    runWithContext {
+      _.parallelize(tableData) shouldNot containColumnFamilies(columnFamily1)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) shouldNot containColumnFamilies (columnFamily1, columnFamily2)
+      }
+    }
+  }
+
+  val cellValue1 = "cv1"
+  val cellValue2 = "cv2"
+
+  it should "support containSetCellValue" in {
+    val cell1 = Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+        .setFamilyName(columnFamily1)
+        .setColumnQualifier(columnFamily1)
+        .setValue(ByteString.copyFromUtf8(cellValue1))
+      ).build()
+
+    val cell2 = Mutation.newBuilder()
+      .setSetCell(SetCell.newBuilder()
+        .setFamilyName(columnFamily2)
+        .setColumnQualifier(columnFamily2)
+        .setValue(ByteString.copyFromUtf8(cellValue2))
+      ).build()
+
+    val tableData: Seq[BTRow] = Seq(
+      (key1, Seq(cell1, cell2))
+    )
+
+    // should cases
+    runWithContext { sc =>
+      sc.parallelize(tableData) should containSetCellValue(key1, columnFamily1, cellValue1)
+      sc.parallelize(tableData) should containSetCellValue(key1, columnFamily2, cellValue2)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) should containSetCellValue(key2, columnFamily1, cellValue1)
+      }
+    }
+
+    // should not cases
+    runWithContext {
+      _.parallelize(tableData) shouldNot containSetCellValue(key2, columnFamily1, cellValue1)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) shouldNot containSetCellValue(key1, columnFamily1, cellValue1)
+      }
+    }
+  }
+
+  it should "support containCellMutationCase" in {
+    val cell = Mutation.newBuilder()
+      .setDeleteFromRow(Mutation.DeleteFromRow.newBuilder().build())
+      .build()
+
+    val tableData: Seq[BTRow] = Seq(
+      (key1, Seq(cell))
+    )
+
+    // should cases
+    runWithContext {
+      _.parallelize(tableData) should containCellMutationCase(key1, MutationCase.DELETE_FROM_ROW)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) should containCellMutationCase(key1, MutationCase.SET_CELL)
+      }
+    }
+
+    // should not cases
+    runWithContext {
+      _.parallelize(tableData) shouldNot containCellMutationCase(key1, MutationCase.SET_CELL)
+    }
+
+    an [AssertionError] should be thrownBy {
+      runWithContext {
+        _.parallelize(tableData) shouldNot containCellMutationCase(key1,
+          MutationCase.DELETE_FROM_ROW)
+      }
+    }
+  }
+}
+// scalastyle:on no.whitespace.before.left.bracket
