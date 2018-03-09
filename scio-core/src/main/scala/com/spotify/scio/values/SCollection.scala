bd4b863e866ed6fc0147c25ebd1304ef5ed1b1a6
inejc <nejc.ilenic@gmail.com>
2018-03-09 09:23:56 +0100

    incorporate initial name in the saveAsTypedBigQuery's second transform name

--- scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
@@ -1102,10 +1102,11 @@ sealed trait SCollection[T] extends PCollectionWrapper[T] {
       }
     } else {
       val bqt = BigQueryType[T]
+      val initialTfName = this.tfName
       import scala.concurrent.ExecutionContext.Implicits.global
       this
         .map(bqt.toTableRow)
-        .withName(s"${this.tfName.replaceFirst("@.*", "")}$$Write").saveAsBigQuery(
+        .withName(s"$initialTfName$$Write").saveAsBigQuery(
           table,
           bqt.schema,
           writeDisposition,
