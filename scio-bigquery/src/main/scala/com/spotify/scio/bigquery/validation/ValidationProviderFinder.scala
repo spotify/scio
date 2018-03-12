233307b9b6f589cee10acd67ff2708f4aefb689c
Josh Freeberg <joshuaf@spotify.com>
2018-03-12 14:10:16 -0400

    cleaning code

--- scio-bigquery/src/main/scala/com/spotify/scio/bigquery/validation/ValidationProviderFinder.scala
+++ scio-bigquery/src/main/scala/com/spotify/scio/bigquery/validation/ValidationProviderFinder.scala
@@ -4,26 +4,16 @@ package com.spotify.scio.bigquery.validation
 object ValidationProviderFinder {
 
   def getProvider: ValidationProvider = {
-
-  /**
-    println(Class.forName("com.spotify.semantic.SemanticTypeValidationProvider"))
-    val x = ServiceLoader.load(classOf[ValidationProvider])
-      .iterator()
-
-    if (x.hasNext) {
-      x.next()
-    } else {
-      println("Using old now")
-      new DummyValidationProvider
-    }
-  */
     try {
+      // Load the class dynamically at compile time and runtime
       Class.forName(System.getenv("VALIDATION_PROVIDER"))
         .newInstance()
         .asInstanceOf[ValidationProvider]
     } catch {
-      case _: Throwable => println("Using old now")
+      case _: Throwable => {
+        println("Using DummyValidationProvider")
         new DummyValidationProvider
+      }
     }
   }
 }
