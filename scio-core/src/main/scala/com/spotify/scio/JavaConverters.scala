d455d1db02a41bed64ad7ad4ba70386ed25a89b8
Juan-Pablo Velez <jpvelez@spotify.com>
2018-03-12 17:05:13 -0400

    Implement FilenamePolicy Java Converter

--- scio-core/src/main/scala/com/spotify/scio/JavaConverters.scala
+++ scio-core/src/main/scala/com/spotify/scio/JavaConverters.scala
@@ -1,16 +1,24 @@
 package com.spotify.scio
 
-import com.spotify.scio.avro.types.AvroType
-import org.apache.beam.sdk.extensions.gcp.storage.GcsResourceId
-import org.apache.beam.sdk.io._
+import org.apache.beam.sdk.io.{DefaultFilenamePolicy, FileBasedSink}
 import org.apache.beam.sdk.io.fs.ResourceId
 import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider
-import org.apache.beam.sdk.util.gcsfs.GcsPath
 
 object JavaConverters {
 
   implicit def toResourceId(filenamePrefix: String): ResourceId =
     FileBasedSink.convertToFileResourceIfPossible(filenamePrefix)
+
+  case class FilenamePolicy(baseFilename: String, shardTemplate: String = null,
+                       templateSuffix: String = null, windowedWrites: Boolean = false)
+
+  implicit def toFilenamePolicy(policy: FilenamePolicy): DefaultFilenamePolicy = {
+    DefaultFilenamePolicy.fromStandardParameters(
+      StaticValueProvider.of(policy.baseFilename),
+      policy.shardTemplate,
+      policy.templateSuffix,
+      policy.windowedWrites)
+  }
 
 }
 
--- scio-examples/src/main/scala/com/spotify/scio/examples/extra/JavaConvertersExample.scala
+++ scio-examples/src/main/scala/com/spotify/scio/examples/extra/JavaConvertersExample.scala
@@ -1,6 +1,6 @@
 package com.spotify.scio.examples.extra
 
-import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.io.{AvroIO, TextIO}
 import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider
 import com.spotify.scio.JavaConverters._
 
@@ -8,5 +8,9 @@ object JavaConvertersExample {
 
   val path: String = "gs://foobar/path/to/file"
   TextIO.writeCustomType().toResource(StaticValueProvider.of(path))
+
+  AvroIO.writeCustomType().to(FilenamePolicy(path))
+  TextIO.writeCustomType().to(FilenamePolicy(path, "-SSSSS-of-NNNNN", ".csv", true))
+  AvroIO.writeCustomType().to(FilenamePolicy(path, templateSuffix = ".tsv"))
 
 }
