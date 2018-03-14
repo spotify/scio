64311c1f2ea00bd5e9d5d5ff3d2ef39b5ac62971
Josh Freeberg <joshuaf@spotify.com>
2018-03-14 18:54:42 -0400

    adjusting null

--- scio-bigquery/src/main/scala/com/spotify/scio/bigquery/validation/ValidationProviderFinder.scala
+++ scio-bigquery/src/main/scala/com/spotify/scio/bigquery/validation/ValidationProviderFinder.scala
@@ -25,7 +25,7 @@ object ValidationProviderFinder {
   def getProvider: ValidationProvider = {
     try {
       // Load the class dynamically at compile time and runtime
-      Class.forName(System.getProperty("VALIDATION_PROVIDER"))
+      Class.forName(System.getProperty("VALIDATION_PROVIDER", ""))
         .newInstance()
         .asInstanceOf[ValidationProvider]
     } catch {
