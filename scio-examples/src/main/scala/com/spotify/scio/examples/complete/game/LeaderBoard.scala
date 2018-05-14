f91e1948b7653d9a8cef4193325cdf2ad627e987
Anne DeCusatis <anned@spotify.com>
2018-05-14 13:17:49 -0400

    add explanatory comments to LeaderBoard

--- scio-examples/src/main/scala/com/spotify/scio/examples/complete/game/LeaderBoard.scala
+++ scio-examples/src/main/scala/com/spotify/scio/examples/complete/game/LeaderBoard.scala
@@ -30,8 +30,11 @@ import org.apache.beam.sdk.transforms.windowing._
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.{DateTimeZone, Duration, Instant}
 
+// Example: Calculate leaderboard for game (highest team scores, highest user scores)
+
 object LeaderBoard {
 
+  // The schemas for the BigQuery tables to write output to are defined as annotated case classes
   @BigQueryType.toTable
   case class TeamScoreSums(team: String, total_score: Int,
                            window_start: String, processing_time: String, timing: String)
@@ -41,43 +44,60 @@ object LeaderBoard {
 
   // scalastyle:off method.length
   def main(cmdlineArgs: Array[String]): Unit = {
+    // Create `ScioContext` and `Args`
     val (opts, args) = ScioContext.parseArguments[ExampleOptions](cmdlineArgs)
     val sc = ScioContext(opts)
     sc.optionsAs[StreamingOptions].setStreaming(true)
     val exampleUtils = new ExampleUtils(sc.options)
 
+    // Date formatter for full timestamp
     def fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
       .withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone("PST")))
+    // Duration in minutes over which to calculate team scores, defaults to 1 hour
     val teamWindowDuration = Duration.standardMinutes(args.int("teamWindowDuration", 60))
+    // Data that comes in from our streaming pipeline after this duration isn't considered in our
+    // processing. Measured in minutes, defaults to 2 hours
     val allowedLateness = Duration.standardMinutes(args.int("allowedLateness", 120))
 
+    // Read in streaming data from PubSub and parse each row as `GameActionInfo` events
     val gameEvents = sc.pubsubTopic(args("topic"), timestampAttribute = "timestamp_ms")
       .flatMap(UserScore.parseEvent)
 
     calculateTeamScores(gameEvents, teamWindowDuration, allowedLateness)
+      // Add windowing information to team score results by converting to `WindowedSCollection`
       .toWindowed
       .map { wv =>
+        // Convert from score tuple to TeamScoreSums object with both tuple and windowing info
         val start = fmt.print(wv.window.asInstanceOf[IntervalWindow].start())
         val now = fmt.print(Instant.now())
         val timing = wv.pane.getTiming.toString
         wv.copy(value = TeamScoreSums(wv.value._1, wv.value._2, start, now, timing))
       }
+      // Done with windowing information, convert back to regular `SCollection`
       .toSCollection
+      // Save to the BigQuery table defined by "output" in the arguments passed in + "_team" suffix
       .saveAsTypedBigQuery(args("output") + "_team")
 
     gameEvents
+      // Recalculate using global window, 10 minutes after every first processing in the window
       .withGlobalWindow(WindowOptions(
         trigger = Repeatedly.forever(AfterProcessingTime.pastFirstElementInPane()
           .plusDelayOf(Duration.standardMinutes(10))),
         accumulationMode = ACCUMULATING_FIRED_PANES,
         allowedLateness = allowedLateness)
       )
+      // Change each event into a tuple of: user, and that user's score
       .map(i => (i.user, i.score))
+      // Sum the scores for a user
       .sumByKey
+      // Map summed results from tuples into `UserScoreSums` case class, so we can save to BQ
       .map(kv => UserScoreSums(kv._1, kv._2, fmt.print(Instant.now())))
+      // Save to the BigQuery table defined by "output" in the arguments passed in + "_user" suffix
       .saveAsTypedBigQuery(args("output") + "_user")
 
+    // Close context and run the job
     val result = sc.close()
+    // Wait to finish processing before exiting when streaming pipeline is halted
     exampleUtils.waitToFinish(result.internal)
   }
   // scalastyle:on method.length
@@ -86,6 +106,9 @@ object LeaderBoard {
                           teamWindowDuration: Duration,
                           allowedLateness: Duration): SCollection[(String, Int)] =
     infos.withFixedWindows(
+      // Using a fixed window, calculate every time the window ends, allowing recalculations
+      // anytime after: 5 minutes after first element was processed, and
+      // anytime until: 10 minutes after first element was processed.
       teamWindowDuration,
       options = WindowOptions(
         trigger = AfterWatermark.pastEndOfWindow()
@@ -95,7 +118,9 @@ object LeaderBoard {
             .plusDelayOf(Duration.standardMinutes(10))),
         accumulationMode = ACCUMULATING_FIRED_PANES,
         allowedLateness = allowedLateness))
+      // Change each event into a tuple of: team user was on, and that user's score
       .map(i => (i.team, i.score))
+      // Sum the scores across the defined window, using "team" as the key to sum by
       .sumByKey
 
 }
