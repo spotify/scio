ba5360f4a9f0d69c16e11c6e93b87e0061775025
Andrew Nguonly <andrew@joinhoney.com>
2018-05-14 13:56:28 -0700

    Create SpannerScioContext class for reading from Spanner.
    903

--- scio-spanner/src/main/scala/com/spotify/scio/spanner/package.scala
+++ scio-spanner/src/main/scala/com/spotify/scio/spanner/package.scala
@@ -1,10 +1,11 @@
 package com.spotify.scio
 
-import com.google.cloud.spanner.Mutation
+import com.google.cloud.spanner.{KeySet, Mutation, Struct}
 import com.spotify.scio.io.Tap
 import com.spotify.scio.values.SCollection
 import org.apache.beam.sdk.io.gcp.spanner.{SpannerConfig, SpannerIO}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 /**
@@ -15,6 +16,67 @@ import scala.concurrent.Future
   * }}}
   */
 package object spanner {
+
+  /** Enhanced version of [[ScioContext]] with Spanner methods. */
+  implicit class SpannerScioContext(val self: ScioContext) extends AnyVal {
+
+    /** Read from Spanner table. Return [[SCollection]] of [[Struct]]s. */
+    def spannerFromTable(projectId: String,
+                         instanceId: String,
+                         databaseId: String,
+                         table: String,
+                         columns: Iterable[String],
+                         keySet: KeySet = null): SCollection[Struct] = {
+
+      val spannerConfig = SpannerConfig.create
+        .withProjectId(projectId)
+        .withInstanceId(instanceId)
+        .withDatabaseId(databaseId)
+
+      spannerFromTableWithConfig(spannerConfig, table, columns, keySet)
+    }
+
+    /** Read from Spanner table. Return [[SCollection]] of [[Struct]]s. */
+    def spannerFromTableWithConfig(spannerConfig: SpannerConfig,
+                                   table: String,
+                                   columns: Iterable[String],
+                                   keySet: KeySet = null): SCollection[Struct] = {
+
+      var read = SpannerIO.read.withSpannerConfig(spannerConfig)
+        .withTable(table)
+        .withColumns(columns.toSeq.asJava)
+
+      if (keySet != null) { read = read.withKeySet(keySet) }
+
+      self.wrap(self.applyInternal(read))
+    }
+
+    /** Read from Spanner with query. Return [[SCollection]] of [[Struct]]s. */
+    def spannerFromQuery(projectId: String,
+                         instanceId: String,
+                         databaseId: String,
+                         query: String,
+                         index: String = null): SCollection[Struct] = {
+
+      val spannerConfig = SpannerConfig.create
+        .withProjectId(projectId)
+        .withInstanceId(instanceId)
+        .withDatabaseId(databaseId)
+
+      spannerFromQueryWithConfig(spannerConfig, query)
+    }
+
+    /** Read from Spanner with query. Return [[SCollection]] of [[Struct]]s. */
+    def spannerFromQueryWithConfig(spannerConfig: SpannerConfig,
+                                   query: String,
+                                   index: String = null): SCollection[Struct] = {
+
+      var read = SpannerIO.read.withSpannerConfig(spannerConfig).withQuery(query)
+      if (index != null) { read = read.withIndex(index) }
+
+      self.wrap(self.applyInternal(read))
+    }
+  }
 
   /** Enhanced version of [[SCollection]] with Spanner methods. */
   implicit class SpannerSCollection(val self: SCollection[Mutation]) extends AnyVal {
@@ -37,10 +99,10 @@ package object spanner {
     def saveAsSpannerWithConfig(spannerConfig: SpannerConfig,
                                 batchSizeBytes: Long = 0): Future[Tap[Mutation]] = {
 
-      var sink = SpannerIO.write.withSpannerConfig(spannerConfig)
-      if (batchSizeBytes > 0) { sink = sink.withBatchSizeBytes(batchSizeBytes) }
+      var write = SpannerIO.write.withSpannerConfig(spannerConfig)
+      if (batchSizeBytes > 0) { write = write.withBatchSizeBytes(batchSizeBytes) }
 
-      self.applyInternal(sink)
+      self.applyInternal(write)
       Future.failed(new NotImplementedError("Spanner future not implemented."))
     }
   }
