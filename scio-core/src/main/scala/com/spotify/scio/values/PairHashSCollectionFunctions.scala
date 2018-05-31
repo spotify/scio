4a5773cb1162053d5ef09afdac0e700845a9f085
martinbomio <martinbomio@gmail.com>
2018-05-31 21:16:02 +0200

    fix #1064, add skew join variants (#1193)

---
+++ scio-core/src/main/scala/com/spotify/scio/values/PairHashSCollectionFunctions.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.values
+
+import scala.collection.mutable.{ArrayBuffer, Map => MMap}
+import scala.reflect.ClassTag
+
+ /**
+  * Extra functions available on SCollections of (key, value) pairs for hash based joins
+  * through an implicit conversion.
+  *
+  * @groupname join Join Operations
+  */
+class PairHashSCollectionFunctions[K, V](val self: SCollection[(K, V)])
+  (implicit ctKey: ClassTag[K], ctValue: ClassTag[V]) {
+
+   /**
+    * Perform an inner join by replicating `that` to all workers. The right side should be tiny and
+    * fit in memory.
+    *
+    * @group join
+    */
+  def hashJoin[W: ClassTag](that: SCollection[(K, W)])
+  : SCollection[(K, (V, W))] = self.transform { in =>
+    val side = combineAsMapSideInput(that)
+
+    in.withSideInputs(side).flatMap[(K, (V, W))] { (kv, s) =>
+      s(side).getOrElse(kv._1, ArrayBuffer.empty[W]).iterator.map(w => (kv._1, (kv._2, w)))
+    }.toSCollection
+  }
+
+   /**
+    * Perform a left outer join by replicating `that` to all workers. The right side should be tiny
+    * and fit in memory.
+    *
+    * @group join
+    */
+  def hashLeftJoin[W: ClassTag](that: SCollection[(K, W)])
+  : SCollection[(K, (V, Option[W]))] = self.transform { in =>
+    val side = combineAsMapSideInput(that)
+
+    in.withSideInputs(side).flatMap[(K, (V, Option[W]))] { (kv, s) =>
+      val (k, v) = kv
+      val m = s(side)
+      if (m.contains(k)) m(k).iterator.map(w => (k, (v, Some(w)))) else Iterator((k, (v, None)))
+    }.toSCollection
+  }
+
+   /**
+    * Perform a gull outer join by replicating `that` to all workers. The right side should be tiny
+    * and fit in memory.
+    *
+    * @group join
+    */
+  def hashFullOuterJoin[W: ClassTag](that: SCollection[(K, W)])
+  : SCollection[(K, (Option[V], Option[W]))] = self.transform { in =>
+    val side = combineAsMapSideInput(that)
+
+    val leftHashed = in.withSideInputs(side).flatMap { (kv, s) =>
+      val (k, v) = kv
+      val m = s(side)
+      if (m.contains(k)) {
+        m(k).iterator.map[(K, (Option[V], Option[W]), Boolean)](w => (k, (Some(v), Some(w)), true))
+      } else {
+        Iterator((k, (Some(v), None), false))
+      }
+    }.toSCollection
+
+    val rightHashed = leftHashed.map(x => (x._1, x._3)).filter(_._2)
+      .keys.combine(List(_))(_ ++ List(_))(_ ++ _)
+      .withSideInputs(side)
+      .flatMap { (mk, s) =>
+        val m = s(side)
+        (m.keySet diff mk.toSet)
+          .flatMap(k => m(k).iterator.map[(K, (Option[V], Option[W]))](w => (k, (None, Some(w)))))
+      }.toSCollection
+
+    leftHashed.map(x => (x._1, x._2)) ++ rightHashed
+  }
+
+  private def combineAsMapSideInput[W: ClassTag](that: SCollection[(K, W)])
+  : SideInput[MMap[K, ArrayBuffer[W]]] = {
+    that.combine { case (k, v) =>
+      MMap(k -> ArrayBuffer(v))
+    } { case (combiner, (k, v)) =>
+      combiner.getOrElseUpdate(k, ArrayBuffer.empty[W]) += v
+      combiner
+    } { case (left, right) =>
+      right.foreach { case (k, vs) => left.getOrElseUpdate(k, ArrayBuffer.empty[W]) ++= vs }
+      left
+    }.asSingletonSideInput(MMap.empty[K, ArrayBuffer[W]])
+  }
+}
---
+++ scio-test/src/test/scala/com/spotify/scio/values/PairHashSCollectionFunctionsTest.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.scio.values
+
+import com.spotify.scio.testing.PipelineSpec
+
+class PairHashSCollectionFunctionsTest extends PipelineSpec {
+
+  it should "support hashJoin()" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2), ("c", 3)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("b", 12), ("d", 14)))
+      val p = p1.hashJoin(p2)
+      p should containInAnyOrder (Seq(("a", (1, 11)), ("b", (2, 12))))
+    }
+  }
+
+  it should "support hashJoin() with duplicate keys" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("a", 2), ("b", 3)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("b", 12), ("b", 13)))
+      val p = p1.hashJoin(p2)
+      p should
+        containInAnyOrder (Seq(("a", (1, 11)), ("a", (2, 11)), ("b", (3, 12)), ("b", (3, 13))))
+    }
+  }
+
+  it should "support hashJoin() with empty RHS" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2), ("c", 3)))
+      val p2 = sc.parallelize(Seq.empty[(String, Int)])
+      val p = p1.hashJoin(p2)
+      p should haveSize(0)
+    }
+  }
+
+  it should "support hashLeftJoin()" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2), ("c", 3)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("b", 12), ("d", 14)))
+      val p = p1.hashLeftJoin(p2)
+      p should containInAnyOrder (Seq(("a", (1, Some(11))), ("b", (2, Some(12))), ("c", (3, None))))
+    }
+  }
+
+  it should "support hashLeftJoin() with empty RHS" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2), ("c", 3)))
+      val p2 = sc.parallelize(Seq.empty[(String, Int)])
+      val p = p1.hashLeftJoin(p2)
+      val empty = Option.empty[Int]
+      p should containInAnyOrder (Seq(("a", (1, empty)), ("b", (2, empty)), ("c", (3, empty))))
+    }
+  }
+
+  it should "support hashLeftJoin() with duplicate keys" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("a", 2), ("b", 3), ("c", 4)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("b", 12), ("b", 13)))
+      val p = p1.hashLeftJoin(p2)
+      p should containInAnyOrder (Seq(
+        ("a", (1, Some(11))),
+        ("a", (2, Some(11))),
+        ("b", (3, Some(12))),
+        ("b", (3, Some(13))),
+        ("c", (4, None))))
+    }
+  }
+
+  it should "support hashFullOuterJoin()" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("c", 13)))
+      val p = p1.hashFullOuterJoin(p2)
+      val e =
+        p should containInAnyOrder (Seq(
+          ("a", (Some(1), Some(11))),
+          ("b", (Some(2), None)),
+          ("c", (None, Some(13)))))
+    }
+  }
+
+  it should "support hashFullOuterJoin() with empty RHS" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("b", 2)))
+      val p2 = sc.parallelize(Seq.empty[(String, Int)])
+      val p = p1.hashFullOuterJoin(p2)
+      val empty = Option.empty[Int]
+      val some = Option[Int] _
+      p should containInAnyOrder (Seq(("a", (some(1), empty)), ("b", (some(2), empty))))
+    }
+  }
+
+  it should "support hashFullOuterJoin() with duplicate keys" in {
+    runWithContext { sc =>
+      val p1 = sc.parallelize(Seq(("a", 1), ("a", 2), ("b", 3), ("c", 4)))
+      val p2 = sc.parallelize(Seq(("a", 11), ("b", 12), ("b", 13), ("d", 14)))
+      val p = p1.hashFullOuterJoin(p2)
+      p should containInAnyOrder (Seq(
+        ("a", (Some(1), Some(11))),
+        ("a", (Some(2), Some(11))),
+        ("b", (Some(3), Some(12))),
+        ("b", (Some(3), Some(13))),
+        ("c", (Some(4), None)),
+        ("d", (None, Some(14)))))
+    }
+  }
+}
