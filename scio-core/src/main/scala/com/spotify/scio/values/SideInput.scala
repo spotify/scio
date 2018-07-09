c35c6012ca9c281661c6273c8fce010be2449955
Julien Tournay <boudhevil@gmail.com>
2018-07-09 18:05:11 +0200

    Do not cache SideInput for Global windows, fix #1190 (#1231)

--- scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
@@ -22,7 +22,7 @@ import java.util.{List => JList, Map => JMap}
 
 import com.spotify.scio.util.JMapWrapper
 import org.apache.beam.sdk.transforms.DoFn
-import org.apache.beam.sdk.transforms.windowing.BoundedWindow
+import org.apache.beam.sdk.transforms.windowing.{BoundedWindow, GlobalWindow}
 import org.apache.beam.sdk.values.PCollectionView
 
 import scala.collection.JavaConverters._
@@ -33,13 +33,15 @@ trait SideInput[T] extends Serializable {
   @transient private var window: BoundedWindow = _
 
   private[values] def get[I, O](context: DoFn[I, O]#ProcessContext): T
+
   def getCache[I, O](context: DoFn[I, O]#ProcessContext, window: BoundedWindow): T = {
-    if (cache == null || this.window != window) {
+    if (cache == null || window == GlobalWindow.INSTANCE || this.window != window) {
       this.window = window
       cache = get(context)
     }
     cache
   }
+
   private[values] val view: PCollectionView[_]
 }
 
