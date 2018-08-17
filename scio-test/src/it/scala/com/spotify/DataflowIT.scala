7e97fa3b631b3962f3eee7dc3cbc8f468dc8081c
Neville Li <neville@spotify.com>
2018-08-17 16:28:46 -0400

    move DataflowIT to com.spotify package

--- scio-test/src/it/scala/com/spotify/DataflowIT.scala
+++ scio-test/src/it/scala/com/spotify/DataflowIT.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Spotify AB.
+ * Copyright 2018 Spotify AB.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package com.spotify
 
 import com.spotify.scio._
 import com.spotify.scio.runners.dataflow._
