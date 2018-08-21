aa996140869da0b45c2de832833550b8c75544e5
Neville Li <neville@spotify.com>
2018-08-21 14:45:31 -0400

    clean up CustomIO

--- scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
@@ -1038,7 +1038,7 @@ sealed trait SCollection[T] extends PCollectionWrapper[T] {
   def saveAsCustomOutput[O <: POutput](name: String, transform: PTransform[PCollection[T], O])
   : Future[Tap[T]] = {
     if (context.isTest) {
-      context.testOut(CustomIO[T](name))(this)
+      context.testOut(nio.CustomIO[T](name))(this)
     } else {
       this.internal.apply(name, transform)
     }
