96a5aeb6ebe0d2d7089e56af2331b0c85f820739
Claire McGinty <clairem@spotify.com>
2018-08-27 12:05:22 -0400

    localize isTest/requireNotClosed usages

--- scio-core/src/main/scala/com/spotify/scio/nio/PubSubIO.scala
+++ scio-core/src/main/scala/com/spotify/scio/nio/PubSubIO.scala
@@ -37,7 +37,7 @@ sealed trait PubSubIO[T] extends ScioIO[T] {
   type ReadP = ReadParams
   type WriteP = Unit
 
-  def tap(params: ReadParams): Tap[T] = ???
+  def tap(params: ReadParams): Tap[T] = throw new NotImplementedError("Pubsub tap not implemented")
 }
 
 object PubSubIO {
@@ -61,74 +61,66 @@ private case class PubSubIOWithoutAttributes[T: ClassTag](name: String,
 
   def id: String = name
 
-  def read(sc: ScioContext, params: ReadP): SCollection[T] = sc.requireNotClosed {
-    if (sc.isTest) {
-      sc.getTestInput(this.asInstanceOf[ScioIO[T]])
-    } else {
-      def setup[U](read: BeamPubSubIO.Read[U]) = {
-        var r = read
-        r = if (params.isSubscription) r.fromSubscription(name) else r.fromTopic(name)
-        if (idAttribute != null) {
-          r = r.withIdAttribute(idAttribute)
-        }
-        if (timestampAttribute != null) {
-          r = r.withTimestampAttribute(timestampAttribute)
-        }
-        r
+  def read(sc: ScioContext, params: ReadP): SCollection[T] = {
+    def setup[U](read: BeamPubSubIO.Read[U]) = {
+      var r = read
+      r = if (params.isSubscription) r.fromSubscription(name) else r.fromTopic(name)
+      if (idAttribute != null) {
+        r = r.withIdAttribute(idAttribute)
       }
+      if (timestampAttribute != null) {
+        r = r.withTimestampAttribute(timestampAttribute)
+      }
+      r
+    }
 
-      if (classOf[String] isAssignableFrom cls) {
-        val t = setup(BeamPubSubIO.readStrings())
-        sc.wrap(sc.applyInternal(t)).setName(name).asInstanceOf[SCollection[T]]
-      } else if (classOf[SpecificRecordBase] isAssignableFrom cls) {
-        val t = setup(BeamPubSubIO.readAvros(cls))
-        sc.wrap(sc.applyInternal(t)).setName(name)
-      } else if (classOf[Message] isAssignableFrom cls) {
-        val t = setup(BeamPubSubIO.readProtos(cls.asSubclass(classOf[Message])))
-        sc.wrap(sc.applyInternal(t)).setName(name).asInstanceOf[SCollection[T]]
-      } else {
-        val coder = sc.pipeline.getCoderRegistry.getScalaCoder[T](sc.options)
-        val t = setup(BeamPubSubIO.readMessages())
-        sc.wrap(sc.applyInternal(t)).setName(name)
-          .map(m => CoderUtils.decodeFromByteArray(coder, m.getPayload))
-      }
+    if (classOf[String] isAssignableFrom cls) {
+      val t = setup(BeamPubSubIO.readStrings())
+      sc.wrap(sc.applyInternal(t)).setName(name).asInstanceOf[SCollection[T]]
+    } else if (classOf[SpecificRecordBase] isAssignableFrom cls) {
+      val t = setup(BeamPubSubIO.readAvros(cls))
+      sc.wrap(sc.applyInternal(t)).setName(name)
+    } else if (classOf[Message] isAssignableFrom cls) {
+      val t = setup(BeamPubSubIO.readProtos(cls.asSubclass(classOf[Message])))
+      sc.wrap(sc.applyInternal(t)).setName(name).asInstanceOf[SCollection[T]]
+    } else {
+      val coder = sc.pipeline.getCoderRegistry.getScalaCoder[T](sc.options)
+      val t = setup(BeamPubSubIO.readMessages())
+      sc.wrap(sc.applyInternal(t)).setName(name)
+        .map(m => CoderUtils.decodeFromByteArray(coder, m.getPayload))
     }
   }
 
   def write(data: SCollection[T], params: WriteP): Future[Tap[T]] = {
-    if (data.context.isTest) {
-      data.context.testOut[T](this.asInstanceOf[ScioIO[T]])
+    def setup[U](write: BeamPubSubIO.Write[U]) = {
+      var w = write.to(name)
+      if (idAttribute != null) {
+        w = w.withIdAttribute(idAttribute)
+      }
+      if (timestampAttribute != null) {
+        w = w.withTimestampAttribute(timestampAttribute)
+      }
+      w
+    }
+    if (classOf[String] isAssignableFrom cls) {
+      val t = setup(BeamPubSubIO.writeStrings())
+      data
+        .asInstanceOf[SCollection[String]]
+        .applyInternal(t)
+    } else if (classOf[SpecificRecordBase] isAssignableFrom cls) {
+      val t = setup(BeamPubSubIO.writeAvros(cls))
+      data.applyInternal(t)
+    } else if (classOf[Message] isAssignableFrom cls) {
+      val t = BeamPubSubIO.writeProtos(cls.asInstanceOf[Class[Message]])
+      data.asInstanceOf[SCollection[Message]].applyInternal(t)
     } else {
-      def setup[U](write: BeamPubSubIO.Write[U]) = {
-        var w = write.to(name)
-        if (idAttribute != null) {
-          w = w.withIdAttribute(idAttribute)
-        }
-        if (timestampAttribute != null) {
-          w = w.withTimestampAttribute(timestampAttribute)
-        }
-        w
-      }
-      if (classOf[String] isAssignableFrom cls) {
-        val t = setup(BeamPubSubIO.writeStrings())
-        data
-          .asInstanceOf[SCollection[String]]
-          .applyInternal(t)
-      } else if (classOf[SpecificRecordBase] isAssignableFrom cls) {
-        val t = setup(BeamPubSubIO.writeAvros(cls))
-        data.applyInternal(t)
-      } else if (classOf[Message] isAssignableFrom cls) {
-        val t = BeamPubSubIO.writeProtos(cls.asInstanceOf[Class[Message]])
-        data.asInstanceOf[SCollection[Message]].applyInternal(t)
-      } else {
-        val coder = data.internal.getPipeline.getCoderRegistry
-          .getScalaCoder[T](data.context.options)
-        val t = setup(BeamPubSubIO.writeMessages())
-        data.map { record =>
-          val payload = CoderUtils.encodeToByteArray(coder, record)
-          new PubsubMessage(payload, Map.empty[String, String].asJava)
-        }.applyInternal(t)
-      }
+      val coder = data.internal.getPipeline.getCoderRegistry
+        .getScalaCoder[T](data.context.options)
+      val t = setup(BeamPubSubIO.writeMessages())
+      data.map { record =>
+        val payload = CoderUtils.encodeToByteArray(coder, record)
+        new PubsubMessage(payload, Map.empty[String, String].asJava)
+      }.applyInternal(t)
     }
     Future.failed(new NotImplementedError("Pubsub future not implemented"))
   }
@@ -142,48 +134,39 @@ private case class PubSubIOWithAttributes[T: ClassTag](name: String,
 
   def id: String = name
 
-  def read(sc: ScioContext, params: ReadP): SCollection[WithAttributeMap] =
-    sc.requireNotClosed {
-      if (sc.isTest) {
-        sc.getTestInput(this.asInstanceOf[ScioIO[WithAttributeMap]])
-      } else {
-        var r = BeamPubSubIO.readMessagesWithAttributes()
-        r = if (params.isSubscription) r.fromSubscription(name) else r.fromTopic(name)
-        if (idAttribute != null) {
-          r = r.withIdAttribute(idAttribute)
-        }
-        if (timestampAttribute != null) {
-          r = r.withTimestampAttribute(timestampAttribute)
-        }
-
-        val elementCoder = sc.pipeline.getCoderRegistry.getScalaCoder[T](sc.options)
-        sc.wrap(sc.applyInternal(r)).setName(name)
-          .map { m =>
-            val payload = CoderUtils.decodeFromByteArray(elementCoder, m.getPayload)
-            val attributes = JMapWrapper.of(m.getAttributeMap)
-            (payload, attributes)
-          }
+  def read(sc: ScioContext, params: ReadP): SCollection[WithAttributeMap] = {
+      var r = BeamPubSubIO.readMessagesWithAttributes()
+      r = if (params.isSubscription) r.fromSubscription(name) else r.fromTopic(name)
+      if (idAttribute != null) {
+        r = r.withIdAttribute(idAttribute)
       }
+      if (timestampAttribute != null) {
+        r = r.withTimestampAttribute(timestampAttribute)
+      }
+
+      val elementCoder = sc.pipeline.getCoderRegistry.getScalaCoder[T](sc.options)
+      sc.wrap(sc.applyInternal(r)).setName(name)
+        .map { m =>
+          val payload = CoderUtils.decodeFromByteArray(elementCoder, m.getPayload)
+          val attributes = JMapWrapper.of(m.getAttributeMap)
+          (payload, attributes)
+        }
     }
 
   def write(data: SCollection[WithAttributeMap], params: WriteP): Future[Tap[WithAttributeMap]] = {
-    if (data.context.isTest) {
-      data.context.testOut[T](this.asInstanceOf[ScioIO[T]])
-    } else {
-      var w = BeamPubSubIO.writeMessages().to(name)
-      if (idAttribute != null) {
-        w = w.withIdAttribute(idAttribute)
-      }
-      if (timestampAttribute != null) {
-        w = w.withTimestampAttribute(timestampAttribute)
-      }
-      val coder = data.internal.getPipeline.getCoderRegistry.getScalaCoder[T](data.context.options)
-      data.map { kv =>
-        val payload = CoderUtils.encodeToByteArray(coder, kv._1)
-        val attributes = kv._2.asJava
-        new PubsubMessage(payload, attributes)
-      }.applyInternal(w)
+    var w = BeamPubSubIO.writeMessages().to(name)
+    if (idAttribute != null) {
+      w = w.withIdAttribute(idAttribute)
     }
+    if (timestampAttribute != null) {
+      w = w.withTimestampAttribute(timestampAttribute)
+    }
+    val coder = data.internal.getPipeline.getCoderRegistry.getScalaCoder[T](data.context.options)
+    data.map { kv =>
+      val payload = CoderUtils.encodeToByteArray(coder, kv._1)
+      val attributes = kv._2.asJava
+      new PubsubMessage(payload, attributes)
+    }.applyInternal(w)
     Future.failed(new NotImplementedError("Pubsub future not implemented"))
   }
 }
