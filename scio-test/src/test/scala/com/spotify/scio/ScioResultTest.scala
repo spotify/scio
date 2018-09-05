f47b137b8bc0b38940f9de02bb38a767050dceb0
Neville Li <neville@spotify.com>
2018-09-05 06:11:30 -0400

    fix ScioResultTest

--- scio-test/src/test/scala/com/spotify/scio/ScioResultTest.scala
+++ scio-test/src/test/scala/com/spotify/scio/ScioResultTest.scala
@@ -30,6 +30,7 @@ import org.joda.time
 
 import scala.concurrent.Future
 import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ScioResultTest extends PipelineSpec {
 
