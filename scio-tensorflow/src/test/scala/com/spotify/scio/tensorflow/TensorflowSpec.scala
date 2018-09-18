f454270424a9a0b73ad86f568042cb00fb59c960
Filipe Regadas <filiperegadas@gmail.com>
2018-09-18 10:48:21 +0200

    Remove deprecated tensorflow graph prediction method (#1370)

--- scio-tensorflow/src/test/scala/com/spotify/scio/tensorflow/TensorflowSpec.scala
+++ scio-tensorflow/src/test/scala/com/spotify/scio/tensorflow/TensorflowSpec.scala
@@ -17,12 +17,11 @@
 
 package com.spotify.scio.tensorflow
 
-import java.nio.file.Files
 import java.util.Collections
 
-import com.spotify.featran.scio._
-import com.spotify.featran.tensorflow._
 import com.spotify.featran.FeatureSpec
+import com.spotify.featran.tensorflow._
+import com.spotify.featran.scio._
 import com.spotify.featran.transformers.StandardScaler
 import com.spotify.scio.ContextAndArgs
 import com.spotify.scio.io._
@@ -32,36 +31,6 @@ import org.tensorflow._
 import org.tensorflow.example.Example
 
 import scala.io.Source
-
-private object TFGraphJob {
-
-  def main(argv: Array[String]): Unit = {
-    val (sc, args) = ContextAndArgs(argv)
-    sc.parallelize(1L to 10)
-      .predict(args("graphURI"), Seq("multiply")) { e =>
-        Map("input" -> Tensors.create(e))
-      } { (r, o) =>
-        (r, o.map { case (_, t) => t.longValue() }.head)
-      }
-      .saveAsTextFile(args("output"))
-    sc.close().waitUntilDone()
-  }
-}
-
-private object TFGraphJob2Inputs {
-
-  def main(argv: Array[String]): Unit = {
-    val (sc, args) = ContextAndArgs(argv)
-    sc.parallelize(1L to 10)
-      .predict(args("graphURI"), Seq("multiply")) { e =>
-        Map("input" -> Tensors.create(e), "input2" -> Tensors.create(3L))
-      } { (r, o) =>
-        (r, o.map { case (_, t) => t.longValue() }.head)
-      }
-      .saveAsTextFile(args("output"))
-    sc.close().waitUntilDone()
-  }
-}
 
 private[tensorflow] object TFSavedJob {
 
@@ -107,106 +76,6 @@ private[tensorflow] object TFSavedJob {
 }
 
 class TensorflowSpec extends PipelineSpec {
-
-  private def createHelloWorldGraph = {
-    val const = "MyConst"
-    val graph = new Graph()
-    val helloworld = s"Hello from ${TensorFlow.version()}"
-    val t = Tensors.create(helloworld)
-    graph.opBuilder("Const", const).setAttr("dtype", t.dataType()).setAttr("value", t).build()
-    t.close()
-    (graph, const, helloworld)
-  }
-
-  "Tensorflow" should "work for hello-wold" in {
-    val (graph, const, helloworld) = createHelloWorldGraph
-    val session = new Session(graph)
-    val r = session.runner().fetch(const).run().get(0)
-    try {
-      new String(r.bytesValue()) should be(helloworld)
-    } finally {
-      session.close()
-      graph.close()
-      r.close()
-    }
-  }
-
-  it should "work for serde model" in {
-    val (graph, const, helloworld) = createHelloWorldGraph
-    val session = new Session(graph)
-    val r = session.runner().fetch(const).run().get(0)
-    val newGraph = new Graph()
-    try {
-      val graphFile = Files.createTempFile("tf-graph", ".bin")
-      Files.write(graphFile, graph.toGraphDef)
-      newGraph.importGraphDef(Files.readAllBytes(graphFile))
-      new String(r.bytesValue()) should be(helloworld)
-    } finally {
-      session.close()
-      graph.close()
-      newGraph.close()
-      r.close()
-    }
-  }
-
-  it should "allow to predict" in {
-    val g = new Graph()
-    val t3 = Tensors.create(3L)
-    val graphFile = Files.createTempFile("tf-graph", ".bin")
-    try {
-      val input = g.opBuilder("Placeholder", "input").setAttr("dtype", t3.dataType).build.output(0)
-      val c3 = g
-        .opBuilder("Const", "c3")
-        .setAttr("dtype", t3.dataType)
-        .setAttr("value", t3)
-        .build
-        .output(0)
-      g.opBuilder("Mul", "multiply").addInput(c3).addInput(input).build()
-
-      Files.write(graphFile, g.toGraphDef)
-
-      JobTest[TFGraphJob.type]
-        .args(s"--graphURI=${graphFile.toUri}", "--output=output")
-        .output(TextIO("output")) {
-          _ should containInAnyOrder((1L to 10).map(x => (x, x * 3)).map(_.toString))
-        }
-        .run()
-    } finally {
-      g.close()
-      t3.close()
-      graphFile.toFile.deleteOnExit()
-    }
-  }
-
-  it should "allow to predict with 2 inputs" in {
-    val g = new Graph()
-    val graphFile = Files.createTempFile("tf-graph", ".bin")
-    try {
-      val input = g
-        .opBuilder("Placeholder", "input")
-        .setAttr("dtype", DataType.INT64)
-        .build
-        .output(0)
-      val input2 = g
-        .opBuilder("Placeholder", "input2")
-        .setAttr("dtype", DataType.INT64)
-        .build
-        .output(0)
-      g.opBuilder("Mul", "multiply").addInput(input2).addInput(input).build()
-
-      Files.write(graphFile, g.toGraphDef)
-
-      JobTest[TFGraphJob2Inputs.type]
-        .args(s"--graphURI=${graphFile.toUri}", "--output=output")
-        .output(TextIO("output")) {
-          _ should containInAnyOrder((1L to 10).map(x => (x, x * 3)).map(_.toString))
-        }
-        .run()
-    } finally {
-      g.close()
-      graphFile.toFile.deleteOnExit()
-    }
-  }
 
   it should "allow saved model prediction" in {
     val resource = getClass.getResource("/trained_model")
