e7938d49f5d5bfdf0d438faf15fb11f2a4caf418
Filipe Regadas <filiperegadas@gmail.com>
2018-09-21 16:11:38 +0200

    Fix format

--- scio-jmh/src/test/scala/com/spotify/scio/jmh/KryoAtomicCoderBenchmark.scala
+++ scio-jmh/src/test/scala/com/spotify/scio/jmh/KryoAtomicCoderBenchmark.scala
@@ -62,14 +62,17 @@ class KryoAtomicCoderBenchmark {
   val derivedListCoder =
     CoderMaterializer.beamWithDefault(Coder[List[SpecializedUserForDerived]])
 
-
   val specializedMapKryoCoder = new KryoAtomicCoder[Map[String, Long]](KryoOptions())
   val derivedMapCoder = CoderMaterializer.beamWithDefault(Coder[Map[String, Long]])
-  val mapExample = (1 to 1000).map { x => (s"stringvalue$x", x.toLong) }.toMap
+  val mapExample = (1 to 1000).map { x =>
+    (s"stringvalue$x", x.toLong)
+  }.toMap
 
   val specializedStringListKryoCoder = new KryoAtomicCoder[List[String]](KryoOptions())
   val derivedStringListCoder = CoderMaterializer.beamWithDefault(Coder[List[String]])
-  val stringListExample = (1 to 1000).map { x => s"stringvalue$x" }.toList
+  val stringListExample = (1 to 1000).map { x =>
+    s"stringvalue$x"
+  }.toList
 
   @Benchmark
   def kryoEncode: Array[Byte] =
