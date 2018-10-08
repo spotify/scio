b09aa26b74a89ee373a5d4acdaf3026e3f58adf5
Julien Tournay <boudhevil@gmail.com>
2018-10-08 17:58:17 +0200

    Add a test for MinimalWordCountTypedArguments (#1437)

--- scio-examples/src/test/scala/com/spotify/scio/examples/WordCountTest.scala
+++ scio-examples/src/test/scala/com/spotify/scio/examples/WordCountTest.scala
@@ -41,6 +41,14 @@ class WordCountTest extends PipelineSpec {
       .run()
   }
 
+  "MinimalWordCountTypedArguments" should "work" in {
+    JobTest[com.spotify.scio.examples.MinimalWordCountTypedArguments.type]
+      .args("--input=in.txt", "--output=out.txt")
+      .input(TextIO("in.txt"), inData)
+      .output(TextIO("out.txt"))(_ should containInAnyOrder(expected))
+      .run()
+  }
+
   "DebuggingWordCount" should "work" in {
     val in = Seq("Flourish a b", "Flourish c d", "Flourish e", "stomach a") ++ (1 to 100)
       .map("x" * _)
