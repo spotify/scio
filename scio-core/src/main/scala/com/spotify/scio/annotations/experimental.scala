727f6661647322a797187b03f9b31394b8951b5e
Filipe Regadas <filiperegadas@gmail.com>
2018-10-29 10:58:15 -0400

    Add experimental annotation

---
+++ scio-core/src/main/scala/com/spotify/scio/annotations/experimental.scala
@@ -0,0 +1,11 @@
+package com.spotify.scio.annotations
+
+/**
+ * Public API's (public class, method or field) marked with this annotation is subject
+ * to incompatible changes, or even removal, in a future release.
+ *
+ * <p>Note that the presence of this annotation implies nothing about the quality or performance of
+ * the API in question, only the fact that the API or behavior may change in any way.</p>
+ */
+//scalastyle:off class.name
+final class experimental extends scala.annotation.StaticAnnotation
