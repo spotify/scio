3914b9c7a6c838eb1c733d6c590a718cce5e25bd
Filipe Regadas <filiperegadas@gmail.com>
2018-11-01 14:59:04 -0400

    Cleaner config

--- scripts/circleci_setup.sh
+++ scripts/circleci_setup.sh
@@ -12,25 +12,9 @@ fi
 if [ -n "$CIRCLE_PR_USERNAME" ]; then
   echo "Running tests for Scala $SCALA_VERSION, forked PR #$CIRCLE_PR_NUMBER from $CIRCLE_PR_USERNAME/$CIRCLE_PR_REPONAME"
   PROPS="-Dbigquery.project=dummy-project"
-  TESTS="test"
 else
   echo "Running tests for Scala $SCALA_VERSION, branch: $CIRCLE_BRANCH"
   PROPS="-Dbigquery.project=data-integration-test -Dbigquery.secret=$DIR_OF_SCRIPT/$JSON_KEY"
-  TESTS="test it:test"
 fi
 
 "$DIR_OF_SCRIPT/gen_schemas.sh"
-
-if [ $SCOVERAGE -eq 1 ]; then
-  sbt -v $PROPS ++$SCALA_VERSION \
-    scalafmtCheck test:scalafmtCheck it:scalafmtCheck scalafmtSbtCheck \
-    scalastyle \
-    coverage \
-    $TESTS \
-    coverageReport coverageAggregate
-else
-  sbt -v $PROPS ++$SCALA_VERSION \
-    scalafmtCheck test:scalafmtCheck it:scalafmtCheck scalafmtSbtCheck \
-    scalastyle \
-    $TESTS
-fi
