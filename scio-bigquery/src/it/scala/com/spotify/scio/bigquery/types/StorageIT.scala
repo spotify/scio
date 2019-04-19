398450f505bc02508a9d0cbf799e14b482ded39a
Filipe Regadas <filiperegadas@gmail.com>
2019-04-19 08:21:05 -0400

    Update scio-bigquery/src/it/scala/com/spotify/scio/bigquery/types/StorageIT.scala
    Co-Authored-By: nevillelyh <neville.lyh@gmail.com>

--- scio-bigquery/src/it/scala/com/spotify/scio/bigquery/types/StorageIT.scala
+++ scio-bigquery/src/it/scala/com/spotify/scio/bigquery/types/StorageIT.scala
@@ -109,7 +109,9 @@ class StorageIT extends FlatSpec with Matchers {
     val expected = (0 until 10).map { i =>
       (i.toLong, s"s$i", i.toLong)
     }.asJava
-    val sc = ScioContext()
+    val (sc, _) = ContextAndArgs(
+      Array("--project=data-integration-test", "--tempLocation=gs://data-integration-test-eu/temp")
+    )
     val p = sc
       .typedBigQuery[NestedWithFields]()
       .map { r =>
