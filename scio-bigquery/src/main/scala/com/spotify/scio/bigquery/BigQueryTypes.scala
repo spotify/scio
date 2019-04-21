f3338089ccd74d7149a7997c83e8fee89672aafb
Filipe Regadas <filiperegadas@gmail.com>
2019-04-21 13:45:35 -0400

    Rename Formatter and Parser

--- scio-bigquery/src/main/scala/com/spotify/scio/bigquery/BigQueryTypes.scala
+++ scio-bigquery/src/main/scala/com/spotify/scio/bigquery/BigQueryTypes.scala
@@ -44,10 +44,10 @@ object TableRow {
 /** Utility for BigQuery `TIMESTAMP` type. */
 object Timestamp {
   // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.DDDDDD]][time zone]
-  private[this] val formatter =
+  private[this] val Formatter =
     DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS ZZZ")
 
-  private[this] val parser = new DateTimeFormatterBuilder()
+  private[this] val Parser = new DateTimeFormatterBuilder()
     .append(DateTimeFormat.forPattern("yyyy-MM-dd"))
     .appendOptional(
       new DateTimeFormatterBuilder()
@@ -70,14 +70,14 @@ object Timestamp {
     .withZoneUTC()
 
   /** Convert `Instant` to BigQuery `TIMESTAMP` string. */
-  def apply(instant: Instant): String = formatter.print(instant)
+  def apply(instant: Instant): String = Formatter.print(instant)
 
   /** Convert millisecond instant to BigQuery `TIMESTAMP` string. */
-  def apply(instant: Long): String = formatter.print(instant)
+  def apply(instant: Long): String = Formatter.print(instant)
 
   /** Convert BigQuery `TIMESTAMP` string to `Instant`. */
   def parse(timestamp: String): Instant =
-    parser.parseDateTime(timestamp).toInstant
+    Parser.parseDateTime(timestamp).toInstant
 
   // For BigQueryType macros only, do not use directly
   def parse(timestamp: Any): Instant = timestamp match {
@@ -90,14 +90,14 @@ object Timestamp {
 /** Utility for BigQuery `DATE` type. */
 object Date {
   // YYYY-[M]M-[D]D
-  private[this] val formatter =
+  private[this] val Formatter =
     DateTimeFormat.forPattern("yyyy-MM-dd").withZoneUTC()
 
   /** Convert `LocalDate` to BigQuery `DATE` string. */
-  def apply(date: LocalDate): String = formatter.print(date)
+  def apply(date: LocalDate): String = Formatter.print(date)
 
   /** Convert BigQuery `DATE` string to `LocalDate`. */
-  def parse(date: String): LocalDate = LocalDate.parse(date, formatter)
+  def parse(date: String): LocalDate = LocalDate.parse(date, Formatter)
 
   // For BigQueryType macros only, do not use directly
   def parse(date: Any): LocalDate = date match {
@@ -109,19 +109,19 @@ object Date {
 /** Utility for BigQuery `TIME` type. */
 object Time {
   // [H]H:[M]M:[S]S[.DDDDDD]
-  private[this] val formatter =
+  private[this] val Formatter =
     DateTimeFormat.forPattern("HH:mm:ss.SSSSSS").withZoneUTC()
-  private[this] val parser = new DateTimeFormatterBuilder()
+  private[this] val Parser = new DateTimeFormatterBuilder()
     .append(DateTimeFormat.forPattern("HH:mm:ss").getParser)
     .appendOptional(DateTimeFormat.forPattern(".SSSSSS").getParser)
     .toFormatter
     .withZoneUTC()
 
   /** Convert `LocalTime` to BigQuery `TIME` string. */
-  def apply(time: LocalTime): String = formatter.print(time)
+  def apply(time: LocalTime): String = Formatter.print(time)
 
   /** Convert BigQuery `TIME` string to `LocalTime`. */
-  def parse(time: String): LocalTime = parser.parseLocalTime(time)
+  def parse(time: String): LocalTime = Parser.parseLocalTime(time)
 
   // For BigQueryType macros only, do not use directly
   def parse(time: Any): LocalTime = time match {
@@ -133,10 +133,10 @@ object Time {
 /** Utility for BigQuery `DATETIME` type. */
 object DateTime {
   // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.DDDDDD]]
-  private[this] val formatter =
+  private[this] val Formatter =
     DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
 
-  private[this] val parser = new DateTimeFormatterBuilder()
+  private[this] val Parser = new DateTimeFormatterBuilder()
     .append(DateTimeFormat.forPattern("yyyy-MM-dd"))
     .appendOptional(
       new DateTimeFormatterBuilder()
@@ -154,11 +154,11 @@ object DateTime {
     .withZoneUTC()
 
   /** Convert `LocalDateTime` to BigQuery `DATETIME` string. */
-  def apply(datetime: LocalDateTime): String = formatter.print(datetime)
+  def apply(datetime: LocalDateTime): String = Formatter.print(datetime)
 
   /** Convert BigQuery `DATETIME` string to `LocalDateTime`. */
   def parse(datetime: String): LocalDateTime =
-    parser.parseLocalDateTime(datetime)
+  Parser.parseLocalDateTime(datetime)
 }
 
 /**
@@ -184,8 +184,8 @@ object Numeric {
   val MaxNumericPrecision = 38
   val MaxNumericScale = 9
 
-  private[this] val conversions = new DecimalConversion
-  private[this] val logicalType = LogicalTypes.decimal(MaxNumericPrecision, MaxNumericScale)
+  private[this] val DecimalConverter = new DecimalConversion
+  private[this] val DecimalLogicalType = LogicalTypes.decimal(MaxNumericPrecision, MaxNumericScale)
 
   def apply(value: String): BigDecimal = apply(BigDecimal(value))
 
@@ -206,7 +206,7 @@ object Numeric {
 
   // For BigQueryType macros only, do not use directly
   def parse(value: Any): BigDecimal = value match {
-    case b: ByteBuffer => conversions.fromBytes(b, null, logicalType)
+    case b: ByteBuffer => DecimalConverter.fromBytes(b, null, DecimalLogicalType)
     case _             => apply(value.toString)
   }
 }
