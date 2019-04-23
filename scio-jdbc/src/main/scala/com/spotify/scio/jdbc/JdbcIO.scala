f7670f2fa43eb25c1c29843dbb721b9743ae5e9b
Filipe Regadas <filiperegadas@gmail.com>
2019-04-23 11:15:19 -0400

    Replace IllegalStateException with UnsupportedOperationException (#1861)

--- scio-jdbc/src/main/scala/com/spotify/scio/jdbc/JdbcIO.scala
+++ scio-jdbc/src/main/scala/com/spotify/scio/jdbc/JdbcIO.scala
@@ -82,7 +82,7 @@ final case class JdbcSelect[T: Coder](readOptions: JdbcReadOptions[T]) extends S
   }
 
   override def write(data: SCollection[T], params: WriteP): Tap[Nothing] =
-    throw new IllegalStateException("jdbc.Select is read-only")
+    throw new UnsupportedOperationException("jdbc.Select is read-only")
 
   override def tap(params: ReadP): Tap[Nothing] =
     EmptyTap
@@ -97,7 +97,7 @@ final case class JdbcWrite[T](writeOptions: JdbcWriteOptions[T]) extends ScioIO[
   override def testId: String = s"JdbcIO(${JdbcIO.jdbcIoId(writeOptions)})"
 
   override def read(sc: ScioContext, params: ReadP): SCollection[T] =
-    throw new IllegalStateException("jdbc.Write is write-only")
+    throw new UnsupportedOperationException("jdbc.Write is write-only")
 
   override def write(data: SCollection[T], params: WriteP): Tap[Nothing] = {
     var transform = beam.JdbcIO
