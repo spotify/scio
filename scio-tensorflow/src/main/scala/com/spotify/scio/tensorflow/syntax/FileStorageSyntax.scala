de56298e6207e68d7dfa6721c1b1c7b2b1fbd915
Filipe Regadas <filiperegadas@gmail.com>
2019-05-07 14:19:32 -0400

    Fix FileStorage implicit access modifier

--- scio-tensorflow/src/main/scala/com/spotify/scio/tensorflow/syntax/FileStorageSyntax.scala
+++ scio-tensorflow/src/main/scala/com/spotify/scio/tensorflow/syntax/FileStorageSyntax.scala
@@ -47,6 +47,6 @@ final class FileStorageOps(private val self: FileStorage) extends AnyVal {
 }
 
 trait FileStorageSyntax {
-  implicit def tensorFlowFileStorageFunctions(s: FileStorage): FileStorageOps =
+  private[scio] implicit def tensorFlowFileStorageFunctions(s: FileStorage): FileStorageOps =
     new FileStorageOps(s)
 }
