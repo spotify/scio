bb0047df38fc2a6767adae4b8f81a2cfcd2948b6
Filipe Regadas <filiperegadas@gmail.com>
2019-07-11 11:24:50 -0400

    Add bq not implemented tap when write append (#2042)

--- scio-core/src/main/scala/com/spotify/scio/io/Tap.scala
+++ scio-core/src/main/scala/com/spotify/scio/io/Tap.scala
@@ -65,6 +65,11 @@ case object EmptyTap extends Tap[Nothing] {
   override def open(sc: ScioContext): SCollection[Nothing] = sc.empty[Nothing]
 }
 
+case class UnsupportedTap[T](msg: String) extends Tap[T] {
+  override def value: Iterator[T] = throw new UnsupportedOperationException(msg)
+  override def open(sc: ScioContext): SCollection[T] = throw new UnsupportedOperationException(msg)
+}
+
 /** Tap for text files on local file system or GCS. */
 final case class TextTap(path: String) extends Tap[String] {
   override def value: Iterator[String] = FileStorage(path).textFile
