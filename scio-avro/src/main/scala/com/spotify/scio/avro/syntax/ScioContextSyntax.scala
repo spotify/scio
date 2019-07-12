a570df173c0f44fd7f96c104b0d9f4a9f2735518
Anish Chakraborty <anish749@users.noreply.github.com>
2019-07-12 20:42:22 +0200

    fix style

--- scio-avro/src/main/scala/com/spotify/scio/avro/syntax/ScioContextSyntax.scala
+++ scio-avro/src/main/scala/com/spotify/scio/avro/syntax/ScioContextSyntax.scala
@@ -70,9 +70,9 @@ final class ScioContextOps(private val self: ScioContext) extends AnyVal {
    *
    * {{{
    *   val sColl: SCollection[CaseClass] =
-   *     sc.parseAvroFile("gs://.....")(
-   *       g => CaseClass(g.get("id").asInstanceOf[Int], g.get("name").asInstanceOf[String])
-   *     )
+   *     sc.parseAvroFile("gs://.....") { g => 
+   *       CaseClass(g.get("id").asInstanceOf[Int], g.get("name").asInstanceOf[String])
+   *     }
    * }}}
    *
    */
