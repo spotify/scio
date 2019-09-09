d1a65b08514876d13b1a488b51fdfc7d1e1d5aab
Filipe Regadas <filiperegadas@gmail.com>
2019-09-09 09:44:47 +0100

    Use scala 2.12.8 for site gen (#2212)

--- scripts/make-site.sh
+++ scripts/make-site.sh
@@ -2,7 +2,8 @@
 
 # generate new site locally
 # target/site/index.html
-SOCCO=true sbt scio-examples/clean scio-examples/compile site/makeSite
+# using scala 2.12.8 due to https://github.com/criteo/socco/pull/6
+SOCCO=true sbt ++2.12.8 scio-examples/clean scio-examples/compile site/makeSite
 
 # generate new site and push to GitHub
 #SOCCO=true sbt scio-examples/clean scio-examples/compile site/ghpagesPushSite
