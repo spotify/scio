9cf93e5ca745b295efb1908929b2d6d40ab5b29e
Neville Li <neville@spotify.com>
2019-10-02 16:10:49 -0400

    fix path in gen_schemas.sh

--- scripts/gen_schemas.sh
+++ scripts/gen_schemas.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Restoring cached BigQuery schemas"
 
-SCHEMAS_DIR=/tmp/scio-bigquery-circleci/.bigquery/
+SCHEMAS_DIR=/tmp/scio-bigquery-$USER/.bigquery/
 
 mkdir -p $SCHEMAS_DIR
 cp -v scripts/bigquery/* $SCHEMAS_DIR
