a80160a2f8536721e187f67b98e104fd24946921
Filipe Regadas <filiperegadas@gmail.com>
2019-11-18 14:52:25 -0500

    Deprecate scio-elasticsearch2 and scio-cassandra2

--- scio-elasticsearch/es2/src/main/scala/com/spotify/scio/elasticsearch/package.scala
+++ scio-elasticsearch/es2/src/main/scala/com/spotify/scio/elasticsearch/package.scala
@@ -49,6 +49,7 @@ package object elasticsearch {
      *                   number of pipeline workers
      * @param errorFn function to handle error when performing Elasticsearch bulk writes
      */
+    @deprecated("scio-elasticsearch2 will be removed", "0.8.0")
     def saveAsElasticsearch(
       esOptions: ElasticsearchOptions,
       flushInterval: Duration = WriteParam.DefaultFlushInterval,
