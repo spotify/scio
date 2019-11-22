spotify/scio

commit e6976fc0c637eb6e4888d49cbd336ad1a409b702

Better consistency around BigQuery API (#2412)

scio-bigquery/src/main/scala/com/spotify/scio/bigquery/syntax/ScioContextSyntax.scala
@@ -170,13 +170,16 @@ final class ScioContextOps(private val self: ScioContext) extends AnyVal {
     "0.8.0"
   )
   def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](
-    newSource: String = null
+    newSource: String
   ): SCollection[T] = {
     val src = Option(newSource).map { s =>
       Try(Table.Spec(s)).getOrElse(Query(s))
     }.orNull
     typedBigQuery(src)
   }
+
+  def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](): SCollection[T] =
+    self.read(BigQueryTyped.dynamic[T](None))
 
   def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](
     newSource: Source

scio-bigquery/src/main/scala/com/spotify/scio/bigquery/syntax/SCollectionSyntax.scala
@@ -46,6 +46,10 @@ final class SCollectionTableRowOps[T <: TableRow](private val self: SCollection[
    * Save this SCollection as a BigQuery table. Note that elements must be of type
    * [[com.google.api.services.bigquery.model.TableRow TableRow]].
    */
+  @deprecated(
+    "this method will be removed; use saveAsBigQuery(Table.Ref(table)) instead",
+    "0.8.0"
+  )
   def saveAsBigQuery(
     table: TableReference,
     schema: TableSchema,
@@ -69,6 +73,10 @@ final class SCollectionTableRowOps[T <: TableRow](private val self: SCollection[
    * Save this SCollection as a BigQuery table. Note that elements must be of type
    * [[com.google.api.services.bigquery.model.TableRow TableRow]].
    */
+  @deprecated(
+    "this method will be removed; use saveAsBigQuery(Table.Ref(table)) instead",
+    "0.8.0"
+  )
   def saveAsBigQuery(
     tableSpec: String,
     schema: TableSchema = BigQueryTable.WriteParam.DefaultSchema,
@@ -88,6 +96,31 @@ final class SCollectionTableRowOps[T <: TableRow](private val self: SCollection[
     self
       .asInstanceOf[SCollection[TableRow]]
       .write(BigQueryTable(Table.Spec(tableSpec)))(param)
+  }
+
+  /**
+   * Save this SCollection as a BigQuery table. Note that elements must be of type
+   * [[com.google.api.services.bigquery.model.TableRow TableRow]].
+   */
+  def saveAsBigQueryTable(
+    table: Table,
+    schema: TableSchema = BigQueryTable.WriteParam.DefaultSchema,
+    writeDisposition: WriteDisposition = BigQueryTable.WriteParam.DefaultWriteDisposition,
+    createDisposition: CreateDisposition = BigQueryTable.WriteParam.DefaultCreateDisposition,
+    tableDescription: String = BigQueryTable.WriteParam.DefaultTableDescription,
+    timePartitioning: TimePartitioning = BigQueryTable.WriteParam.DefaultTimePartitioning
+  ): ClosedTap[TableRow] = {
+    val param =
+      BigQueryTable.WriteParam(
+        schema,
+        writeDisposition,
+        createDisposition,
+        tableDescription,
+        timePartitioning
+      )
+    self
+      .asInstanceOf[SCollection[TableRow]]
+      .write(BigQueryTable(table))(param)
   }
 
   /**
@@ -132,17 +165,18 @@ final class SCollectionTypedOps[T <: HasAnnotation](private val self: SCollectio
    * Save this SCollection as a BigQuery table. Note that element type `T` must be a case class
    * annotated with [[com.spotify.scio.bigquery.types.BigQueryType.toTable BigQueryType.toTable]].
    */
+  @deprecated(
+    "this method will be removed; use saveAsBigQuery(Table.Ref(table)) instead",
+    "0.8.0"
+  )
   def saveAsTypedBigQuery(
     table: TableReference,
     writeDisposition: WriteDisposition,
     createDisposition: CreateDisposition,
     timePartitioning: TimePartitioning
   )(implicit tt: TypeTag[T], ct: ClassTag[T], coder: Coder[T]): ClosedTap[T] = {
-    val param =
-      TableWriteParam(writeDisposition, createDisposition, timePartitioning)
-    self
-      .write(BigQueryTyped.Table(Table.Ref(table)))(param)
-      .asInstanceOf[ClosedTap[T]]
+    val param = TableWriteParam(writeDisposition, createDisposition, timePartitioning)
+    self.write(BigQueryTyped.Table(Table.Ref(table)))(param)
   }
 
   /**
@@ -175,17 +209,58 @@ final class SCollectionTypedOps[T <: HasAnnotation](private val self: SCollectio
    *   .saveAsTypedBigQuery("myproject:samples.gsod")
    * }}}
    */
+  @deprecated(
+    "this method will be removed; use saveAsBigQuery(Table.Ref(table)) instead",
+    "0.8.0"
+  )
   def saveAsTypedBigQuery(
     tableSpec: String,
     writeDisposition: WriteDisposition = TableWriteParam.DefaultWriteDisposition,
     createDisposition: CreateDisposition = TableWriteParam.DefaultCreateDisposition,
     timePartitioning: TimePartitioning = TableWriteParam.DefaultTimePartitioning
   )(implicit tt: TypeTag[T], ct: ClassTag[T], coder: Coder[T]): ClosedTap[T] = {
-    val param =
-      TableWriteParam(writeDisposition, createDisposition, timePartitioning)
-    self
-      .write(BigQueryTyped.Table[T](Table.Spec(tableSpec)))(param)
-      .asInstanceOf[ClosedTap[T]]
+    val param = TableWriteParam(writeDisposition, createDisposition, timePartitioning)
+    self.write(BigQueryTyped.Table[T](Table.Spec(tableSpec)))(param)
+  }
+
+  /**
+   * Save this SCollection as a BigQuery table. Note that element type `T` must be annotated with
+   * [[com.spotify.scio.bigquery.types.BigQueryType BigQueryType]].
+   *
+   * This could be a complete case class with
+   * [[com.spotify.scio.bigquery.types.BigQueryType.toTable BigQueryType.toTable]]. For example:
+   *
+   * {{{
+   * @BigQueryType.toTable
+   * case class Result(name: String, score: Double)
+   *
+   * val p: SCollection[Result] = // process data and convert elements to Result
+   * p.saveAsTypedBigQueryTable(Table.Spec("myproject:mydataset.mytable"))
+   * }}}
+   *
+   * It could also be an empty class with schema from
+   * [[com.spotify.scio.bigquery.types.BigQueryType.fromSchema BigQueryType.fromSchema]],
+   * [[com.spotify.scio.bigquery.types.BigQueryType.fromTable BigQueryType.fromTable]], or
+   * [[com.spotify.scio.bigquery.types.BigQueryType.fromQuery BigQueryType.fromQuery]]. For
+   * example:
+   *
+   * {{{
+   * @BigQueryType.fromTable("publicdata:samples.gsod")
+   * class Row
+   *
+   * sc.typedBigQuery[Row]()
+   *   .sample(withReplacement = false, fraction = 0.1)
+   *   .saveAsTypedBigQueryTable(Table.Spec("myproject:samples.gsod"))
+   * }}}
+   */
+  def saveAsTypedBigQueryTable(
+    table: Table,
+    timePartitioning: TimePartitioning = TableWriteParam.DefaultTimePartitioning,
+    writeDisposition: WriteDisposition = TableWriteParam.DefaultWriteDisposition,
+    createDisposition: CreateDisposition = TableWriteParam.DefaultCreateDisposition
+  )(implicit tt: TypeTag[T], ct: ClassTag[T], coder: Coder[T]): ClosedTap[T] = {
+    val param = TableWriteParam(writeDisposition, createDisposition, timePartitioning)
+    self.write(BigQueryTyped.Table[T](table))(param)
   }
 }
 

scio-examples/src/main/scala/com/spotify/scio/examples/complete/AutoComplete.scala
@@ -145,7 +145,7 @@ object AutoComplete {
     if (outputToBigqueryTable) {
       tags
         .map(kv => Record(kv._1, kv._2.map(p => Tag(p._1, p._2)).toList))
-        .saveAsTypedBigQuery(args("output"))
+        .saveAsTypedBigQueryTable(Table.Spec(args("output")))
     }
     if (outputToDatastore) {
       val kind = args.getOrElse("kind", "autocomplete-demo")

scio-examples/src/main/scala/com/spotify/scio/examples/complete/StreamingWordExtract.scala
@@ -49,7 +49,7 @@ object StreamingWordExtract {
       .flatMap(_.split("[^a-zA-Z']+").filter(_.nonEmpty))
       .map(_.toUpperCase)
       .map(s => TableRow("string_field" -> s))
-      .saveAsBigQuery(args("output"), schema)
+      .saveAsBigQueryTable(Table.Spec(args("output")), schema)
 
     val result = sc.run()
     exampleUtils.waitToFinish(result.pipelineResult)

scio-examples/src/main/scala/com/spotify/scio/examples/complete/TrafficMaxLaneFlow.scala
@@ -128,7 +128,7 @@ object TrafficMaxLaneFlow {
             ts
           )
       }
-      .saveAsTypedBigQuery(args("output"))
+      .saveAsTypedBigQueryTable(Table.Spec(args("output")))
 
     val result = sc.run()
     exampleUtils.waitToFinish(result.pipelineResult)

scio-examples/src/main/scala/com/spotify/scio/examples/complete/TrafficRoutes.scala
@@ -113,7 +113,7 @@ object TrafficRoutes {
         case (r, ts) =>
           Record(r.route, r.avgSpeed, r.slowdownEvent, ts)
       }
-      .saveAsTypedBigQuery(args("output"))
+      .saveAsTypedBigQueryTable(Table.Spec(args("output")))
 
     val result = sc.run()
     exampleUtils.waitToFinish(result.pipelineResult)

scio-examples/src/main/scala/com/spotify/scio/examples/complete/game/GameStats.scala
@@ -111,7 +111,7 @@ object GameStats {
       // Done using windowing information, convert back to regular `SCollection`
       .toSCollection
       // Save to the BigQuery table defined by "output" in the arguments passed in + "_team" suffix
-      .saveAsTypedBigQuery(args("output") + "_team")
+      .saveAsTypedBigQueryTable(Table.Spec(args("output") + "_team"))
 
     userEvents
     // Window over a variable length of time - sessions end after sessionGap minutes no activity
@@ -141,7 +141,7 @@ object GameStats {
           AvgSessionLength(mean, fmt.print(w.start()))
       }
       // Save to the BigQuery table defined by "output" + "_sessions" suffix
-      .saveAsTypedBigQuery(args("output") + "_sessions")
+      .saveAsTypedBigQueryTable(Table.Spec(args("output") + "_sessions"))
 
     // Close context and execute the pipeline
     val result = sc.run()

scio-examples/src/main/scala/com/spotify/scio/examples/complete/game/HourlyTeamScore.scala
@@ -92,7 +92,7 @@ object HourlyTeamScore {
           TeamScoreSums(team, score, start)
       }
       // Save to the BigQuery table defined by "output" in the arguments passed in
-      .saveAsTypedBigQuery(args("output"))
+      .saveAsTypedBigQueryTable(Table.Spec(args("output")))
 
     // Close context and execute the pipeline
     sc.run()

commit 57e769338c70bd9297998fcf7c5d34ccf4a4a67d

Fix bug introduced in #2412

scio-bigquery/src/main/scala/com/spotify/scio/bigquery/syntax/ScioContextSyntax.scala
@@ -179,16 +179,20 @@ final class ScioContextOps(private val self: ScioContext) extends AnyVal {
   }
 
   def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](): SCollection[T] =
-    self.read(BigQueryTyped.dynamic[T](None))
+    typedBigQuery[T](None)
 
   def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](
     newSource: Source
+  ): SCollection[T] = typedBigQuery(Option(newSource))
+
+  private def typedBigQuery[T <: HasAnnotation: ClassTag: TypeTag: Coder](
+    newSource: Option[Source]
   ): SCollection[T] = {
     val bqt = BigQueryType[T]
     if (bqt.isStorage) {
-      typedBigQueryStorage(newSource)
+      typedBigQueryStorage(newSource.orNull)
     } else {
-      self.read(BigQueryTyped.dynamic[T](Option(newSource)))
+      self.read(BigQueryTyped.dynamic[T](newSource))
     }
   }
 