4201013fe24407d8fb3f0b0c480cdee58c0664f0
Claire McGinty <claire.d.mcginty@gmail.com>
2019-11-25 15:11:42 -0500

    forklift SMB implementation into scio-smb module (#2416)
    * forklift SMB into scio
    
    * fix copyright headers
    
    * initialize FS in ScioContext constructor
    
    * Remove redundant AvroGeneratedUser schema, pull from Beam instead
    
    * Rebase and fix build.sbt

---
+++ scio-smb/src/main/java/org/apache/beam/sdk/extensions/smb/FileOperations.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2019 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.beam.sdk.extensions.smb;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.io.Compression;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.FileIO.ReadableFile;
+import org.apache.beam.sdk.io.FileSystems;
+import org.apache.beam.sdk.io.ReadableFileUtil;
+import org.apache.beam.sdk.io.fs.MatchResult.Metadata;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.sdk.transforms.display.DisplayData;
+import org.apache.beam.sdk.transforms.display.DisplayData.Builder;
+import org.apache.beam.sdk.transforms.display.HasDisplayData;
+
+/**
+ * Abstracts IO operations for file-based formats.
+ *
+ * <p>Since the SMB algorithm doesn't support {@link org.apache.beam.sdk.io.Source} splitting, I/O
+ * operations must be abstracted at a per-record granularity. {@link Reader} and {@link Writer} must
+ * be {@link Serializable} to be used in {@link SortedBucketSource} and {@link SortedBucketSink}
+ * transforms.
+ */
+public abstract class FileOperations<V> implements Serializable, HasDisplayData {
+
+  private final Compression compression;
+  private final String mimeType;
+
+  protected FileOperations(Compression compression, String mimeType) {
+    this.compression = compression;
+    this.mimeType = mimeType;
+  }
+
+  protected abstract Reader<V> createReader();
+
+  // Delegate to FileIO.Sink<V> for writer logic
+  protected abstract FileIO.Sink<V> createSink();
+
+  public abstract Coder<V> getCoder();
+
+  public final Iterator<V> iterator(ResourceId resourceId) throws IOException {
+    final ReadableFile readableFile = toReadableFile(resourceId);
+
+    final Reader<V> reader = createReader();
+    reader.prepareRead(readableFile.open());
+    return reader.iterator();
+  }
+
+  public Writer<V> createWriter(ResourceId resourceId) throws IOException {
+    final Writer<V> writer = new Writer<>(createSink(), compression);
+    writer.prepareWrite(FileSystems.create(resourceId, mimeType));
+    return writer;
+  }
+
+  @Override
+  public void populateDisplayData(Builder builder) {
+    builder.add(DisplayData.item("FileOperations", getClass()));
+    builder.add(DisplayData.item("compression", compression.toString()));
+    builder.add(DisplayData.item("mimeType", mimeType));
+  }
+
+  /** Per-element file reader. */
+  public abstract static class Reader<V> implements Serializable {
+    public abstract void prepareRead(ReadableByteChannel channel) throws IOException;
+
+    /** Reads next record in the collection. */
+    abstract V readNext() throws IOException, NoSuchElementException;
+
+    abstract boolean hasNextElement() throws IOException;
+
+    public abstract void finishRead() throws IOException;
+
+    Iterator<V> iterator() {
+      return new Iterator<V>() {
+
+        @Override
+        public boolean hasNext() {
+          try {
+            boolean hasNext = hasNextElement();
+
+            if (!hasNext) {
+              finishRead();
+            }
+
+            return hasNext;
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        }
+
+        @Override
+        public V next() {
+          try {
+            return readNext();
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        }
+      };
+    }
+  }
+
+  /** Per-element file writer. */
+  public static class Writer<V> implements Serializable, AutoCloseable {
+
+    private final FileIO.Sink<V> sink;
+    private transient WritableByteChannel channel;
+    private Compression compression;
+
+    Writer(FileIO.Sink<V> sink, Compression compression) {
+      this.sink = sink;
+      this.compression = compression;
+    }
+
+    private void prepareWrite(WritableByteChannel channel) throws IOException {
+      this.channel = compression.writeCompressed(channel);
+      sink.open(this.channel);
+    }
+
+    public void write(V value) throws IOException {
+      sink.write(value);
+    }
+
+    @Override
+    public void close() throws IOException {
+      try {
+        sink.flush();
+      } catch (IOException e) {
+        // always close channel
+        channel.close();
+        throw e;
+      }
+      channel.close();
+    }
+  }
+
+  private ReadableFile toReadableFile(ResourceId resourceId) {
+    try {
+      final Metadata metadata = FileSystems.matchSingleFileSpec(resourceId.toString());
+
+      return ReadableFileUtil.newReadableFile(
+          metadata,
+          compression == Compression.AUTO
+              ? Compression.detect(resourceId.getFilename())
+              : compression);
+    } catch (IOException e) {
+      throw new RuntimeException(
+          String.format("Exception fetching metadata for %s", resourceId), e);
+    }
+  }
+}
---
+++ scio-smb/src/test/java/org/apache/beam/sdk/extensions/smb/AvroFileOperationsTest.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2019 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.beam.sdk.extensions.smb;
+
+import static org.apache.beam.sdk.extensions.smb.TestUtils.fromFolder;
+import static org.apache.beam.sdk.transforms.display.DisplayDataMatchers.hasDisplayItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.apache.avro.Schema;
+import org.apache.avro.file.CodecFactory;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.beam.sdk.io.AvroGeneratedUser;
+import org.apache.beam.sdk.io.Compression;
+import org.apache.beam.sdk.io.fs.ResolveOptions.StandardResolveOptions;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.sdk.transforms.display.DisplayData;
+import org.apache.beam.sdk.util.MimeTypes;
+import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.Lists;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/** Unit tests for {@link AvroFileOperations}. */
+public class AvroFileOperationsTest {
+  @Rule public final TemporaryFolder output = new TemporaryFolder();
+
+  private static final Schema USER_SCHEMA =
+      Schema.createRecord(
+          "User",
+          "",
+          "org.apache.beam.sdk.extensions.smb.avro",
+          false,
+          Lists.newArrayList(
+              new Schema.Field("name", Schema.create(Schema.Type.STRING), "", ""),
+              new Schema.Field("age", Schema.create(Schema.Type.INT), "", 0)));
+
+  @Test
+  public void testGenericRecord() throws Exception {
+    final AvroFileOperations<GenericRecord> fileOperations = AvroFileOperations.of(USER_SCHEMA);
+    final ResourceId file =
+        fromFolder(output).resolve("file.avro", StandardResolveOptions.RESOLVE_FILE);
+
+    final List<GenericRecord> records =
+        IntStream.range(0, 10)
+            .mapToObj(
+                i ->
+                    new GenericRecordBuilder(USER_SCHEMA)
+                        .set("name", String.format("user%02d", i))
+                        .set("age", i)
+                        .build())
+            .collect(Collectors.toList());
+    final FileOperations.Writer<GenericRecord> writer = fileOperations.createWriter(file);
+    for (GenericRecord record : records) {
+      writer.write(record);
+    }
+    writer.close();
+
+    final List<GenericRecord> actual = new ArrayList<>();
+    fileOperations.iterator(file).forEachRemaining(actual::add);
+
+    Assert.assertEquals(records, actual);
+  }
+
+  @Test
+  public void testSpecificRecord() throws Exception {
+    final AvroFileOperations<AvroGeneratedUser> fileOperations =
+        AvroFileOperations.of(AvroGeneratedUser.class);
+    final ResourceId file =
+        fromFolder(output).resolve("file.avro", StandardResolveOptions.RESOLVE_FILE);
+
+    final List<AvroGeneratedUser> records =
+        IntStream.range(0, 10)
+            .mapToObj(
+                i ->
+                    AvroGeneratedUser.newBuilder()
+                        .setName(String.format("user%02d", i))
+                        .setFavoriteColor(String.format("color%02d", i))
+                        .setFavoriteNumber(i)
+                        .build())
+            .collect(Collectors.toList());
+    final FileOperations.Writer<AvroGeneratedUser> writer = fileOperations.createWriter(file);
+    for (AvroGeneratedUser record : records) {
+      writer.write(record);
+    }
+    writer.close();
+
+    final List<AvroGeneratedUser> actual = new ArrayList<>();
+    fileOperations.iterator(file).forEachRemaining(actual::add);
+
+    Assert.assertEquals(records, actual);
+  }
+
+  @Test
+  public void testDisplayData() {
+    final AvroFileOperations<AvroGeneratedUser> fileOperations =
+        AvroFileOperations.of(AvroGeneratedUser.class);
+
+    final DisplayData displayData = DisplayData.from(fileOperations);
+    MatcherAssert.assertThat(
+        displayData, hasDisplayItem("FileOperations", AvroFileOperations.class));
+    MatcherAssert.assertThat(displayData, hasDisplayItem("mimeType", MimeTypes.BINARY));
+    MatcherAssert.assertThat(
+        displayData, hasDisplayItem("compression", Compression.UNCOMPRESSED.toString()));
+    MatcherAssert.assertThat(
+        displayData, hasDisplayItem("codecFactory", CodecFactory.snappyCodec().getClass()));
+    MatcherAssert.assertThat(
+        displayData, hasDisplayItem("schema", AvroGeneratedUser.SCHEMA$.getFullName()));
+  }
+}
