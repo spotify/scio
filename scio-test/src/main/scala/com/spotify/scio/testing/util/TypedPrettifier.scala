df99d4bd606a3d1201158253a9e1de8993766eff
Anish <anish749@gmail.com>
2019-12-02 19:38:34 -0500

    fix tests + address review comments

--- scio-test/src/main/scala/com/spotify/scio/testing/util/TypedPrettifier.scala
+++ scio-test/src/main/scala/com/spotify/scio/testing/util/TypedPrettifier.scala
@@ -1,21 +1,41 @@
 package com.spotify.scio.testing.util
 import com.spotify.scio.schemas.Schema
 import org.scalactic.Prettifier
-import shapeless.Refute
 
+/**
+ * A wrapper over Scalatic's [[Prettifier]] which allows us to override the behavior of
+ * a specific type.
+ *
+ * We use this to pull in the [[Schema]] of the type [[T]] so that the Schema can be used
+ * to create a better representation of an `SCollection[T]` in the error messages.
+ */
 trait TypedPrettifier[T] extends Serializable {
   def apply: Prettifier
 }
 
-object TypedPrettifier {
+/**
+ * A low priority fall back [[TypedPrettifier]] which delegates Scalactic's Prettifier.
+ */
+trait LowPriorityFallbackTypedPrettifier {
+
+  /**
+   * Visible when we fail to have a [[Schema]]
+   */
   implicit def noSchemaPrettifier[T](
-    implicit scalactic: Prettifier,
-    refute: Refute[Schema[T]]
+    implicit scalactic: Prettifier
   ): TypedPrettifier[T] =
     new TypedPrettifier[T] {
       override def apply: Prettifier = scalactic
     }
+}
 
+object TypedPrettifier extends LowPriorityFallbackTypedPrettifier {
+
+  /**
+   * An instance of [[TypedPrettifier]] when we have a [[Schema]] available
+   * for our type. We use the Schema to create a table representation of
+   * the SCollection[T]
+   */
   implicit def schemaPrettifier[T: Schema](
     implicit schema: Schema[T],
     scalactic: Prettifier
--- scio-test/src/test/scala/com/spotify/scio/testing/util/SCollectionPrettifierTest.scala
+++ scio-test/src/test/scala/com/spotify/scio/testing/util/SCollectionPrettifierTest.scala
@@ -2,6 +2,7 @@ package com.spotify.scio.testing.util
 import org.scalactic.Prettifier
 import org.scalatest.{FlatSpec, Matchers}
 
+// A class which Scio's Schema derivation will use fallback
 class NoSchemaAvailable() extends Serializable {
   override def toString: String = "ClassWithNoSchemaAvailable"
 }
@@ -23,7 +24,7 @@ class SCollectionPrettifierTest extends FlatSpec with Matchers {
       |├──────────────────────────────┼──────────────────────────────┤
       |│1                             │Row:[one]                     │
       |└──────────────────────────────┴──────────────────────────────┘
-    """.stripMargin
+      |""".stripMargin
 
     prettyString should be(expected)
 
@@ -33,20 +34,28 @@ class SCollectionPrettifierTest extends FlatSpec with Matchers {
     val prettyString =
       implicitly[TypedPrettifier[NoSchemaAvailable]].apply(Seq(new NoSchemaAvailable()))
 
-    val expected = "ClassWithNoSchemaAvailable"
+    val expected = "List(ClassWithNoSchemaAvailable)"
 
     prettyString should be(expected)
   }
 
   it should "use fallback to implicit prettifier available in scope" in {
     implicit val testPrettifier: Prettifier = new Prettifier {
-      override def apply(o: Any): String = "implicit user defined prettifier"
+      override def apply(o: Any): String = "user - defined"
     }
 
     val prettyString =
       implicitly[TypedPrettifier[Something]].apply(Seq(Something(1, NestedRecord("one"))))
 
-    val expected = "implicit user defined prettifier"
+    val expected =
+      """
+        |┌──────────────────────────────┬──────────────────────────────┐
+        |│a                             │b                             │
+        |├──────────────────────────────┼──────────────────────────────┤
+        |│user - defined                │user - defined                │
+        |└──────────────────────────────┴──────────────────────────────┘
+        |""".stripMargin
+
     prettyString should be(expected)
   }
 }
