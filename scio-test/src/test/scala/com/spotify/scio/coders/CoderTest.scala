d971b6b9a224618bd0b1b660f3d8acc86104ce59
Filipe Regadas <filiperegadas@gmail.com>
2020-03-04 16:07:05 -0500

    Fix format

--- scio-test/src/test/scala/com/spotify/scio/coders/CoderTest.scala
+++ scio-test/src/test/scala/com/spotify/scio/coders/CoderTest.scala
@@ -32,7 +32,7 @@ import scala.collection.JavaConverters._
 import scala.collection.{mutable => mut}
 import java.io.ByteArrayInputStream
 import org.apache.beam.sdk.testing.CoderProperties
-import com.google.api.services.bigquery.model.{TableSchema, TableFieldSchema}
+import com.google.api.services.bigquery.model.{TableFieldSchema, TableSchema}
 
 final case class UserId(bytes: Seq[Byte])
 final case class User(id: UserId, username: String, email: String)
