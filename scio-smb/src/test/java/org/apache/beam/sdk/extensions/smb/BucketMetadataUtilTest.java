aab827ca32518361cf1ed913486879450f8550fd
Neville Li <neville.lyh@gmail.com>
2020-03-25 13:03:52 -0400

    improve test

--- scio-smb/src/test/java/org/apache/beam/sdk/extensions/smb/BucketMetadataUtilTest.java
+++ scio-smb/src/test/java/org/apache/beam/sdk/extensions/smb/BucketMetadataUtilTest.java
@@ -17,6 +17,8 @@
 
 package org.apache.beam.sdk.extensions.smb;
 
+import org.apache.beam.sdk.coders.CannotProvideCoderException;
+import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.extensions.smb.BucketMetadataUtil.SourceMetadata;
 import org.apache.beam.sdk.io.FileSystems;
 import org.apache.beam.sdk.io.LocalResources;
@@ -32,6 +34,9 @@ import java.nio.channels.Channels;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /** Unit tests for {@link BucketMetadataUtil}. */
 public class BucketMetadataUtilTest {
@@ -41,70 +46,116 @@ public class BucketMetadataUtilTest {
 
   @Test
   public void testIncompatibleMetadata() throws Exception {
+    final List<TestBucketMetadata> metadataList1 = IntStream.range(0, 10).mapToObj(i ->
+            {
+              try {
+                return TestBucketMetadata.of((int) Math.pow(2.0, 1.0 * i), 1).withKeyIndex(i != 9 ? 0 : 1);
+              } catch (CannotProvideCoderException | Coder.NonDeterministicException e) {
+                throw new RuntimeException(e);
+              }
+            }
+    ).collect(Collectors.toList());
+
+    testIncompatibleMetadata(metadataList1, 0, 9);
+
+    Collections.reverse(metadataList1);
+    testIncompatibleMetadata(metadataList1, 9, 0);
+
+    final List<TestBucketMetadata> metadataList2 = IntStream.range(0, 10).mapToObj(i ->
+            {
+              try {
+                return TestBucketMetadata.of((int) Math.pow(2.0, 1.0 * i), 1).withKeyIndex(i != 4 ? 0 : 1);
+              } catch (CannotProvideCoderException | Coder.NonDeterministicException e) {
+                throw new RuntimeException(e);
+              }
+            }
+    ).collect(Collectors.toList());
+
+    testIncompatibleMetadata(metadataList2, 0, 4);
+
+    Collections.reverse(metadataList2);
+    testIncompatibleMetadata(metadataList2, 9, 5);
+  }
+
+  private void testIncompatibleMetadata(List<TestBucketMetadata> metadataList, int canonicalIdx, int badIdx) throws Exception {
     final List<ResourceId> directories = new ArrayList<>();
-    final List<TestBucketMetadata> metadataList = new ArrayList<>();
+    final List<ResourceId> goodDirectories = new ArrayList<>();
 
-    // first 9 elements are source-compatible, last is not
-    for (int i = 0; i < 10; i++) {
+    // all but one metadata are source-compatible, the one at badIdx is incompatible
+    for (int i = 0; i < metadataList.size(); i++) {
       final File dest = folder.newFolder(String.valueOf(i));
-      final TestBucketMetadata metadata =
-          TestBucketMetadata.of((int) Math.pow(2.0, 1.0 * i), 1).withKeyIndex(i < 9 ? 0 : 1);
       final OutputStream outputStream =
-          Channels.newOutputStream(
-              FileSystems.create(
-                  LocalResources.fromFile(folder.newFile(i + "/metadata.json"), false),
-                  "application/json"));
+              Channels.newOutputStream(
+                      FileSystems.create(
+                              LocalResources.fromFile(folder.newFile(i + "/metadata.json"), false),
+                              "application/json"));
 
-      BucketMetadata.to(metadata, outputStream);
-      directories.add(LocalResources.fromFile(dest, true));
-      metadataList.add(metadata);
+      BucketMetadata.to(metadataList.get(i), outputStream);
+      ResourceId dir = LocalResources.fromFile(dest, true);
+      directories.add(dir);
+      if (i != badIdx) {
+        goodDirectories.add(dir);
+      }
     }
 
-    final TestBucketMetadata canonicalMetadata = metadataList.get(0);
+    final TestBucketMetadata canonicalMetadata = metadataList.get(canonicalIdx);
 
     final SourceMetadata<String, String> sourceMetadata =
-        util.getSourceMetadata(directories.subList(0, 9), ".txt");
+            util.getSourceMetadata(goodDirectories, ".txt");
     Assert.assertEquals(canonicalMetadata, sourceMetadata.getCanonicalMetadata());
-    Assert.assertEquals(9, sourceMetadata.getPartitionMetadata().size());
+    Assert.assertEquals(goodDirectories.size(), sourceMetadata.getPartitionMetadata().size());
 
     Assert.assertThrows(IllegalStateException.class, () -> util.getSourceMetadata(directories, ".txt"));
 
-    Collections.reverse(directories);
-    Assert.assertThrows(IllegalStateException.class, () -> util.getSourceMetadata(directories, ".txt"));
-
-    Collections.shuffle(directories);
-    Assert.assertThrows(IllegalStateException.class, () -> util.getSourceMetadata(directories, ".txt"));
+    folder.delete();
   }
 
   @Test
   public void testMissingMetadata() throws Exception {
+    final List<Optional<TestBucketMetadata>> metadataList = IntStream.range(0, 10).mapToObj(i ->
+            {
+              if (i == 9) {
+                return Optional.<TestBucketMetadata>empty();
+              } else {
+                try {
+                  return  Optional.of(TestBucketMetadata.of((int) Math.pow(2.0, 1.0 * i), 1));
+                } catch (CannotProvideCoderException | Coder.NonDeterministicException e) {
+                  throw new RuntimeException(e);
+                }
+              }
+            }
+    ).collect(Collectors.toList());
+
+    testMissingMetadata(metadataList);
+
+    Collections.reverse(metadataList);
+    testMissingMetadata(metadataList);
+  }
+
+  private void testMissingMetadata(List<Optional<TestBucketMetadata>> metadataList) throws Exception {
     final List<ResourceId> directories = new ArrayList<>();
 
-    // first 9 elements are source-compatible, last is missing metadata.json
-    for (int i = 0; i < 10; i++) {
+    // all but one metadata are compatible
+    for (int i = 0; i < metadataList.size(); i++) {
       final File dest = folder.newFolder(String.valueOf(i));
       directories.add(LocalResources.fromFile(dest, true));
-      if (i == 9) {
-        break;
+
+      if (!metadataList.get(i).isPresent()) {
+        continue;
       }
 
-      final TestBucketMetadata metadata =
-          TestBucketMetadata.of((int) Math.pow(2.0, 1.0 * i), 1);
+      final TestBucketMetadata metadata = metadataList.get(i).get();
       final OutputStream outputStream =
-          Channels.newOutputStream(
-              FileSystems.create(
-                  LocalResources.fromFile(folder.newFile(i + "/metadata.json"), false),
-                  "application/json"));
+              Channels.newOutputStream(
+                      FileSystems.create(
+                              LocalResources.fromFile(folder.newFile(i + "/metadata.json"), false),
+                              "application/json"));
 
       BucketMetadata.to(metadata, outputStream);
     }
 
     Assert.assertFalse(util.getSourceMetadata(directories, ".txt").supportsSmb());
 
-    Collections.reverse(directories);
-    Assert.assertFalse(util.getSourceMetadata(directories, ".txt").supportsSmb());
-
-    Collections.shuffle(directories);
-    Assert.assertFalse(util.getSourceMetadata(directories, ".txt").supportsSmb());
+    folder.delete();
   }
 }
