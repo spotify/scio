ec2410793b98541b3b8acac2d0ef768588a5673c
Filipe Regadas <filiperegadas@gmail.com>
2020-05-06 16:27:17 -0400

    Keep backwards compatibility

--- scio-core/src/main/scala/com/spotify/scio/util/FunctionsWithSideInput.scala
+++ scio-core/src/main/scala/com/spotify/scio/util/FunctionsWithSideInput.scala
@@ -20,21 +20,22 @@ package com.spotify.scio.util
 import com.spotify.scio.values.SideInputContext
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow
 import com.twitter.chill.ClosureCleaner
 
 private[scio] object FunctionsWithSideInput {
   trait SideInputDoFn[T, U] extends NamedDoFn[T, U] {
-    def sideInputContext(c: DoFn[T, U]#ProcessContext): SideInputContext[T] =
+    def sideInputContext(c: DoFn[T, U]#ProcessContext, w: BoundedWindow): SideInputContext[T] =
       // Workaround for type inference limit
-      new SideInputContext(c.asInstanceOf[DoFn[T, AnyRef]#ProcessContext])
+      new SideInputContext(c.asInstanceOf[DoFn[T, AnyRef]#ProcessContext], w)
   }
 
   def filterFn[T](f: (T, SideInputContext[T]) => Boolean): DoFn[T, T] =
     new SideInputDoFn[T, T] {
       val g = ClosureCleaner.clean(f) // defeat closure
       @ProcessElement
-      private[scio] def processElement(c: DoFn[T, T]#ProcessContext): Unit =
-        if (g(c.element(), sideInputContext(c))) {
+      private[scio] def processElement(c: DoFn[T, T]#ProcessContext, w: BoundedWindow): Unit =
+        if (g(c.element(), sideInputContext(c, w))) {
           c.output(c.element())
         }
     }
@@ -43,8 +44,8 @@ private[scio] object FunctionsWithSideInput {
     new SideInputDoFn[T, U] {
       val g = ClosureCleaner.clean(f) // defeat closure
       @ProcessElement
-      private[scio] def processElement(c: DoFn[T, U]#ProcessContext): Unit = {
-        val i = g(c.element(), sideInputContext(c)).toIterator
+      private[scio] def processElement(c: DoFn[T, U]#ProcessContext, w: BoundedWindow): Unit = {
+        val i = g(c.element(), sideInputContext(c, w)).toIterator
         while (i.hasNext) c.output(i.next())
       }
     }
@@ -53,7 +54,7 @@ private[scio] object FunctionsWithSideInput {
     new SideInputDoFn[T, U] {
       val g = ClosureCleaner.clean(f) // defeat closure
       @ProcessElement
-      private[scio] def processElement(c: DoFn[T, U]#ProcessContext): Unit =
-        c.output(g(c.element(), sideInputContext(c)))
+      private[scio] def processElement(c: DoFn[T, U]#ProcessContext, w: BoundedWindow): Unit =
+        c.output(g(c.element(), sideInputContext(c, w)))
     }
 }
--- scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SideInput.scala
@@ -17,8 +17,17 @@
 
 package com.spotify.scio.values
 
+import java.lang.{Iterable => JIterable}
+import java.util.{List => JList, Map => JMap}
+
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.values.PCollectionView
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow
+import org.apache.beam.sdk.transforms.windowing.GlobalWindow
+
+import com.spotify.scio.util.JMapWrapper
+
+import scala.collection.JavaConverters._
 
 /** Encapsulate an SCollection when it is being used as a side input. */
 trait SideInput[T] extends Serializable {
@@ -27,6 +36,10 @@ trait SideInput[T] extends Serializable {
   private[values] def view: PCollectionView[ViewType]
 
   private[values] def get[I, O](context: DoFn[I, O]#ProcessContext): T
+
+  @deprecated("will be removed; override get instead")
+  private[values] def getCache[I, O](context: DoFn[I, O]#ProcessContext, window: BoundedWindow): T =
+    get(context)
 
   /**
    * Create a new [[SideInput]] by applying a function on the elements wrapped in this SideInput.
@@ -43,6 +56,111 @@ object SideInput {
     override private[values] def get[I, O](context: DoFn[I, O]#ProcessContext): T =
       context.sideInput(view)
   }
+
+  /**
+   * Wrap a view of a singleton as a [[SideInput]]. In most cases you want to use
+   * [[SCollection.asSingletonSideInput(*]].
+   *
+   * @deprecated use SCollection.asSingletonSideInput or SideInput(View.asSingleton)
+   */
+  @deprecated("use SCollection.asSingletonSideInput or SideInput(View.asSingleton)", "0.9.1")
+  def wrapSingleton[T](view: PCollectionView[T]): SideInput[T] =
+    new SingletonSideInput[T](view)
+
+  /**
+   * Wrap a view of a [[java.util.List]] as a [[SideInput]]. In most cases you want to use
+   * [[SCollection.asListSideInput]].
+   *
+   * @deprecated use SCollection.asListSideInput or SideInput(com.spotify.scio.values.View.asScalaList)
+   */
+  @deprecated(
+    "use SCollection.asListSideInput or SideInput(com.spotify.scio.values.View.asScalaList)",
+    "0.9.1"
+  )
+  def wrapList[T](view: PCollectionView[JList[T]]): SideInput[Seq[T]] =
+    new ListSideInput[T](view)
+
+  /**
+   * Wrap a view of a [[java.lang.Iterable]] as a [[SideInput]]. In most cases you want to use
+   * [[SCollection.asIterableSideInput]].
+   *
+   * @deprecated use SCollection.asIterableSideInput or SideInput(com.spotify.scio.values.View.asScalaIterable)
+   */
+  @deprecated(
+    "use SCollection.asIterableSideInput or SideInput(com.spotify.scio.values.View.asScalaIterable)",
+    "0.9.1"
+  )
+  def wrapIterable[T](view: PCollectionView[JIterable[T]]): SideInput[Iterable[T]] =
+    new IterableSideInput[T](view)
+
+  /**
+   * Wrap a view of a [[java.util.Map]] as a [[SideInput]]. In most cases you want to use
+   * [[PairSCollectionFunctions.asMapSideInput]].
+   *
+   * @deprecated use SCollection.asMapSideInput or SideInput(com.spotify.scio.values.View.asScalaMap)
+   */
+  @deprecated(
+    "use SCollection.asMapSideInput or SideInput(com.spotify.scio.values.View.asScalaMap)",
+    "0.9.1"
+  )
+  def wrapMap[K, V](view: PCollectionView[JMap[K, V]]): SideInput[Map[K, V]] =
+    new MapSideInput[K, V](view)
+
+  /**
+   * Wrap a view of a multi-map as a [[SideInput]]. In most cases you want to use
+   * [[PairSCollectionFunctions.asMultiMapSideInput]].
+   *
+   * @deprecated use SCollection.asMultiMapSideInput or SideInput(com.spotify.scio.values.View.asScalaMultimap)
+   */
+  @deprecated(
+    "use SCollection.asMultiMapSideInput or SideInput(com.spotify.scio.values.View.asScalaMultimap)",
+    "0.9.1"
+  )
+  def wrapMultiMap[K, V](
+    view: PCollectionView[JMap[K, JIterable[V]]]
+  ): SideInput[Map[K, Iterable[V]]] =
+    new MultiMapSideInput[K, V](view)
+}
+
+private[values] class SingletonSideInput[T](val view: PCollectionView[T])
+    extends CachedSideInput[T] {
+  override type ViewType = T
+
+  override def get[I, O](context: DoFn[I, O]#ProcessContext): T =
+    context.sideInput(view)
+}
+
+private[values] class ListSideInput[T](val view: PCollectionView[JList[T]])
+    extends CachedSideInput[Seq[T]] {
+  override type ViewType = JList[T]
+
+  override def get[I, O](context: DoFn[I, O]#ProcessContext): Seq[T] =
+    context.sideInput(view).iterator().asScala.toSeq
+}
+
+private[values] class IterableSideInput[T](val view: PCollectionView[JIterable[T]])
+    extends CachedSideInput[Iterable[T]] {
+  override type ViewType = JIterable[T]
+
+  override def get[I, O](context: DoFn[I, O]#ProcessContext): Iterable[T] =
+    context.sideInput(view).asScala
+}
+
+private[values] class MapSideInput[K, V](val view: PCollectionView[JMap[K, V]])
+    extends CachedSideInput[Map[K, V]] {
+  override type ViewType = JMap[K, V]
+
+  override def get[I, O](context: DoFn[I, O]#ProcessContext): Map[K, V] =
+    JMapWrapper.of(context.sideInput(view))
+}
+
+private[values] class MultiMapSideInput[K, V](val view: PCollectionView[JMap[K, JIterable[V]]])
+    extends CachedSideInput[Map[K, Iterable[V]]] {
+  override type ViewType = JMap[K, JIterable[V]]
+
+  override def get[I, O](context: DoFn[I, O]#ProcessContext): Map[K, Iterable[V]] =
+    JMapWrapper.ofMultiMap(context.sideInput(view))
+
 }
 
 private[values] class DelegatingSideInput[A, B](val si: SideInput[A], val mapper: A => B)
@@ -55,9 +173,32 @@ private[values] class DelegatingSideInput[A, B](val si: SideInput[A], val mapper
   override private[values] val view: PCollectionView[ViewType] = si.view
 }
 
+@deprecated("will be removed; you should use SideInput")
+private[values] trait CachedSideInput[T] extends SideInput[T] {
+  private var cache: T = _
+  @transient private var window: BoundedWindow = _
+
+  // Use this attribute in implementations of SideInput to force caching
+  // even on GlobalWindows. (Used to fix #1269)
+  protected def updateCacheOnGlobalWindow = true
+
+  override def getCache[I, O](context: DoFn[I, O]#ProcessContext, window: BoundedWindow): T = {
+    if (cache == null || this.window != window ||
+        (updateCacheOnGlobalWindow && window == GlobalWindow.INSTANCE)) {
+      this.window = window
+      cache = get(context)
+    }
+    cache
+  }
+
+}
+
 /** Encapsulate context of one or more [[SideInput]]s in an [[SCollectionWithSideInput]]. */
-class SideInputContext[T] private[scio] (val context: DoFn[T, AnyRef]#ProcessContext) {
+class SideInputContext[T] private[scio] (
+  val context: DoFn[T, AnyRef]#ProcessContext,
+  val window: BoundedWindow
+) {
 
   /** Extract the value of a given [[SideInput]]. */
-  def apply[S](side: SideInput[S]): S = side.get(context)
+  def apply[S](side: SideInput[S]): S = side.getCache(context, window)
 }
