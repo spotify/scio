e2bd5f05f384ad590193ddf520aafa5edf3bdc8a
Filipe Regadas <filiperegadas@gmail.com>
2020-06-24 17:26:49 +0100

    Update socco to socco-ng 0.1.1

--- scripts/make-site.sh
+++ scripts/make-site.sh
@@ -6,18 +6,14 @@ set -o pipefail
 
 # generate new site locally
 # /site/target/site/index.html
-# using scala 2.12.8 for annotated examples due to https://github.com/criteo/socco/pull/6
-SOCCO=true sbt ++2.12.8 scio-examples/clean scio-examples/compile
-# using latest scala for scaladoc due to
-# https://github.com/scala/bug/issues/11635
-sbt ++2.12.11 site/makeSite
+SOCCO=true sbt scio-examples/compile site/makeSite
 
 # push to GitHub
 while true; do
     read -r -p "Push site to GitHub? (y/n)" yn
     case $yn in
     [Yy]*)
-        sbt ++2.12.11 site/ghpagesPushSite
+        sbt site/ghpagesPushSite
         break
         ;;
     [Nn]*) exit ;;
