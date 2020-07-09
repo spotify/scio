136bdc4fc7674e2418ed46076a70faf9e1f91f6c
Filipe Regadas <filiperegadas@gmail.com>
2020-07-09 10:08:01 +0100

    Add Elasticsearch 5,6,7 add/update alias on multiple indices ops (#3134)

--- scio-elasticsearch/es7/src/main/scala/com/spotify/scio/elasticsearch/IndexAdmin.scala
+++ scio-elasticsearch/es7/src/main/scala/com/spotify/scio/elasticsearch/IndexAdmin.scala
@@ -17,24 +17,23 @@
 
 package com.spotify.scio.elasticsearch
 
-import org.apache.http.HttpHost
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest.AliasActions
-import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
 import org.elasticsearch.action.support.master.AcknowledgedResponse
 import org.elasticsearch.client._
 import org.elasticsearch.client.indices.{CreateIndexRequest, CreateIndexResponse}
 import org.elasticsearch.common.unit.TimeValue
 import org.elasticsearch.common.xcontent.XContentType
+
 import org.slf4j.LoggerFactory
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.util.Try
 
 object IndexAdmin {
-
-  private val Logger = LoggerFactory.getLogger(this.getClass)
+  private[this] val Logger = LoggerFactory.getLogger(getClass)
 
   private def indicesClient[A](esOptions: ElasticsearchOptions)(f: IndicesClient => A): Try[A] = {
     val client = new RestHighLevelClient(RestClient.builder(esOptions.nodes: _*))
@@ -43,36 +42,6 @@ object IndexAdmin {
     client.close()
     result
   }
-
-  /**
-   * Ensure that index is created.
-   * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
-   *
-   * @param index index to be created
-   * @param mappingSource a valid json string
-   */
-  def ensureIndex(
-    nodes: Iterable[HttpHost],
-    index: String,
-    mappingSource: String
-  ): Try[CreateIndexResponse] = {
-    val esOptions = ElasticsearchOptions(nodes.toSeq)
-    ensureIndex(esOptions, index, mappingSource)
-  }
-
-  /**
-   * Ensure that index is created.
-   * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
-   *
-   * @param index index to be created
-   * @param mappingSource a valid json string
-   */
-  def ensureIndex(
-    esOptions: ElasticsearchOptions,
-    index: String,
-    mappingSource: String
-  ): Try[CreateIndexResponse] =
-    indicesClient(esOptions)(client => ensureIndex(index, mappingSource, client))
 
   /**
    * Ensure that index is created.
@@ -91,32 +60,18 @@ object IndexAdmin {
     )
 
   /**
-   * Delete index
-   * @param index to be deleted
-   * @param timeout defaults to 1 minute
-   * @return Failure or unacknowledged response if operation did not succeed
+   * Ensure that index is created.
+   * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
+   *
+   * @param index index to be created
+   * @param mappingSource a valid json string
    */
-  def removeIndex(
-    nodes: Iterable[HttpHost],
-    index: String,
-    timeout: TimeValue = TimeValue.timeValueMinutes(1)
-  ): Try[AcknowledgedResponse] = {
-    val esOptions = ElasticsearchOptions(nodes.toSeq)
-    removeIndex(esOptions, index, timeout)
-  }
-
-  /**
-   * Delete index
-   * @param index to be deleted
-   * @param timeout defaults to 1 minute
-   * @return Failure or unacknowledged response if operation did not succeed
-   */
-  def removeIndex(
+  def ensureIndex(
     esOptions: ElasticsearchOptions,
     index: String,
-    timeout: TimeValue
-  ): Try[AcknowledgedResponse] =
-    indicesClient(esOptions)(client => removeIndex(esOptions, index, client, timeout))
+    mappingSource: String
+  ): Try[CreateIndexResponse] =
+    indicesClient(esOptions)(client => ensureIndex(index, mappingSource, client))
 
   /**
    * Delete index
@@ -125,34 +80,69 @@ object IndexAdmin {
    * @return Failure or unacknowledged response if operation did not succeed
    */
   private def removeIndex(
-    esOptions: ElasticsearchOptions,
-    index: String,
     client: IndicesClient,
+    index: String,
     timeout: TimeValue
-  ): AcknowledgedResponse = {
-    val request = new DeleteIndexRequest(index)
-      .timeout(timeout)
-
-    client.delete(request, RequestOptions.DEFAULT)
-  }
+  ): AcknowledgedResponse =
+    client.delete(new DeleteIndexRequest(index).timeout(timeout), RequestOptions.DEFAULT)
 
   /**
-   * Add index alias and remove the alias from all other indexes if it is already pointed to any.
-   * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
-   *
-   * @param alias        to be re-assigned
-   * @param indexName index to point the alias to
+   * Delete index
+   * @param index to be deleted
    * @param timeout defaults to 1 minute
+   * @return Failure or unacknowledged response if operation did not succeed
    */
-  def createOrUpdateAlias(
-    nodes: Iterable[HttpHost],
+  def removeIndex(
+    esOptions: ElasticsearchOptions,
+    index: String,
+    timeout: TimeValue
+  ): Try[AcknowledgedResponse] =
+    indicesClient(esOptions)(client => removeIndex(client, index, timeout))
+
+  /**
+   * Add or update index alias with an option to remove the alias from all other indexes if it is already
+   * pointed to any.
+   *
+   * @param alias            to be re-assigned
+   * @param indices          Iterable of pairs (index, isWriteIndex) to point the alias to.
+   *                         Note: only one index can be assigned as write index.
+   * @param removePrevious   When set to true, the indexAlias would be removed from all indices it
+   *                         was assigned to before adding new index alias assignment
+   */
+  private def createOrUpdateAlias(
+    client: IndicesClient,
+    indices: Iterable[(String, Boolean)],
     alias: String,
-    indexName: String,
     removePrevious: Boolean,
-    timeout: TimeValue = TimeValue.timeValueMinutes(1)
-  ): Try[AcknowledgedResponse] = {
-    val esOptions = ElasticsearchOptions(nodes.toSeq)
-    createOrUpdateAlias(esOptions, alias, indexName, removePrevious, timeout)
+    timeout: TimeValue
+  ): AcknowledgedResponse = {
+    require(
+      indices.find(_._2).size == 1,
+      "Only one index per alias can be assigned to be the write index at a time"
+    )
+
+    val request = indices.foldLeft(new IndicesAliasesRequest()) {
+      case (request, (idx, isWriteIndex)) =>
+        request.addAliasAction(
+          new AliasActions(AliasActions.Type.ADD)
+            .index(idx)
+            .writeIndex(isWriteIndex)
+            .alias(alias)
+        )
+    }
+
+    if (removePrevious) {
+      val getAliasesResponse = client.getAlias(new GetAliasesRequest(alias), RequestOptions.DEFAULT)
+      val indexAliacesToRemove = getAliasesResponse.getAliases.asScala.keys
+      Logger.info(s"Removing alias $alias from ${indexAliacesToRemove.mkString(", ")}")
+
+      indexAliacesToRemove.foreach { indexName =>
+        request.addAliasAction(
+          new AliasActions(AliasActions.Type.REMOVE).index(indexName).alias(alias)
+        )
+      }
+    }
+    client.updateAliases(request.timeout(timeout), RequestOptions.DEFAULT);
   }
 
   /**
@@ -161,61 +151,39 @@ object IndexAdmin {
    * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
    *
    * @param alias            to be re-assigned
-   * @param indexName        index to point the alias to
+   * @param index            index to point the alias to
    * @param removePrevious   When set to true, the indexAlias would be removed from all indices it
    *                         was assigned to before adding new index alias assignment.
    */
   def createOrUpdateAlias(
     esOptions: ElasticsearchOptions,
     alias: String,
-    indexName: String,
+    index: String,
     removePrevious: Boolean,
     timeout: TimeValue
   ): Try[AcknowledgedResponse] =
-    indicesClient(esOptions)(client =>
-      createOrUpdateAlias(esOptions, alias, indexName, removePrevious, client, timeout)
-    )
+    createOrUpdateAlias(esOptions, List((index, true)), alias, removePrevious, timeout)
 
   /**
-   * Add index alias and remove the alias from all other indexes if it is already pointed to any.
+   * Add or update index alias with an option to remove the alias from all other indexes if it is already
+   * pointed to any.
    * If index already exists or some other error occurs this results in a [[scala.util.Failure]].
    *
    * @param alias            to be re-assigned
-   * @param indexName        index to point the alias to
+   * @param indices          Iterable of pairs (index, isWriteIndex) to point the alias to.
+   *                         Note: only one index can be assigned as write index.
    * @param removePrevious   When set to true, the indexAlias would be removed from all indices it
    *                         was assigned to before adding new index alias assignment
    */
-  private def createOrUpdateAlias(
+  def createOrUpdateAlias(
     esOptions: ElasticsearchOptions,
+    indices: Iterable[(String, Boolean)],
     alias: String,
-    indexName: String,
     removePrevious: Boolean,
-    client: IndicesClient,
     timeout: TimeValue
-  ): AcknowledgedResponse = {
-
-    val getAliasesResponse =
-      client.getAlias(new GetAliasesRequest(alias), RequestOptions.DEFAULT)
-
-    val request = new IndicesAliasesRequest()
-      .addAliasAction(
-        new AliasActions(AliasActions.Type.ADD)
-          .index(indexName)
-          .alias(alias)
-      )
-
-    if (removePrevious) {
-      val indexAliacesToRemove = getAliasesResponse.getAliases.asScala.map(_._1)
-      Logger.info(s"Removing alias $alias from ${indexAliacesToRemove.mkString(", ")}")
-
-      indexAliacesToRemove.foreach(indexName =>
-        request.addAliasAction(
-          new AliasActions(AliasActions.Type.REMOVE)
-            .index(indexName)
-            .alias(alias)
-        )
-      )
+  ): Try[AcknowledgedResponse] =
+    indicesClient(esOptions) { client =>
+      createOrUpdateAlias(client, indices, alias, removePrevious, timeout)
     }
-    client.updateAliases(request, RequestOptions.DEFAULT);
-  }
+
 }
