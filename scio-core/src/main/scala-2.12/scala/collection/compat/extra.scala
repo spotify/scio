c6a5a76e21c99ce001c8c87d275ed71747466613
Filipe Regadas <filiperegadas@gmail.com>
2020-08-07 18:50:53 +0100

    Add some convinient scala collection converters

--- scio-core/src/main/scala-2.12/scala/collection/compat/extra.scala
+++
@@ -1,6 +0,0 @@
-package scala.collection.compat
-
-object extra {
-  type Wrappers = scala.collection.convert.Wrappers.type
-  val Wrappers = scala.collection.convert.Wrappers
-}
--- scio-core/src/main/scala-2.13/scala/collection/compat/extra.scala
+++ scio-core/src/main/scala-2.13/scala/collection/compat/extra.scala
@@ -3,4 +3,6 @@ package scala.collection.compat
 object extra {
   type Wrappers = scala.collection.convert.JavaCollectionWrappers.type
   val Wrappers = scala.collection.convert.JavaCollectionWrappers
+
+  val CollectionConverters = scala.jdk.javaapi.CollectionConverters
 }
