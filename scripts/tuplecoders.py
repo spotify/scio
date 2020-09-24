513ac81194cdc634b56800e9206a6bb0712aed6d
Filipe Regadas <filiperegadas@gmail.com>
2020-09-24 16:15:22 +0100

    Generate TupleCoders

--- scripts/tuplecoders.py
+++ scripts/tuplecoders.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 #  Copyright 2016 Spotify AB.
 #
@@ -18,16 +18,13 @@
 import string
 import sys
 import textwrap
+import os
 
 
 # Utilities
 
 def mkVals(n):
-    return list(string.uppercase.replace('F', '')[:n])
-
-
-def mkTypes(n):
-    return ', '.join(mkVals(n))
+    return list(string.ascii_uppercase.replace('F', '')[:n])
 
 def mkBounds(n):
     return ', '.join(x + ': Coder' for x in mkVals(n))
@@ -35,15 +32,95 @@ def mkBounds(n):
 # Functions
 
 def tupleFns(out, n):
-    types = mkTypes(n)
-    print >> out, '''    
-    implicit def tuple{size}Coder[{bounds}]: Coder[({type_list})] = {{
-      Coder.gen[({type_list})]
-    }}'''.format(size=n, bounds=mkBounds(n), type_list=types)
+    t_type = f'T{string.ascii_uppercase[n-1]}'
+    types = mkVals(n)
+    bounds = mkBounds(n)
+
+    def coder_transform(a):
+        if len(a) == 1:
+            return f'''Coder.transform(C{a[0]})({a[0].lower()}c => Coder.beam(new Tuple{n}Coder[{','.join(types)}]({','.join(t.lower() + 'c' for t in types)})))'''
+        else:
+            return f'''Coder.transform(C{a[0]}) {{ {a[0].lower()}c =>''' + coder_transform(a[1:]) + "}"
+        
+    print(f'''    
+final private class Tuple{n}Coder[{','.join(types)}]({','.join(f'{t.lower()}c: BCoder[{t}]' for t in types)}) extends AtomicCoder[({','.join(types)})] {{
+  private[this] val materializationStackTrace: Array[StackTraceElement] = CoderStackTrace.prepare
+
+  @inline def onErrorMsg[{t_type}](msg: => (String, String))(f: => {t_type}): {t_type} =
+    try {{
+      f
+    }} catch {{
+      case e: Exception =>
+        // allow Flink memory management, see WrappedBCoder#catching comment.
+        throw CoderStackTrace.append(
+          e,
+          Some(
+            s"Exception while trying to `${{msg._1}}` an instance" +
+              s" of Tuple{n}: Can't decode field ${{msg._2}}"
+          ),
+          materializationStackTrace
+        )
+    }}
+
+  override def encode(value: ({','.join(types)}), os: OutputStream): Unit = {{
+    {os.linesep.join(f'onErrorMsg("encode" -> "_{idx}")({t.lower()}c.encode(value._{idx}, os))' for idx, t in enumerate(types, 1))}
+  }}
+  override def decode(is: InputStream): ({','.join(types)}) = {{
+    ({','.join(f'onErrorMsg("decode" -> "_{idx}")({t.lower()}c.decode(is))' for idx, t in enumerate(types, 1))})
+  }}
+
+  override def toString: String =
+    s"Tuple{n}Coder({', '.join(f'_{idx} -> ${t.lower()}c' for idx, t in enumerate(types, 1))})"
+
+  // delegate methods for determinism and equality checks
+
+  override def verifyDeterministic(): Unit = {{
+    val cs = List({', '.join(f'"_{idx}" -> {t.lower()}c' for idx, t in enumerate(types, 1))})
+    val problems = cs.flatMap {{ case (label, c) =>
+      try {{
+        c.verifyDeterministic()
+        Nil
+      }} catch {{
+        case e: NonDeterministicException =>
+          val reason = s"field $label is using non-deterministic $c"
+          List(reason -> e)
+      }}
+    }}
+
+    problems match {{
+      case (_, e) :: _ =>
+        val reasons = problems.map {{ case (reason, _) => reason }}
+        throw new NonDeterministicException(this, reasons.asJava, e)
+      case Nil =>
+    }}
+  }}
+
+  override def consistentWithEquals(): Boolean =
+    {' && '.join(f'{t.lower()}c.consistentWithEquals()' for t in types)}
+
+  override def structuralValue(value: ({','.join(types)})): AnyRef =
+    if (consistentWithEquals()) {{
+      value.asInstanceOf[AnyRef]
+    }} else {{
+        ({','.join(f'{t.lower()}c.structuralValue(value._{idx})' for idx, t in enumerate(types, 1))})
+    }}
+
+  // delegate methods for byte size estimation
+  override def isRegisterByteSizeObserverCheap(value: ({','.join(types)})): Boolean =
+    {' && '.join(f'{t.lower()}c.isRegisterByteSizeObserverCheap(value._{idx})' for idx, t in enumerate(types, 1))}
+
+  override def registerByteSizeObserver(value: ({','.join(types)}), observer: ElementByteSizeObserver): Unit = {{
+    {os.linesep.join(f'{t.lower()}c.registerByteSizeObserver(value._{idx}, observer)' for idx, t in enumerate(types, 1))}
+  }}
+}}
+    
+    implicit def tuple{n}Coder[{','.join(types)}](implicit {','.join(f'C{t}: Coder[{t}]' for t in types)}): Coder[({','.join(types)})] = {{
+    {coder_transform(types)}
+    }}''', file=out)
 
 
 def main(out):
-    print >> out, textwrap.dedent('''
+    print(textwrap.dedent('''
         /*
          * Copyright 2020 Spotify AB.
          *
@@ -69,16 +146,23 @@ def main(out):
     
         package com.spotify.scio.coders.instances
 
-        import com.spotify.scio.coders.Coder
+        import java.io.{InputStream, OutputStream}
+
+        import com.spotify.scio.coders.{Coder, CoderStackTrace}
+        import org.apache.beam.sdk.coders.Coder.NonDeterministicException
+        import org.apache.beam.sdk.coders.{Coder => BCoder, _}
+        import org.apache.beam.sdk.util.common.ElementByteSizeObserver
+
+        import scala.jdk.CollectionConverters._
 
         trait TupleCoders {
-        ''').replace('  # NOQA', '').lstrip('\n')
+        ''').replace('  # NOQA', '').lstrip('\n'), file=out)
 
     N = 22
-    for i in xrange(3, N + 1):
+    for i in range(3, N + 1):
         tupleFns(out, i)
 
-    print >> out, '}'
+    print('}', file=out)
 
 if __name__ == '__main__':
     main(sys.stdout)
