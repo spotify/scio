b3cee3584885b7e835e2771fce4f7b24c6ec188c
Filipe Regadas <filiperegadas@gmail.com>
2020-11-03 13:06:24 +0000

    Revert read/write test param removal

--- scio-core/src/main/scala/com/spotify/scio/io/ScioIO.scala
+++ scio-core/src/main/scala/com/spotify/scio/io/ScioIO.scala
@@ -67,31 +67,19 @@ trait ScioIO[T] {
   def testId: String = this.toString
 
   private[scio] def readWithContext(sc: ScioContext, params: ReadP): SCollection[T] =
-    sc.requireNotClosed {
-      if (sc.isTest) {
-        readTest(sc)
-      } else {
-        read(sc, params)
-      }
-    }
+    sc.requireNotClosed(if (sc.isTest) readTest(sc, params) else read(sc, params))
 
-  protected def readTest(sc: ScioContext): SCollection[T] =
+  protected def readTest(sc: ScioContext, params: ReadP): SCollection[T] =
     TestDataManager.getInput(sc.testId.get)(this).toSCollection(sc)
 
   protected def read(sc: ScioContext, params: ReadP): SCollection[T]
 
   private[scio] def writeWithContext(data: SCollection[T], params: WriteP): ClosedTap[tapT.T] =
-    ClosedTap {
-      if (data.context.isTest) {
-        writeTest(data)
-      } else {
-        write(data, params)
-      }
-    }
+    ClosedTap(if (data.context.isTest) writeTest(data, params) else write(data, params))
 
   protected def write(data: SCollection[T], params: WriteP): Tap[tapT.T]
 
-  protected def writeTest(data: SCollection[T]): Tap[tapT.T] = {
+  protected def writeTest(data: SCollection[T], params: WriteP): Tap[tapT.T] = {
     TestDataManager.getOutput(data.context.testId.get)(this)(data)
     tapT.saveForTest(data)
   }
