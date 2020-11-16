b7dbe84305cf2d75c9e7b0444ab66f8de1717b1d
Martin <martinbomio@gmail.com>
2020-11-16 09:46:59 -0500

    Update scio-core/src/main/scala/com/spotify/scio/hash/ApproxFilter.scala
    Co-authored-by: Filipe Regadas <filiperegadas@gmail.com>

--- scio-core/src/main/scala/com/spotify/scio/hash/ApproxFilter.scala
+++ scio-core/src/main/scala/com/spotify/scio/hash/ApproxFilter.scala
@@ -277,7 +277,7 @@ sealed trait ApproxFilterCompanion {
       val settings = partitionSettings(expectedInsertions, fpp, 100 * 1024 * 1024)
       logger.info(
         s"Partition settings for approximate filter side input of $expectedInsertions keys: " +
-          "partitions=$settings.partitions, expectedInsertions=$settings.expectedInsertions," +
+          s"partitions=${settings.partitions}, expectedInsertions=${settings.expectedInsertions}," +
           "sizeBytes=$settings.sizeBytes"
       )
       elems
