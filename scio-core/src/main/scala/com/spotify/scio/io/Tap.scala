67d77d33c4708babeaf879aff15315bdc201eb19
Neville Li <neville.lyh@gmail.com>
2020-11-17 12:48:32 -0500

    fix doc error in Tap.scala

--- scio-core/src/main/scala/com/spotify/scio/io/Tap.scala
+++ scio-core/src/main/scala/com/spotify/scio/io/Tap.scala
@@ -59,6 +59,7 @@ trait Tap[T] extends Serializable { self =>
 }
 
 case object EmptyTap extends Tap[Nothing] {
+  implicit val nothingCoder: Coder[Nothing] = Coder.xmap[Unit, Nothing](Coder[Unit])(???, ???)
   override def value: Iterator[Nothing] = Iterator.empty
   override def open(sc: ScioContext): SCollection[Nothing] = sc.empty[Nothing]
 }
