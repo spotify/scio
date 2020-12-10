20bf67c2c2f2329665ad05215253e795db588abc
Julien Tournay <julient@spotify.com>
2020-12-10 11:18:37 +0100

    fallback coder ?

--- scio-core/src/main/scala-2/com/spotify/scio/coders/FallbackCoder.scala
+++ scio-core/src/main/scala-2/com/spotify/scio/coders/FallbackCoder.scala
@@ -1,6 +1,7 @@
 package com.spotify.scio.coders
 
 trait FallbackCoder {
+  // TODO: scala3 - Make that the fallback implicit
   def fallback[T](implicit lp: shapeless.LowPriority): Coder[T] =
     macro CoderMacros.issueFallbackWarning[T]
 }
