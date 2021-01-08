539c1572a985e8fae264aa8cca08dc56f0a03621
Filipe Regadas <filiperegadas@gmail.com>
2021-01-08 12:40:23 +0000

    Rework RedisMutator (#3543)

--- scio-redis/src/main/scala/com/spotify/scio/redis/RedisMutator.scala
+++ scio-redis/src/main/scala/com/spotify/scio/redis/RedisMutator.scala
@@ -21,171 +21,138 @@ import com.spotify.scio.redis.types._
 import redis.clients.jedis.{Pipeline, Response}
 
 import java.lang.{Long => JLong}
+import com.spotify.scio.redis.types.RedisType.{ByteArrayRedisType, StringRedisType}
+import redis.clients.jedis.util.SafeEncoder
 
 sealed abstract class RedisMutator[T] extends Serializable {
   def mutate(client: Pipeline, mutation: T): List[Response[_]]
 }
 
 object RedisMutator {
-
-  implicit val stringAppend: RedisMutator[Append[String]] =
-    new RedisMutator[Append[String]] {
-      override def mutate(client: Pipeline, mutation: Append[String]): List[Response[JLong]] = {
-        client.append(mutation.key, mutation.value) ::
-          mutation.ttl
-            .map(expireTime => client.pexpire(mutation.key, expireTime.getMillis))
-            .toList
+  implicit def appendMutator[T]: RedisMutator[Append[T]] = new RedisMutator[Append[T]] {
+    override def mutate(client: Pipeline, mutation: Append[T]): List[Response[JLong]] = {
+      val (key, value) = mutation.rt match {
+        case ByteArrayRedisType =>
+          (mutation.key, mutation.value)
+        case StringRedisType =>
+          (SafeEncoder.encode(mutation.key), SafeEncoder.encode(mutation.value))
       }
-    }
 
-  implicit val byteArrayAppend: RedisMutator[Append[Array[Byte]]] =
-    new RedisMutator[Append[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: Append[Array[Byte]]): List[Response[JLong]] =
-        client.append(mutation.key, mutation.value) ::
-          mutation.ttl
-            .map(expireTime => client.pexpire(mutation.key, expireTime.getMillis))
-            .toList
+      client.append(key, value) ::
+        mutation.ttl
+          .map(expireTime => client.pexpire(key, expireTime.getMillis))
+          .toList
     }
+  }
 
-  implicit val stringSet: RedisMutator[Set[String]] =
-    new RedisMutator[Set[String]] {
-      override def mutate(client: Pipeline, mutation: Set[String]): List[Response[_]] =
-        mutation.ttl.fold(client.set(mutation.key, mutation.value)) { ttl =>
-          client.psetex(mutation.key, ttl.getMillis(), mutation.value)
-        } :: Nil
+  implicit def setMutator[T]: RedisMutator[Set[T]] = new RedisMutator[Set[T]] {
+    override def mutate(client: Pipeline, mutation: Set[T]): List[Response[_]] = {
+      val (key, value) = mutation.rt match {
+        case ByteArrayRedisType =>
+          (mutation.key, mutation.value)
+        case StringRedisType =>
+          (SafeEncoder.encode(mutation.key), SafeEncoder.encode(mutation.value))
+      }
+
+      mutation.ttl.fold(client.set(key, value)) { ttl =>
+        client.psetex(key, ttl.getMillis(), value)
+      } :: Nil
     }
+  }
 
-  implicit val byteArraySet: RedisMutator[Set[Array[Byte]]] =
-    new RedisMutator[Set[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: Set[Array[Byte]]): List[Response[_]] =
-        mutation.ttl.fold(client.set(mutation.key, mutation.value)) { ttl =>
-          client.psetex(mutation.key, ttl.getMillis(), mutation.value)
-        } :: Nil
+  implicit def incrByMutator[T]: RedisMutator[IncrBy[T]] = new RedisMutator[IncrBy[T]] {
+    override def mutate(client: Pipeline, mutation: IncrBy[T]): List[Response[JLong]] = {
+      val key = mutation.rt match {
+        case ByteArrayRedisType => mutation.key
+        case StringRedisType    => SafeEncoder.encode(mutation.key)
+      }
+      client.incrBy(key, mutation.value) ::
+        mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
     }
+  }
 
-  implicit val stringIncrBy: RedisMutator[IncrBy[String]] =
-    new RedisMutator[IncrBy[String]] {
-      override def mutate(client: Pipeline, mutation: IncrBy[String]): List[Response[JLong]] =
-        client.incrBy(mutation.key, mutation.value) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
+  implicit def decrByMutator[T]: RedisMutator[DecrBy[T]] = new RedisMutator[DecrBy[T]] {
+    override def mutate(client: Pipeline, mutation: DecrBy[T]): List[Response[JLong]] = {
+      val key = mutation.rt match {
+        case ByteArrayRedisType => mutation.key
+        case StringRedisType    => SafeEncoder.encode(mutation.key)
+      }
+      client.decrBy(key, mutation.value) ::
+        mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
     }
+  }
 
-  implicit val byteArrayIncrBy: RedisMutator[IncrBy[Array[Byte]]] =
-    new RedisMutator[IncrBy[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: IncrBy[Array[Byte]]): List[Response[JLong]] =
-        client.incrBy(mutation.key, mutation.value) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
+  implicit def saddMutator[T]: RedisMutator[SAdd[T]] = new RedisMutator[SAdd[T]] {
+    override def mutate(client: Pipeline, mutation: SAdd[T]): List[Response[JLong]] = {
+      val (key, value) = mutation.rt match {
+        case ByteArrayRedisType =>
+          (mutation.key, mutation.value)
+        case StringRedisType =>
+          (SafeEncoder.encode(mutation.key), mutation.value.map(SafeEncoder.encode(_)))
+      }
+
+      client.sadd(key, value: _*) ::
+        mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
     }
+  }
 
-  implicit val stringDecrBy: RedisMutator[DecrBy[String]] =
-    new RedisMutator[DecrBy[String]] {
-      override def mutate(client: Pipeline, mutation: DecrBy[String]): List[Response[JLong]] =
-        client.decrBy(mutation.key, mutation.value) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
+  implicit def lpushMutator[T]: RedisMutator[LPush[T]] = new RedisMutator[LPush[T]] {
+    override def mutate(client: Pipeline, mutation: LPush[T]): List[Response[JLong]] = {
+      val (key, value) = mutation.rt match {
+        case ByteArrayRedisType =>
+          (mutation.key, mutation.value)
+        case StringRedisType =>
+          (SafeEncoder.encode(mutation.key), mutation.value.map(SafeEncoder.encode(_)))
+      }
+
+      client.lpush(key, value: _*) ::
+        mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
     }
+  }
 
-  implicit val byteArrayDecrBy: RedisMutator[DecrBy[Array[Byte]]] =
-    new RedisMutator[DecrBy[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: DecrBy[Array[Byte]]): List[Response[JLong]] =
-        client.decrBy(mutation.key, mutation.value) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
+  implicit def rpushMutator[T]: RedisMutator[RPush[T]] = new RedisMutator[RPush[T]] {
+    override def mutate(client: Pipeline, mutation: RPush[T]): List[Response[JLong]] = {
+      val (key, value) = mutation.rt match {
+        case ByteArrayRedisType =>
+          (mutation.key, mutation.value)
+        case StringRedisType =>
+          (SafeEncoder.encode(mutation.key), mutation.value.map(SafeEncoder.encode(_)))
+      }
+
+      client.rpush(key, value: _*) ::
+        mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
     }
+  }
 
-  implicit val stringSAdd: RedisMutator[SAdd[String]] =
-    new RedisMutator[SAdd[String]] {
-      override def mutate(client: Pipeline, mutation: SAdd[String]): List[Response[JLong]] =
-        client.sadd(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val byteArraySAdd: RedisMutator[SAdd[Array[Byte]]] =
-    new RedisMutator[SAdd[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: SAdd[Array[Byte]]): List[Response[JLong]] =
-        client.sadd(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val stringLPush: RedisMutator[LPush[String]] =
-    new RedisMutator[LPush[String]] {
-      override def mutate(client: Pipeline, mutation: LPush[String]): List[Response[JLong]] =
-        client.lpush(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val byteArrayLPush: RedisMutator[LPush[Array[Byte]]] =
-    new RedisMutator[LPush[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: LPush[Array[Byte]]): List[Response[JLong]] =
-        client.lpush(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val stringRPush: RedisMutator[RPush[String]] =
-    new RedisMutator[RPush[String]] {
-      override def mutate(client: Pipeline, mutation: RPush[String]): List[Response[JLong]] =
-        client.rpush(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val byteArrayRPush: RedisMutator[RPush[Array[Byte]]] =
-    new RedisMutator[RPush[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: RPush[Array[Byte]]): List[Response[JLong]] =
-        client.rpush(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val stringPFAdd: RedisMutator[PFAdd[String]] =
-    new RedisMutator[PFAdd[String]] {
-      override def mutate(client: Pipeline, mutation: PFAdd[String]): List[Response[JLong]] =
-        client.pfadd(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit val byteArrayPFAdd: RedisMutator[PFAdd[Array[Byte]]] =
-    new RedisMutator[PFAdd[Array[Byte]]] {
-      override def mutate(client: Pipeline, mutation: PFAdd[Array[Byte]]): List[Response[JLong]] =
-        client.pfadd(mutation.key, mutation.value: _*) ::
-          mutation.ttl.map(expireTime => client.pexpire(mutation.key, expireTime.getMillis)).toList
-    }
-
-  implicit def redisMutator[T <: RedisMutation]: RedisMutator[T] =
-    new RedisMutator[T] {
-      override def mutate(client: Pipeline, mutation: T): List[Response[_]] = {
-        mutation match {
-          case RedisMutation(mt: Append[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: Append[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: Set[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: Set[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: IncrBy[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: IncrBy[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: DecrBy[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: DecrBy[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: SAdd[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: SAdd[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: RPush[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: RPush[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: LPush[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: LPush[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: PFAdd[Array[Byte] @unchecked], RedisType.ByteArrayRedisType) =>
-            RedisMutator.mutate(client)(mt)
-          case RedisMutation(mt: PFAdd[String @unchecked], RedisType.StringRedisType) =>
-            RedisMutator.mutate(client)(mt)
+  implicit def pfaddMutator[T]: RedisMutator[PFAdd[T]] =
+    new RedisMutator[PFAdd[T]] {
+      override def mutate(client: Pipeline, mutation: PFAdd[T]): List[Response[JLong]] = {
+        val (key, value) = mutation.rt match {
+          case ByteArrayRedisType =>
+            (mutation.key, mutation.value)
+          case StringRedisType =>
+            (SafeEncoder.encode(mutation.key), mutation.value.map(SafeEncoder.encode(_)))
         }
+
+        client.pfadd(key, value: _*) ::
+          mutation.ttl.map(expireTime => client.pexpire(key, expireTime.getMillis)).toList
       }
     }
+
+  implicit def redisMutator[T <: RedisMutation]: RedisMutator[T] = new RedisMutator[T] {
+    override def mutate(client: Pipeline, mutation: T): List[Response[_]] = {
+      mutation match {
+        case mt @ Append(_, _, _) => appendMutator.mutate(client, mt)
+        case mt @ Set(_, _, _)    => setMutator.mutate(client, mt)
+        case mt @ IncrBy(_, _, _) => incrByMutator.mutate(client, mt)
+        case mt @ DecrBy(_, _, _) => decrByMutator.mutate(client, mt)
+        case mt @ SAdd(_, _, _)   => saddMutator.mutate(client, mt)
+        case mt @ LPush(_, _, _)  => lpushMutator.mutate(client, mt)
+        case mt @ RPush(_, _, _)  => rpushMutator.mutate(client, mt)
+        case mt @ PFAdd(_, _, _)  => pfaddMutator.mutate(client, mt)
+      }
+    }
+  }
 
   def mutate[T <: RedisMutation: RedisMutator](client: Pipeline)(value: T): List[Response[_]] =
     implicitly[RedisMutator[T]].mutate(client, value)
