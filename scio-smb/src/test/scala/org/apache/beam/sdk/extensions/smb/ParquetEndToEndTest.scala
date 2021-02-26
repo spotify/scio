d187e6705d63e208ea780497b5d06f0b5ec67895
Neville Li <neville.lyh@gmail.com>
2021-02-26 15:18:27 -0500

    add Parquet SMB E2E test

---
+++ scio-smb/src/test/scala/org/apache/beam/sdk/extensions/smb/ParquetEndToEndTest.scala
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2021 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.beam.sdk.extensions.smb
+
+import java.io.File
+import java.util.UUID
+import com.spotify.scio._
+import com.spotify.scio.smb._
+import com.spotify.scio.testing._
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericRecord, GenericRecordBuilder}
+import org.apache.beam.sdk.values.TupleTag
+
+import scala.jdk.CollectionConverters._
+
+object ParquetEndToEndTest {
+  val eventSchema = Schema.createRecord(
+    "Event",
+    "",
+    "org.apache.beam.sdk.extensions.smb.avro",
+    false,
+    List(
+      new Schema.Field("user", Schema.create(Schema.Type.STRING), "", ""),
+      new Schema.Field("event", Schema.create(Schema.Type.STRING), "", ""),
+      new Schema.Field("timestamp", Schema.create(Schema.Type.INT), "", 0)
+    ).asJava
+  )
+
+  val userSchema = Schema.createRecord(
+    "User",
+    "",
+    "org.apache.beam.sdk.extensions.smb.avro",
+    false,
+    List(
+      new Schema.Field("name", Schema.create(Schema.Type.STRING), "", ""),
+      new Schema.Field("age", Schema.create(Schema.Type.INT), "", 0)
+    ).asJava
+  )
+
+  def avroEvent(x: Int): GenericRecord = new GenericRecordBuilder(eventSchema)
+    .set("user", s"user${x % 10 + 1}")
+    .set("event", s"event$x")
+    .set("timestamp", x)
+    .build()
+
+  def avroUser(x: Int): GenericRecord = new GenericRecordBuilder(userSchema)
+    .set("name", s"user$x")
+    .set("age", x)
+    .build()
+
+  case class Event(user: String, event: String, timestamp: Int)
+  case class User(name: String, age: Int)
+
+  object Event {
+    def apply(x: Int): Event = Event(s"user${x % 10 + 1}", s"event$x", x)
+  }
+
+  object User {
+    def apply(x: Int): User = User(s"user$x", x)
+  }
+
+  val avroEvents = (1 to 100).map(avroEvent)
+  val avroUsers = (1 to 15).map(avroUser)
+  val events = (1 to 100).map(Event(_))
+  val users = (1 to 15).map(User(_))
+}
+
+class ParquetEndToEndTest extends PipelineSpec {
+  import ParquetEndToEndTest._
+
+  def tmpDir = new File(new File(CoreSysProps.TmpDir.value), "scio-test-" + UUID.randomUUID())
+
+  "Parquet SMB" should "support writing Avro reading typed" in {
+    val eventsDir = tmpDir
+    val usersDir = tmpDir
+
+    val sc1 = ScioContext()
+    // Write one with keyClass = CharSequence and one with String
+    // Downstream should be able to handle the difference
+    sc1
+      .parallelize(avroEvents)
+      .saveAsSortedBucket(
+        ParquetAvroSortedBucketIO
+          .write(classOf[CharSequence], "user", eventSchema)
+          .to(eventsDir.toString)
+      )
+    sc1
+      .parallelize(avroUsers)
+      .saveAsSortedBucket(
+        ParquetAvroSortedBucketIO
+          .write(classOf[String], "name", userSchema)
+          .to(usersDir.toString)
+      )
+    sc1.run()
+
+    val sc2 = ScioContext()
+    val actual = sc2
+      .sortMergeJoin(
+        classOf[String],
+        ParquetTypeSortedBucketIO
+          .read[Event](new TupleTag[Event]("lhs"))
+          .from(eventsDir.toString),
+        ParquetTypeSortedBucketIO
+          .read[User](new TupleTag[User]("rhs"))
+          .from(usersDir.toString)
+      )
+    val userMap = users.groupBy(_.name).view.mapValues(_.head)
+    val expected = events.groupBy(_.user).toSeq.flatMap { case (k, es) =>
+      es.map(e => (k, (e, userMap(k))))
+    }
+    actual should containInAnyOrder(expected)
+    sc2.run()
+
+    eventsDir.delete()
+    usersDir.delete()
+  }
+
+  it should "support writing typed reading Avro" in {
+    val eventsDir = tmpDir
+    val usersDir = tmpDir
+
+    val sc1 = ScioContext()
+    sc1
+      .parallelize(events)
+      .saveAsSortedBucket(
+        ParquetTypeSortedBucketIO
+          .write[String, Event]("user")
+          .to(eventsDir.toString)
+      )
+    sc1
+      .parallelize(users)
+      .saveAsSortedBucket(
+        ParquetTypeSortedBucketIO
+          .write[String, User]("name")
+          .to(usersDir.toString)
+      )
+    sc1.run()
+
+    val sc2 = ScioContext()
+    val actual = sc2
+      .sortMergeJoin(
+        classOf[String],
+        ParquetAvroSortedBucketIO
+          .read(new TupleTag[GenericRecord]("lhs"), eventSchema)
+          .from(eventsDir.toString),
+        ParquetAvroSortedBucketIO
+          .read(new TupleTag[GenericRecord]("rhs"), userSchema)
+          .from(usersDir.toString)
+      )
+    val userMap = avroUsers.groupBy(_.get("name").toString).view.mapValues(_.head)
+    val expected = avroEvents.groupBy(_.get("user").toString).toSeq.flatMap { case (k, es) =>
+      es.map(e => (k, (e, userMap(k))))
+    }
+    actual should containInAnyOrder(expected)
+    sc2.run()
+
+    eventsDir.delete()
+    usersDir.delete()
+  }
+}
