4725b42969923e46951c59e89272a51a9afb8ef9
Annica Ivert <annicai@spotify.com>
2021-03-09 14:19:56 +0100

    Remved unnecessary measure var, formatting

--- scio-extra/src/main/scala/com/spotify/scio/extra/rollup/syntax/SCollectionSyntax.scala
+++ scio-extra/src/main/scala/com/spotify/scio/extra/rollup/syntax/SCollectionSyntax.scala
@@ -10,30 +10,29 @@ trait SCollectionSyntax {
 
     /**
      * Takes an [[SCollection]] with elements consisting of three sets of dimensions and one
-      * measure and returns an [[SCollection]] tuple, where the key is a set of dimensions and
-      * the value the summed measure combined with a distinct count.
-      *
-      * This is to be used when doing a count dinstint for one key over a set of
-      * dimensions, when that key can be present in multiple elements in the final dataset,
-      * such that there is a need to provide additional rollups over the non-unique dimensions
-      * where distinct counts are not summable.
-      *
-      * U - Unique key, this is what we want to count distinct occurences of
-      * D - Dimensions that should not be rolled up (these are either unique per U or we are not
-      *     expected to sum U over these dimensions, eg. a metric for different dates)
-      * R - Dimensions that should be rolled up
-      * M - Additional measure that is summable over all dimensions
+     * measure and returns an [[SCollection]] tuple, where the key is a set of dimensions and
+     * the value the summed measure combined with a distinct count.
+     *
+     * This is to be used when doing a count dinstint for one key over a set of
+     * dimensions, when that key can be present in multiple elements in the final dataset,
+     * such that there is a need to provide additional rollups over the non-unique dimensions
+     * where distinct counts are not summable.
+     *
+     * U - Unique key, this is what we want to count distinct occurences of
+     * D - Dimensions that should not be rolled up (these are either unique per U or we are not
+     *     expected to sum U over these dimensions, eg. a metric for different dates)
+     * R - Dimensions that should be rolled up
+     * M - Additional measure that is summable over all dimensions
      *
      * @param rollupFunction A function takes dimensions R for one element and creates new element
-      *                       for each combination of rollups that we want to provide
-      *
+     *                       for each combination of rollups that we want to provide
      */
     def rollupAndCount(rollupFunction: R => Set[R])(implicit
-                                                    c: Coder[U],
-                                                    d: Coder[D],
-                                                    r: Coder[R],
-                                                    m: Coder[M],
-                                                    g: Group[M]
+      c: Coder[U],
+      d: Coder[D],
+      r: Coder[R],
+      m: Coder[M],
+      g: Group[M]
     ): SCollection[((D, R), (M, Long))] = {
 
       val doubleCounting = self
@@ -49,14 +48,14 @@ trait SCollectionSyntax {
         }
 
       val correctingCounts = self
-        .map { case (uniqueKey, dims, rollupDims, measure) =>
-          ((uniqueKey, dims), (rollupDims, measure))
+        .map { case (uniqueKey, dims, rollupDims, _) =>
+          ((uniqueKey, dims), rollupDims)
         }
         .groupByKey
         .filterValues(_.size > 1)
         .flatMapValues {
-          _.flatMap { case (rollupDims, _) =>
-            rollupFunction(rollupDims)
+          _.flatMap {
+            rollupFunction(_)
               .map(newDims => (newDims, -1L))
           }
             .groupBy { case (rollupDims, _) =>
@@ -72,8 +71,7 @@ trait SCollectionSyntax {
         }
         .map { case ((_, dims), (rollupDims, count)) => ((dims, rollupDims), (g.zero, count)) }
 
-      SCollection.unionAll(List(doubleCounting, correctingCounts))
-        .sumByKey
+      SCollection.unionAll(List(doubleCounting, correctingCounts)).sumByKey
 
     }
 
