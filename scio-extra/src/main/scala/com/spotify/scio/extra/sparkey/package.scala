4d32157f75b39c5fc46d8b02dae662a02384e2ad
Peter Sobot <psobot@spotify.com>
2021-03-11 12:25:47 -0500

    Add Sparkey-backed .asLargeMapSideInput with coder support.

--- scio-extra/src/main/scala/com/spotify/scio/extra/sparkey/package.scala
+++ scio-extra/src/main/scala/com/spotify/scio/extra/sparkey/package.scala
@@ -26,6 +26,7 @@ import com.spotify.scio.extra.sparkey.instances._
 import com.spotify.scio.util.Cache
 import com.spotify.scio.values.{SCollection, SideInput}
 import com.spotify.sparkey.{CompressionType, SparkeyReader}
+import org.apache.beam.sdk.coders
 import org.apache.beam.sdk.io.FileSystems
 import org.apache.beam.sdk.transforms.{DoFn, View}
 import org.apache.beam.sdk.util.CoderUtils
