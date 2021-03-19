cc31efab9f9c72fa6ae802d301b0f9a65df9d78c
Neville Li <neville.lyh@gmail.com>
2021-03-18 22:29:52 -0400

    fix tests

--- scio-test/src/test/scala/com/spotify/scio/values/PairHashSCollectionFunctionsTest.scala
+++ scio-test/src/test/scala/com/spotify/scio/values/PairHashSCollectionFunctionsTest.scala
@@ -245,7 +245,7 @@ class PairHashSCollectionFunctionsTest extends PipelineSpec {
 
   it should "support hashSubtractByKey() with empty LHS" in {
     runWithContext { sc =>
-      val p1 = sc.parallelize(Seq.empty[(String, Any)])
+      val p1 = sc.parallelize(Seq.empty[(String, Int)])
       val p2 = sc.parallelize(Seq("1", "2", "3"))
       val output = p1.hashSubtractByKey(p2)
       output should beEmpty
