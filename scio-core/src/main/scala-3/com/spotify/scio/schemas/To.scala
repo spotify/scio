e55b1a1b65261a43e2f2017332d89570dd0879e7
Maxime Kjaer <maxime.kjaer@gmail.com>
2021-03-30 23:36:21 +0200

    Continue To.safe macro implementation

--- scio-core/src/main/scala-3/com/spotify/scio/schemas/To.scala
+++ scio-core/src/main/scala-3/com/spotify/scio/schemas/To.scala
@@ -27,23 +27,52 @@ import scala.quoted._
 
 object ToMacro {
 
-  def interpretSchema[T: Type](schemaExpr: Expr[Schema[T]])(using Quotes): Option[Schema[T]] =
-    schemaExpr match
+  def interpretSchema[T: Type](schemaExpr: Expr[Schema[T]])(using Quotes): Option[Schema[T]] = schemaExpr match {
+    case '{ Schema.stringSchema }     => Some(Schema.stringSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.byteSchema }       => Some(Schema.byteSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.bytesSchema }      => Some(Schema.bytesSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.sortSchema }       => Some(Schema.sortSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.intSchema }        => Some(Schema.intSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.longSchema }       => Some(Schema.longSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.floatSchema }      => Some(Schema.floatSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.doubleSchema }     => Some(Schema.doubleSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.bigDecimalSchema } => Some(Schema.bigDecimalSchema.asInstanceOf[Schema[T]])
+    case '{ Schema.booleanSchema }    => Some(Schema.booleanSchema.asInstanceOf[Schema[T]])
+
     case '{ Schema.optionSchema[t](using $tSchemaExpr) } =>
-      for
-        tSchema <- interpretSchema(tSchemaExpr)
-      yield
-        Schema.optionSchema(using tSchema).asInstanceOf[Schema[T]]
+      for (tSchema <- interpretSchema(tSchemaExpr))
+      yield Schema.optionSchema(using tSchema).asInstanceOf[Schema[T]]
+
     case '{ Schema.mapSchema[k, v](using $keySchemaExpr, $valueSchemaExpr) } =>
-      for 
+      for {
         keySchema <- interpretSchema(keySchemaExpr)
         valueSchema <- interpretSchema(valueSchemaExpr)
-      yield Schema.mapSchema(using keySchema, valueSchema).asInstanceOf[Schema[T]]
+      } yield Schema.mapSchema(using keySchema, valueSchema).asInstanceOf[Schema[T]]
+
     case _ => None
+  }
 
 
-  def safeImpl[I, O](si: Expr[Schema[I]])(implicit q: Quotes): Expr[To[I, O]] = {
-    ???
+  def safeImpl[I, O](iSchema: Expr[Schema[I]], oSchema: Expr[Schema[O]])(implicit q: Quotes): Expr[To[I, O]] = {
+    import scala.quoted.quotes.reflect.report
+
+    (interpretSchema(iSchema), interpretSchema(oSchema)) match {
+      case (None, None) => report.throwError(
+        s"""
+        |Could not interpret input schema:
+        |  ${iSchema.show}
+        |Could not interpret output schema:
+        |  ${oSchema.show}
+        |""".stripMargin
+      )
+      case (None, _) => report.throwError("Could not interpret input schema: " + iSchema.show)
+      case (_, None) => report.throwError("Could not interpret output schema: " + oSchema.show)
+      case (Some(sIn), Some(sOut)) =>
+        val schemaOut: BSchema = SchemaMaterializer.fieldType(sOut).getRowSchema()
+        val schemaIn: BSchema = SchemaMaterializer.fieldType(sIn).getRowSchema()
+        To.checkCompatibility(schemaIn, schemaOut)('{ To.unchecked[I, O] })
+          .fold(message => report.throwError(message), identity)
+    }
   }
 }
 
@@ -54,7 +83,6 @@ trait ToMacro {
    * at compile time.
    * @see To#unsafe
    */
-  // TODO: scala3
-  inline def safe[I, O](inline si: Schema[I], inline so: Schema[O]): To[I, O] =
-    ???
+  inline def safe[I, O](inline iSchema: Schema[I], inline oSchema: Schema[O]): To[I, O] = 
+    ${ ToMacro.safeImpl('iSchema, 'oSchema) }
 }
