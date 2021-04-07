spotify/scio

commit c52f775c48d4ba4f15a1efd3e81c452bbffec3b9

Support `verifyDeterministic` and `consistentWithEquals` in recursive coders (#3716)
* test consistentWithEquals for recursive coders

* Format CoderAssertions

* Support verifyDeterministic and consistentWithEquals in recursive coders

Co-authored-by: Anish Chakraborty <anish749@gmail.com>

scio-core/src/main/scala/com/spotify/scio/coders/Coder.scala
@@ -184,9 +184,55 @@ final private[scio] case class LazyCoder[T](
   def decode(inStream: InputStream): T = bcoder.decode(inStream)
   def encode(value: T, outStream: OutputStream): Unit = bcoder.encode(value, outStream)
   def getCoderArguments(): java.util.List[_ <: BCoder[_]] = bcoder.getCoderArguments()
-  def verifyDeterministic(): Unit = bcoder.verifyDeterministic()
 
-  override def consistentWithEquals(): Boolean = bcoder.consistentWithEquals()
+  /**
+   * Traverse this coder graph and create a version of it without any loop.
+   * Fixes: [[https://github.com/spotify/scio/issues/3707 #3707]]
+   * Should only be used in [[verifyDeterministic]], [[consistentWithEquals]]
+   * and other methods that DO NOT actually serialize / deserialize the data.
+   * Internally, looping coders are replaced by an instance of Coder[Nothing]
+   */
+  private def uncycled: Coder[T] = {
+    def go[B](c: Coder[B], types: Set[String]): Coder[B] =
+      c match {
+        // Stop the recursion. We already traversed that Coder
+        case ref: Ref[_] if types.contains(ref.typeName) =>
+          Coder[Nothing].asInstanceOf[Coder[B]]
+        case Disjunction(typeName, _, _, _) if types.contains(typeName) =>
+          Coder[Nothing].asInstanceOf[Coder[B]]
+        case Record(typeName, _, _, _) if types.contains(typeName) =>
+          Coder[Nothing].asInstanceOf[Coder[B]]
+        //
+        case ref: Ref[_]       => go(ref.value, types + ref.typeName)
+        case c @ RawBeam(beam) => c
+        case c @ Beam(beam)    => c
+        case c @ Fallback(ct)  => c
+        case Transform(c, f) =>
+          val c2 = f(CoderMaterializer.beamImpl(o, c))
+          go(c2, types)
+        case Disjunction(typeName, idCoder, id, coders) =>
+          val ts = types + typeName
+          Disjunction(
+            typeName,
+            go(idCoder, ts),
+            id,
+            coders.map { case (id, c) => id -> go(c, ts) }
+          )
+        case Record(typeName, cs, construct, destruct) =>
+          val cs2 = cs.map { case (n, c) => (n, go(c, types + typeName)) }
+          Record(typeName, cs2, construct, destruct)
+        case KVCoder(koder, voder) =>
+          KVCoder(go(koder, types), go(voder, types))
+      }
+    go(coder, Set.empty)
+  }
+
+  def verifyDeterministic(): Unit =
+    CoderMaterializer.beamImpl(o, uncycled).verifyDeterministic()
+
+  override def consistentWithEquals(): Boolean =
+    CoderMaterializer.beamImpl(o, uncycled).consistentWithEquals()
+
   override def structuralValue(value: T): AnyRef =
     if (consistentWithEquals()) {
       value.asInstanceOf[AnyRef]
@@ -330,6 +376,7 @@ final private[scio] case class RecordCoder[T](
   }
 
   override def consistentWithEquals(): Boolean = cs.forall(_._2.consistentWithEquals())
+
   override def structuralValue(value: T): AnyRef =
     if (consistentWithEquals()) {
       value.asInstanceOf[AnyRef]

scio-core/src/main/scala/com/spotify/scio/coders/instances/ScalaCoders.scala
@@ -46,6 +46,7 @@ private object NothingCoder extends AtomicCoder[Nothing] {
   override def decode(is: InputStream): Nothing =
     // can't possibly happen
     throw new IllegalStateException("Trying to decode a value of type Nothing is impossible")
+  override def consistentWithEquals(): Boolean = true
 }
 
 abstract private[coders] class BaseSeqLikeCoder[M[_], T](val elemCoder: BCoder[T])

scio-test/src/main/scala/com/spotify/scio/testing/CoderAssertions.scala
@@ -76,6 +76,26 @@ object CoderAssertions {
       }
     }
 
+  def beConsistentWithEquals[T: ClassTag](
+    opts: PipelineOptions = DefaultPipelineOptions
+  ): CoderAssertion[T] =
+    new CoderAssertion[T] {
+      override def assert(value: T)(implicit c: Coder[T], eq: Equality[T]): Assertion = {
+        val beamCoder = CoderMaterializer.beamWithDefault(c, o = opts)
+        beamCoder.consistentWithEquals() shouldBe true
+      }
+    }
+
+  def beDeterministic[T: ClassTag](
+    opts: PipelineOptions = DefaultPipelineOptions
+  ): CoderAssertion[T] =
+    new CoderAssertion[T] {
+      override def assert(value: T)(implicit c: Coder[T], eq: Equality[T]): Assertion = {
+        val beamCoder = CoderMaterializer.beamWithDefault(c, o = opts)
+        noException should be thrownBy beamCoder.verifyDeterministic()
+      }
+    }
+
   def coderIsSerializable[A](implicit c: Coder[A]): Assertion =
     coderIsSerializable(CoderMaterializer.beamWithDefault(c))
 

scio-test/src/test/scala/com/spotify/scio/coders/CoderTest.scala
@@ -549,6 +549,10 @@ final class CoderTest extends AnyFlatSpec with Matchers {
 
     SampleField("hello", StringType) coderShould roundtrip()
 
+    // https://github.com/spotify/scio/issues/3707
+    SampleField("hello", StringType) coderShould beConsistentWithEquals()
+    SampleField("hello", StringType) coderShould beDeterministic()
+
     SampleField(
       "hello",
       RecordType(

commit e5dd0469fe7bb90254a2f9b8ab932ee72289e564

fix coder performance regression in #3716

scio-core/src/main/scala/com/spotify/scio/coders/Coder.scala
@@ -192,7 +192,7 @@ final private[scio] case class LazyCoder[T](
    * and other methods that DO NOT actually serialize / deserialize the data.
    * Internally, looping coders are replaced by an instance of Coder[Nothing]
    */
-  private def uncycled: Coder[T] = {
+  private lazy val uncycled: BCoder[T] = {
     def go[B](c: Coder[B], types: Set[String]): Coder[B] =
       c match {
         // Stop the recursion. We already traversed that Coder
@@ -224,14 +224,14 @@ final private[scio] case class LazyCoder[T](
         case KVCoder(koder, voder) =>
           KVCoder(go(koder, types), go(voder, types))
       }
-    go(coder, Set.empty)
+    CoderMaterializer.beamImpl(o, go(coder, Set.empty))
   }
 
-  def verifyDeterministic(): Unit =
-    CoderMaterializer.beamImpl(o, uncycled).verifyDeterministic()
+  override def verifyDeterministic(): Unit =
+    uncycled.verifyDeterministic()
 
   override def consistentWithEquals(): Boolean =
-    CoderMaterializer.beamImpl(o, uncycled).consistentWithEquals()
+    uncycled.consistentWithEquals()
 
   override def structuralValue(value: T): AnyRef =
     if (consistentWithEquals()) {