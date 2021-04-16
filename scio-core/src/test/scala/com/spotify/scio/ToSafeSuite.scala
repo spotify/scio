bb323dc0f575928b13ea196f8c61c4a987e0124d
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2021-04-16 16:09:56 +0200

    Add simple tests

--- scio-core/src/test/scala/com/spotify/scio/ToSafeSuite.scala
+++ scio-core/src/test/scala/com/spotify/scio/ToSafeSuite.scala
@@ -7,11 +7,28 @@ import org.scalatest.matchers.should.Matchers
 
 case class Source(b: Boolean)
 case class Dest(b: Boolean)
+case class Mistake(b: Int)
+case class Mistake2(c: Boolean)
+
+case class Sources(name: String, links: List[Array[Byte]])
+case class Destinations(name: String, links: List[Array[Byte]])
+case class DestinationsWrong(name: String, links: List[Array[Int]])
 
 class ToSafeTest extends AnyFlatSpec with Matchers {
-  To.safe[Source, Dest].convert(Source(true))
+  "To.safe" should "generate a conversion on compatible flat case class schemas" in {
+    To.safe[Source, Dest]
+  }
 
-  // "To.safe" should "generate an unchecked conversion on compatible case class schemas" in {
-  //   To.safe[Source, Dest].convert(Source(true)) shouldBe Dest(true)
-  // }
+  "To.safe" should "fail on incompatible flat case class schemas" in {
+    "To.safe[Source, Mistake2]" shouldNot compile
+    "To.safe[Source, Mistake]" shouldNot compile
+  }
+
+  "To.safe" should "generate a conversion on compatible nested case class schemas" in {
+    To.safe[Sources, Destinations]
+  }
+
+  "To.safe" should "fail on incompatible nested case class schemas" in {
+    "To.safe[Sources, DestinationsWrong]" shouldNot compile
+  }
 }
