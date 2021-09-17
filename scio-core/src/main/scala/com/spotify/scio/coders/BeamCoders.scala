fffdc9cfcc4f708e3278a5fa3b8ad6ba825021b1
stormy-ua <kirill.panarin@gmail.com>
2021-09-17 12:25:38 -0400

    Rollback coder unwrapping in BeamCoders

--- scio-core/src/main/scala/com/spotify/scio/coders/BeamCoders.scala
+++ scio-core/src/main/scala/com/spotify/scio/coders/BeamCoders.scala
@@ -28,6 +28,7 @@ private[scio] object BeamCoders {
   @tailrec
   private def unwrap[T](coder: beam.Coder[T]): beam.Coder[T] =
     coder match {
+      case WrappedBCoder(c)         => unwrap(c)
       case c: LazyCoder[T]          => unwrap(c.bcoder)
       case c: beam.NullableCoder[T] => c.getValueCoder
       case _                        => coder
