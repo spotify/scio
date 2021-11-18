1c55b0c05f14f0817a88ec276dff854dcd7dca4d
Claire McGinty <clairem@spotify.com>
2021-11-18 15:05:08 -0500

    search for ADC if environment var isn't set in GcsConnectorUtil

--- scio-parquet/src/main/scala/com/spotify/scio/parquet/GcsConnectorUtil.scala
+++ scio-parquet/src/main/scala/com/spotify/scio/parquet/GcsConnectorUtil.scala
@@ -17,20 +17,39 @@
 
 package com.spotify.scio.parquet
 
+import com.google.auth.oauth2.{GoogleCredentials, ServiceAccountCredentials}
 import com.spotify.scio.ScioContext
 import com.spotify.scio.util.ScioUtil
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.slf4j.LoggerFactory
+
+import java.io.File
+import java.util.Locale
+import scala.util.Try
 
 private[parquet] object GcsConnectorUtil {
-  def setCredentials(job: Job): Unit =
+  private lazy val log = LoggerFactory.getLogger(getClass)
+
+  def setCredentials(job: Job): Unit = {
     // These are needed since `FileInputFormat.setInputPaths` validates paths locally and
     // requires the user's GCP credentials.
-    sys.env.get("GOOGLE_APPLICATION_CREDENTIALS") match {
-      case Some(json) =>
-        job.getConfiguration
-          .set("fs.gs.auth.service.account.json.keyfile", json)
+    val keyFile = sys.env
+      .get("GOOGLE_APPLICATION_CREDENTIALS")
+      .filter(_.nonEmpty)
+      .orElse {
+        Try(GoogleCredentials.getApplicationDefault()).toOption match {
+          case Some(_: ServiceAccountCredentials) => getWellKnownCredentialFile.map(_.toString)
+          case _ => None // ADC was not set, or was a personal user credential
+        }
+      }
+
+    keyFile match {
+      case Some(path) => job.getConfiguration.set("fs.gs.auth.service.account.json.keyfile", path)
       case None =>
+        log.warn(
+          "Application default credentials could not be resolved, using default Cloud SDK credentials"
+        )
         // Client id/secret of Google-managed project associated with the Cloud SDK
         job.getConfiguration
           .setBoolean("fs.gs.auth.service.account.enable", false)
@@ -38,11 +57,13 @@ private[parquet] object GcsConnectorUtil {
         job.getConfiguration
           .set("fs.gs.auth.client.secret", "ZmssLNjJy2998hD4CTg2ejr2")
     }
+  }
 
   def unsetCredentials(job: Job): Unit = {
     job.getConfiguration.unset("fs.gs.auth.service.account.json.keyfile")
     job.getConfiguration.unset("fs.gs.auth.service.account.enable")
-    job.getConfiguration.unset("fs.gs.auth.client.id")
+    job.getConfiguration.unset("fs.gs.auth.service.account.enable")
+    job.getConfiguration.unset("fs.gs.auth.null.enable")
     job.getConfiguration.unset("fs.gs.auth.client.secret")
   }
 
@@ -57,5 +78,19 @@ private[parquet] object GcsConnectorUtil {
     if (!ScioUtil.isLocalRunner(sc.options.getRunner)) {
       GcsConnectorUtil.unsetCredentials(job)
     }
+  }
+
+  // Adapted from com.google.auth.oauth2.DefaultCredentialsProvider
+  private def getWellKnownCredentialFile: Option[File] = {
+    val os = sys.props.getOrElse("os.name", "").toLowerCase(Locale.US)
+    val cloudRootPath = if (os.contains("windows")) {
+      new File(sys.env("APPDATA"))
+    } else {
+      new File(sys.props.getOrElse("user.home", ""), ".config")
+    }
+    val credentialFilePath =
+      new File(cloudRootPath, "gcloud/application_default_credentials.json")
+
+    if (credentialFilePath.exists()) Some(credentialFilePath) else None
   }
 }
