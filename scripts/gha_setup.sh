68b24d8d9222474e4cd770a3562d65a46a849e8e
Claire McGinty <clairem@spotify.com>
2022-01-07 10:25:18 -0500

    update other workflows to use gloud-auth

--- scripts/gha_setup.sh
+++ scripts/gha_setup.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Decrypt google credentials
-openssl aes-256-cbc -d -salt -pbkdf2 \
-  -in "$GOOGLE_APPLICATION_CREDENTIALS.enc" \
-  -out "$GOOGLE_APPLICATION_CREDENTIALS" \
-  -k "$ENCRYPTION_KEY"
-
 # Configure SBT options
 cat <<EOF >>.jvmopts
 -Dbigquery.project=$GOOGLE_PROJECT_ID
