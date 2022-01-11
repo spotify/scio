8660ede18a07667fd129ae0ad8dd3a4ab653c2f2
Kellen Dye <kellend@spotify.com>
2022-01-11 13:49:21 -0500

    PR comments

--- scio-google-cloud-platform/src/main/scala/com/spotify/scio/bigquery/avro/AvroConverters.scala
+++ scio-google-cloud-platform/src/main/scala/com/spotify/scio/bigquery/avro/AvroConverters.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.spotify.scio.bigquery.avro
 
 import com.google.api.services.bigquery.model.TableSchema
@@ -9,6 +26,11 @@ import org.apache.avro.generic.IndexedRecord
 import scala.jdk.CollectionConverters._
 
 object AvroConverters extends ToTableRow with ToTableSchema {
+  /**
+   * Converts `record` into a TableRow by traversing the schema and converting each field
+   * @param record
+   * @return record converted into a BigQuery TableRow
+   */
   @experimental
   def toTableRow[T <: IndexedRecord](record: T): TableRow = {
     val row = new TableRow
