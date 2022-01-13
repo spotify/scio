1b50463863bff5b31724ed9aee09f24fc630c127
stormy-ua <kirill.panarin@gmail.com>
2022-01-13 11:42:01 -0500

    fix test

--- scio-test/src/test/scala/com/spotify/scio/coders/CoderTest.scala
+++ scio-test/src/test/scala/com/spotify/scio/coders/CoderTest.scala
@@ -219,7 +219,7 @@ final class CoderTest extends AnyFlatSpec with Matchers {
   object Avro {
     import com.spotify.scio.avro.{Account, Address, User => AvUser}
 
-    val accounts: List[Account] = List(new Account(1, "type", "name", 12.5, AccountStatus.Active))
+    val accounts: List[Account] = List(new Account(1, "type", "name", 12.5, null))
     val address =
       new Address("street1", "street2", "city", "state", "01234", "Sweden")
     val user = new AvUser(1, "lastname", "firstname", "email@foobar.com", accounts.asJava, address)
