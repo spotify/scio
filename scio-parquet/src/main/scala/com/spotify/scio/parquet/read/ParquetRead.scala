dee52a91b7e981bf810501c7929e8e4cf1942c02
Claire McGinty <clairem@spotify.com>
2022-06-29 11:59:01 -0700

    Parquet read: replace HadoopFormatIO with Splittable DoFn IO to improve read performance (#4260) (#4436)
    * Parquet read: replace HadoopFormatIO with Splittable DoFn to improve read performance
    
    * fix scala 2.12 compilation
    
    * fix scala 2.13 compilation
    
    * remove unused dep
    
    * minor changes
    
    * review feedback
    
    * type description for immutable set
    
    * Apply suggestions from code review
    
    * update parquet.tensorflow package
    
    * Fold projection/predicate
    
    Co-authored-by: Claire McGinty <clairem@spotify.com>
    
    Co-authored-by: Kirill Panarin <kirill.panarin@gmail.com>

---
+++ scio-parquet/src/main/scala/com/spotify/scio/parquet/read/ParquetRead.scala
@@ -0,0 +1,38 @@
+package com.spotify.scio.parquet.read
+
+import org.apache.beam.sdk.io.FileIO
+import org.apache.beam.sdk.io.FileIO.ReadableFile
+import org.apache.beam.sdk.io.hadoop.SerializableConfiguration
+import org.apache.beam.sdk.transforms.{PTransform, ParDo, SerializableFunction}
+import org.apache.beam.sdk.values.{PBegin, PCollection}
+
+object ParquetRead {
+
+  def read[T, R](
+    readSupportFactory: ReadSupportFactory[T],
+    conf: SerializableConfiguration,
+    filePattern: String,
+    projectionFn: SerializableFunction[T, R]
+  ): PTransform[PBegin, PCollection[R]] =
+    new PTransform[PBegin, PCollection[R]] {
+      override def expand(input: PBegin): PCollection[R] = {
+        input
+          .apply(FileIO.`match`().filepattern(filePattern))
+          .apply(FileIO.readMatches)
+          .apply(readFiles(readSupportFactory, conf, projectionFn))
+      }
+    }
+
+  def readFiles[T, R](
+    readSupportFactory: ReadSupportFactory[T],
+    conf: SerializableConfiguration,
+    projectionFn: SerializableFunction[T, R]
+  ): PTransform[PCollection[ReadableFile], PCollection[R]] =
+    new PTransform[PCollection[ReadableFile], PCollection[R]] {
+      override def expand(input: PCollection[ReadableFile]): PCollection[R] = {
+        val sdf = new ParquetReadFn[T, R](readSupportFactory, conf, projectionFn)
+        input.apply(ParDo.of(sdf))
+      }
+    }
+
+}
