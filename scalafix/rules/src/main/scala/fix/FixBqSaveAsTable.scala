71c1ea877d206aa3c041943f4be79f79eb79e28a
Scala Steward <scala_steward@virtuslab.com>
2022-10-25 22:25:00 +0000

    Reformat with scalafmt 3.6.0
    Executed command: scalafmt --non-interactive

--- scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
+++ scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
@@ -8,31 +8,32 @@ class FixBqSaveAsTable extends SemanticRule("FixBqSaveAsTable") {
   private val scoll = "com/spotify/scio/values/SCollection#"
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    doc.tree.collect { case a @ Term.Apply(fun, head :: tail) =>
-      fun match {
-        case Term.Select(qual, name) =>
-          name match {
-            case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll)=>
-              // the rest of args should be named because the parameter order has changed
-              if (
-                tail.exists(!_.toString.contains("=")) ||
-                // `avroSchema` doesn't exist in the list of the new method
-                tail.exists(_.toString.contains("avroSchema"))
-              ) {
-                Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
-              } else {
-                val headParam = if (head.toString.contains("=")) {
-                  s"table = Table.Ref(${head.toString.split("=").last.trim})"
+    doc.tree.collect {
+      case a @ Term.Apply(fun, head :: tail) =>
+        fun match {
+          case Term.Select(qual, name) =>
+            name match {
+              case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
+                // the rest of args should be named because the parameter order has changed
+                if (
+                  tail.exists(!_.toString.contains("=")) ||
+                  // `avroSchema` doesn't exist in the list of the new method
+                  tail.exists(_.toString.contains("avroSchema"))
+                ) {
+                  Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
                 } else {
-                  s"Table.Ref($head)"
+                  val headParam = if (head.toString.contains("=")) {
+                    s"table = Table.Ref(${head.toString.split("=").last.trim})"
+                  } else {
+                    s"Table.Ref($head)"
+                  }
+                  val allArgs = (headParam :: tail).mkString(", ")
+                  Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
                 }
-                val allArgs = (headParam :: tail).mkString(", ")
-                Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
-              }
-            case _ =>
-              Patch.empty
-          }
-      }
+              case _ =>
+                Patch.empty
+            }
+        }
       case Importer(q"com.spotify.scio.extra.bigquery", imps) =>
         Patch.removeImportee(imps.head) +
           Patch.addGlobalImport(importer"com.spotify.scio.bigquery._")
