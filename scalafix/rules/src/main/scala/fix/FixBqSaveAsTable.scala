9ad30557727391f02bfd03345f5cc0fcab6de57d
farzad-sedghi <109164663+farzad-sedghi@users.noreply.github.com>
2022-11-09 14:45:53 -0500

    Scalafix 0.12.0 bug fixes (#4581)
    * capture the save method more precisely
    
    * remove test method

--- scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
+++ scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
@@ -6,36 +6,45 @@ import scala.meta._
 
 class FixBqSaveAsTable extends SemanticRule("FixBqSaveAsTable") {
   private val scoll = "com/spotify/scio/values/SCollection#"
+  private val methodName = "com.spotify.scio.extra.bigquery.syntax.AvroToBigQuerySCollectionOps.saveAvroAsBigQuery"
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    doc.tree.collect { case a @ Term.Apply(fun, head :: tail) =>
-      fun match {
-        case Term.Select(qual, name) =>
-          name match {
-            case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll)=>
-              // the rest of args should be named because the parameter order has changed
-              if (
-                tail.exists(!_.toString.contains("=")) ||
-                // `avroSchema` doesn't exist in the list of the new method
-                tail.exists(_.toString.contains("avroSchema"))
-              ) {
-                Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
-              } else {
-                val headParam = if (head.toString.contains("=")) {
-                  s"table = Table.Ref(${head.toString.split("=").last.trim})"
-                } else {
-                  s"Table.Ref($head)"
+    doc.tree.collect {
+      case a @ Term.Apply(fun, head :: tail) =>
+        if (fun.symbol.normalized.toString.contains(methodName)) {
+          fun match {
+            case Term.Select(qual, name) =>
+              name match {
+                case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
+                  // the rest of args should be named because the parameter order has changed
+                  if (
+                    tail.exists(!_.toString.contains("=")) ||
+                      // `avroSchema` doesn't exist in the list of the new method
+                      tail.exists(_.toString.contains("avroSchema"))
+                  ) {
+                    Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
+                  } else {
+                    val headParam = if (head.toString.contains("=")) {
+                      s"table = Table.Ref(${head.toString.split("=").last.trim})"
+                    } else {
+                      s"Table.Ref($head)"
+                    }
+                  val allArgs = (headParam :: tail).mkString(", ")
+                  Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
                 }
-                val allArgs = (headParam :: tail).mkString(", ")
-                Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
-              }
+              case _ =>
+                Patch.empty
+            }
             case _ =>
               Patch.empty
           }
-      }
+        } else {
+          Patch.empty
+        }
       case Importer(q"com.spotify.scio.extra.bigquery", imps) =>
-        Patch.removeImportee(imps.head) +
-          Patch.addGlobalImport(importer"com.spotify.scio.bigquery._")
+          Patch.removeImportee(imps.head) +
+            Patch.addGlobalImport(importer"com.spotify.scio.bigquery._")
+      case _ => Patch.empty
     }.asPatch
   }
 
