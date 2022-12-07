e5c9c474212ccb93a14da5cbc21bf50dd71ac49d
Andrew Kabas <akabas@spotify.com>
2022-12-07 15:20:02 -0500

    Update scio-smb/src/main/java/org/apache/beam/sdk/extensions/sorter/ExternalSorter.java
    Co-authored-by: Claire McGinty <clairem@spotify.com>

--- scio-smb/src/main/java/org/apache/beam/sdk/extensions/sorter/ExternalSorter.java
+++ scio-smb/src/main/java/org/apache/beam/sdk/extensions/sorter/ExternalSorter.java
@@ -93,7 +93,7 @@ public abstract class ExternalSorter implements Sorter {
   /** Returns a {@link Sorter} configured with the given {@link Options}. */
   public static ExternalSorter create(Options options) {
     checkArgument(
-        options.getSorterType() == Options.SorterType.NATIVE, "Only Native sorter is supported");
+        options.getSorterType() == Options.SorterType.NATIVE, "NativeExternalSorter is the only supported external sorter");
     return NativeExternalSorter.create(options);
   }
 
