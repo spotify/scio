931d78c0c3f9435460fffb2f54ac3a46a8ef88ad
farzad-sedghi <109164663+farzad-sedghi@users.noreply.github.com>
2023-01-24 14:33:04 -0500

    fix bq lib import for scalafix (#4666)

--- scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
+++ scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
@@ -10,44 +10,55 @@ class FixBqSaveAsTable extends SemanticRule("FixBqSaveAsTable") {
     "com.spotify.scio.extra.bigquery.syntax.AvroToBigQuerySCollectionOps.saveAvroAsBigQuery"
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    doc.tree.collect {
-      case a @ Term.Apply(fun, head :: tail) =>
-        if (fun.symbol.normalized.toString.contains(methodName)) {
-          fun match {
-            case Term.Select(qual, name) =>
-              name match {
-                case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
-                  // the rest of args should be named because the parameter order has changed
-                  if (
-                    tail.exists(!_.toString.contains("=")) ||
-                    // `avroSchema` doesn't exist in the list of the new method
-                    tail.exists(_.toString.contains("avroSchema"))
-                  ) {
-                    Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
-                  } else {
-                    val headParam = if (head.toString.contains("=")) {
-                      s"table = Table.Ref(${head.toString.split("=").last.trim})"
+    val patches =
+      doc.tree.collect {
+        case a @ Term.Apply(fun, head :: tail) =>
+          if (fun.symbol.normalized.toString.contains(methodName)) {
+            fun match {
+              case Term.Select(qual, name) =>
+                name match {
+                  case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
+                    // the rest of args should be named because the parameter order has changed
+                    if (
+                      tail.exists(!_.toString.contains("=")) ||
+                      // `avroSchema` doesn't exist in the list of the new method
+                      tail.exists(_.toString.contains("avroSchema"))
+                    ) {
+                      Patch.empty // not possible to fix, leave it to `LintBqSaveAsTable`
                     } else {
-                      s"Table.Ref($head)"
+                      val headParam = if (head.toString.contains("=")) {
+                        s"table = Table.Ref(${head.toString.split("=").last.trim})"
+                      } else {
+                        s"Table.Ref($head)"
+                      }
+                      val allArgs = (headParam :: tail).mkString(", ")
+                      Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
                     }
-                    val allArgs = (headParam :: tail).mkString(", ")
-                    Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($allArgs)")
-                  }
-                case _ =>
-                  Patch.empty
-              }
-            case _ =>
-              Patch.empty
+                  case _ =>
+                    Patch.empty
+                }
+              case _ =>
+                Patch.empty
+            }
+          } else {
+            Patch.empty
           }
-        } else {
-          Patch.empty
-        }
-      case Importer(q"com.spotify.scio.extra.bigquery", imps) =>
-        Patch.removeImportee(imps.head) +
-          Patch.addGlobalImport(importer"com.spotify.scio.bigquery._")
-      case _ => Patch.empty
-    }.asPatch
-  }
+        case Source(Pkg(_, imp :: _) :: _) =>
+          Patch.addGlobalImport(
+            importer"com.spotify.scio.bigquery._"
+          )
+        case _ => Patch.empty
+      }
+        .filter(_ != Patch.empty)
+
+      // in case the import is the only change, drop the entire patch
+      if (patches.size == 1) {
+        List()
+      } else {
+        patches
+      }
+
+  }.asPatch
 
   private def expectedType(qual: Term, typStr: String)(implicit doc: SemanticDocument): Boolean =
     qual.symbol.info.get.signature match {
