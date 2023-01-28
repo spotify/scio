3f20f5fe0680d719fa04479681d927f094d22013
Scala Steward <scala_steward@virtuslab.com>
2023-01-28 01:04:17 +0000

    Reformat with scalafmt 3.7.1
    Executed command: scalafmt --non-interactive

--- scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
+++ scalafix/rules/src/main/scala/fix/FixBqSaveAsTable.scala
@@ -11,68 +11,69 @@ class FixBqSaveAsTable extends SemanticRule("FixBqSaveAsTable") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     val patches =
-      doc.tree.collect {
-        case a @ Term.Apply(fun, params) =>
-          if (fun.symbol.normalized.toString.contains(methodName)) {
-            fun match {
-              case Term.Select(qual, name) =>
-                name match {
-                  case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
-                    val paramsUpdated =
-                      params
-                        .zipWithIndex
-                        .map { case (param, index) =>
-                          index match {
-                            // table is always the first param and without default value
-                            case 0 =>
-                              param match {
-                                case Term.Assign((_, value)) =>
-                                  q"table = Table.Ref(${value})"
-                                case _ =>
-                                  q"Table.Ref($param)"
-                              }
-                            case _ =>
-                              param match {
-                                case Term.Assign((name, value)) =>
-                                  // parameter name has changes from `avroSchema` to `schema`
-                                  if (name.toString == "avroSchema") {
-                                    q"schema = toTableSchema($value)"
-                                  } else {
-                                    param
-                                  }
-                                case _ =>
-                                  // if not a named param, `avroSchema` param should come second
-                                  if (index == 1) {
-                                    q"toTableSchema($param)"
-                                  } else {
-                                    param
-                                  }
-                              }
+      doc.tree
+        .collect {
+          case a @ Term.Apply(fun, params) =>
+            if (fun.symbol.normalized.toString.contains(methodName)) {
+              fun match {
+                case Term.Select(qual, name) =>
+                  name match {
+                    case Term.Name("saveAvroAsBigQuery") if expectedType(qual, scoll) =>
+                      val paramsUpdated =
+                        params.zipWithIndex
+                          .map { case (param, index) =>
+                            index match {
+                              // table is always the first param and without default value
+                              case 0 =>
+                                param match {
+                                  case Term.Assign((_, value)) =>
+                                    q"table = Table.Ref(${value})"
+                                  case _ =>
+                                    q"Table.Ref($param)"
+                                }
+                              case _ =>
+                                param match {
+                                  case Term.Assign((name, value)) =>
+                                    // parameter name has changes from `avroSchema` to `schema`
+                                    if (name.toString == "avroSchema") {
+                                      q"schema = toTableSchema($value)"
+                                    } else {
+                                      param
+                                    }
+                                  case _ =>
+                                    // if not a named param, `avroSchema` param should come second
+                                    if (index == 1) {
+                                      q"toTableSchema($param)"
+                                    } else {
+                                      param
+                                    }
+                                }
+                            }
                           }
-                        }
-                        .mkString(", ")
+                          .mkString(", ")
 
-                    Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($paramsUpdated)")
+                      Patch.replaceTree(a, s"$qual.saveAsBigQueryTable($paramsUpdated)")
 
-                  case _ =>
-                    Patch.empty
-                }
-              case _ =>
-                Patch.empty
+                    case _ =>
+                      Patch.empty
+                  }
+                case _ =>
+                  Patch.empty
+              }
+            } else {
+              Patch.empty
             }
-          } else {
-            Patch.empty
-          }
-        case _ => Patch.empty
-      }.filter(_ != Patch.empty)
+          case _ => Patch.empty
+        }
+        .filter(_ != Patch.empty)
 
     // in case the import is the only change, drop the entire patch
     if (!patches.isEmpty) {
       patches ++ List(
         Patch.addGlobalImport(importer"com.spotify.scio.bigquery._"),
-          Patch.addGlobalImport(
-            importer"com.spotify.scio.extra.bigquery.AvroConverters.toTableSchema"
-          )
+        Patch.addGlobalImport(
+          importer"com.spotify.scio.extra.bigquery.AvroConverters.toTableSchema"
+        )
       )
     } else {
       patches
