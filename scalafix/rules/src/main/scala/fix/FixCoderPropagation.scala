c01b793f975d9f9042efe15e62b264304b5a840b
Michel Davit <micheld@spotify.com>
2023-01-31 16:21:03 +0100

    Match on function symbol

--- scalafix/rules/src/main/scala/fix/FixCoderPropagation.scala
+++ scalafix/rules/src/main/scala/fix/FixCoderPropagation.scala
@@ -5,47 +5,22 @@ import scalafix.v1._
 import scala.meta._
 
 object FixCoderPropagation {
-  val Scoll: SymbolMatcher =
-    SymbolMatcher.normalized("com/spotify/scio/values/SCollection")
-  val PairedScol: SymbolMatcher =
-    SymbolMatcher.normalized("com/spotify/scio/values/PairSCollectionFunctions")
+  val CurriedSCollFns = Seq("top", "quantilesApprox")
+    .map(fn => SymbolMatcher.normalized(s"com/spotify/scio/values/SCollection#$fn"))
 
-  val CurriedSCollFns = Set(
-    "top",
-    "quantilesApprox"
-  )
+  val CurriedPairedSCollFns = Set("topByKey", "approxQuantilesByKey")
+    .map(fn => SymbolMatcher.normalized(s"com/spotify/scio/values/PairSCollectionFunctions#$fn"))
 
-  val CurriedPairedScolFns = Set(
-    "topByKey",
-    "approxQuantilesByKey"
-  )
+  val CurriedFns = (CurriedSCollFns ++ CurriedPairedSCollFns).reduce(_ + _)
 }
 
 class FixCoderPropagation extends SemanticRule("FixCoderPropagation") {
 
   import FixCoderPropagation._
 
-  private def expectedType(
-    expected: SymbolMatcher
-  )(qual: Term)(implicit doc: SemanticDocument): Boolean =
-    qual.symbol.info.get.signature match {
-      case MethodSignature(_, _, TypeRef(_, typ, _)) =>
-        expected.matches(typ)
-      case ValueSignature(AnnotatedType(_, TypeRef(_, typ, _))) =>
-        expected.matches(typ)
-      case ValueSignature(TypeRef(_, typ, _)) =>
-        Scoll.matches(typ)
-      case _ =>
-        false
-    }
-
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
-      case t @ q"$qual.$fn($arg1, $arg2)"
-          if expectedType(Scoll)(qual) && CurriedSCollFns.contains(fn.value) =>
-        Patch.replaceTree(t, q"$qual.$fn($arg1)($arg2)".syntax)
-      case t @ q"$qual.$fn($arg1, $arg2)"
-          if expectedType(PairedScol)(qual) && CurriedPairedScolFns.contains(fn.value) =>
+      case t @ q"$qual.$fn($arg1, $arg2)" if CurriedFns.matches(fn.symbol) =>
         Patch.replaceTree(t, q"$qual.$fn($arg1)($arg2)".syntax)
     }.asPatch
   }
