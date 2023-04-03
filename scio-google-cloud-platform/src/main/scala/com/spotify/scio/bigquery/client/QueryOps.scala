23688679c00dd46c65d5c5c820cfe0e597646927
Fallon Chen <fallon@spotify.com>
2023-04-03 15:48:07 -0400

    Move isDML into BigQuery object

--- scio-google-cloud-platform/src/main/scala/com/spotify/scio/bigquery/client/QueryOps.scala
+++ scio-google-cloud-platform/src/main/scala/com/spotify/scio/bigquery/client/QueryOps.scala
@@ -20,7 +20,7 @@ package com.spotify.scio.bigquery.client
 import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.services.bigquery.model._
 import com.spotify.scio.bigquery.{Table => STable}
-import com.spotify.scio.bigquery.client.BigQuery.Client
+import com.spotify.scio.bigquery.client.BigQuery.{isDML, Client}
 import com.spotify.scio.bigquery.{BigQueryUtil, TableRow}
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.TypedRead.QueryPriority
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.{CreateDisposition, WriteDisposition}
@@ -38,9 +38,6 @@ private[client] object QueryOps {
   private def isInteractive: Boolean = BigQueryConfig.priority.equals(QueryPriority.INTERACTIVE)
 
   private val Priority = if (isInteractive) "INTERACTIVE" else "BATCH"
-
-  private[scio] def isDML(sqlQuery: String): Boolean =
-    sqlQuery.toUpperCase().matches("(UPDATE|MERGE|INSERT|DELETE).*")
 
   final private[scio] case class QueryJobConfig(
     sql: String,
