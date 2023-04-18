d9ec1df3f0dc26dbf34ba8b67dc2823cee370033
Andrew Kabas <akabas@spotify.com>
2023-04-18 14:34:05 -0400

    Fix formatting

--- scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
+++ scio-core/src/main/scala/com/spotify/scio/values/SCollection.scala
@@ -902,9 +902,9 @@ sealed trait SCollection[T] extends PCollectionWrapper[T] {
     this.pApply(Combine.globally(Functions.reduceFn(context, op)).withoutDefaults())
 
   /**
-   * Return a sampled subset of this SCollection containing exactly `sampleSize` items. Involves combine
-   * operation resulting in shuffling. All the elements of the output should fit into main memory of
-   * a single worker machine.
+   * Return a sampled subset of this SCollection containing exactly `sampleSize` items. Involves
+   * combine operation resulting in shuffling. All the elements of the output should fit into main
+   * memory of a single worker machine.
    *
    * @return
    *   a new SCollection whose single value is an `Iterable` of the samples
