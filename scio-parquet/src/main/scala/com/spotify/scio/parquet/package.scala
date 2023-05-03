5de2e2a5f5e11401c0e6ad6ba72dc1074f369585
Claire McGinty <clairem@spotify.com>
2023-05-03 13:22:06 -0400

    Add opt-in Logical type support to 0.11.x branch (#4792)
    * Support logical types in SpecificRecord Parquet writes (#4772)
    
    * Reproduce SpecificRecords/logicalType bug
    
    * Fix bug
    
    * Cleanup test
    
    * logical types must be added from worker VM, not launcher
    
    * Fix for both reads and writes
    
    * cleanup
    
    * Don't set supplier if user has explicitly configured one
    
    * Test for custom logical type provider
    
    * Pass dataModel to AvroParquetWriter.Builder
    
    * Only override .withDataModel if Configuration key exists
    
    * Isolate SpecificData instances
    
    * fix test
    
    * update copyright year
    
    * cleanup
    
    * Add support for scio-smb Parquet writes
    
    * Manage config defaults via core-site.xml
    
    * Fix formatting
    
    * Should be a standalone class
    
    * add test
    
    * +header
    
    * newline
    
    * Add remaining Converters
    
    * simplify SpecificData creation
    
    * Correctly encode logical types in GenericRecord
    
    * remove unused import
    
    * Make Parquet logical type support opt-in (#4782)
    
    * Make logical type support for Parquet-Avro opt-in
    
    * Update documentation
    
    * conf->jobConf
    
    * scalafmt
    
    * Update scio-parquet/src/main/scala/com/spotify/scio/parquet/avro/ParquetAvroIO.scala
    
    Co-authored-by: Michel Davit <micheld@spotify.com>
    
    * Add parens
    
    ---------
    
    Co-authored-by: Michel Davit <micheld@spotify.com>
    
    ---------
    
    Co-authored-by: Michel Davit <micheld@spotify.com>

---
+++ scio-parquet/src/main/scala/com/spotify/scio/parquet/package.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spotify.scio
+
+import org.apache.hadoop.conf.Configuration
+
+package object parquet {
+  object ParquetConfiguration {
+    def of(entries: (String, Any)*): Configuration = {
+      val conf = new Configuration()
+      entries.foreach { case (k, v) =>
+        v match {
+          case b: Boolean  => conf.setBoolean(k, b)
+          case f: Float    => conf.setFloat(k, f)
+          case d: Double   => conf.setDouble(k, d)
+          case i: Int      => conf.setInt(k, i)
+          case l: Long     => conf.setLong(k, l)
+          case s: String   => conf.set(k, s)
+          case c: Class[_] => conf.setClass(k, c, c)
+          case _           => conf.set(k, v.toString)
+        }
+      }
+      conf
+    }
+  }
+}
