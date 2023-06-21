20e18b587d6c73aaff4ed47f18ed1c9454432595
Kellen Dye <kellend@spotify.com>
2023-06-21 04:15:09 -0400

    Add scalafix

---
+++ scalafix/input-0_13/src/main/scala/fix/v0_13_0/FixTfParameter.scala
@@ -0,0 +1,25 @@
+/*
+rule = FixTfParameter
+ */
+package fix.v0_13_0
+
+import com.spotify.scio.values.SCollection
+import com.spotify.scio.tensorflow._
+import com.spotify.zoltar.tf.TensorFlowModel
+import org.tensorflow._
+
+object FixTfParameter {
+  case class A()
+  case class B()
+  case class C()
+
+  def toTensors(a: A): Map[String, Tensor] = ???
+  def fromTensors(a: A, tensors: Map[String, Tensor]): B = ???
+
+  val elements: SCollection[A] = ???
+  val options: TensorFlowModel.Options = ???
+  val fetchOpts: Seq[String] = ???
+
+  val result: SCollection[B] = elements.predict[B, C]("gs://model-path", fetchOpts, options)(toTensors)(fromTensors)
+  val b: SCollection[B] = elements.predictWithSigDef[B, C]("gs://model-path", options)(toTensors)(fromTensors _)
+}
