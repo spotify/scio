6238f90241bded68e797a5238a5f5164c2e9d466
Kellen Dye <kellend@spotify.com>
2023-06-28 14:02:40 -0400

    Add scalafix rule for taps

--- scalafix/rules/src/main/scala/fix/v0_13_0/FixSkewedJoins.scala
+++ scalafix/rules/src/main/scala/fix/v0_13_0/FixSkewedJoins.scala
@@ -7,12 +7,13 @@ import scala.meta.contrib._
 
 object FixSkewedJoins {
 
-  private val PairSkewedSCollectionFunctions = "com/spotify/scio/values/PairSkewedSCollectionFunctions"
+  private val PairSkewedSCollectionFunctions =
+    "com/spotify/scio/values/PairSkewedSCollectionFunctions"
 
   val SkewedJoins: SymbolMatcher =
     SymbolMatcher.normalized(PairSkewedSCollectionFunctions + "#skewedJoin") +
-    SymbolMatcher.normalized(PairSkewedSCollectionFunctions + "#skewedLeftOuterJoin") +
-    SymbolMatcher.normalized(PairSkewedSCollectionFunctions + "#skewedFullOuterJoin")
+      SymbolMatcher.normalized(PairSkewedSCollectionFunctions + "#skewedLeftOuterJoin") +
+      SymbolMatcher.normalized(PairSkewedSCollectionFunctions + "#skewedFullOuterJoin")
 
   val HotKeyMethodImport = importer"com.spotify.scio.values.HotKeyMethod"
 
@@ -44,11 +45,11 @@ class FixSkewedJoins extends SemanticRule("FixSkewedJoins") {
   private def isOldSkewedJoinApi(fn: Term)(implicit doc: SemanticDocument): Boolean = {
     val symbol = fn.symbol
     SkewedJoins.matches(symbol) && (symbol.info.get.signature match {
-      case MethodSignature(_, parameterLists, _) => parameterLists.flatten.map(_.symbol.displayName) == OldParameters
+      case MethodSignature(_, parameterLists, _) =>
+        parameterLists.flatten.map(_.symbol.displayName) == OldParameters
       case _ => false
     })
   }
-
 
   private def findParam(param: Term.Name, pos: Int)(args: List[Term]): Option[Term] = {
     args
@@ -64,14 +65,19 @@ class FixSkewedJoins extends SemanticRule("FixSkewedJoins") {
     doc.tree.collect {
       case t @ q"$fn(..$params)" if isOldSkewedJoinApi(fn) =>
         val rhs = findParam(ParamRhs, 0)(params).map(p => q"rhs = $p")
-        val hotKeyMethod = findParam(ParamHotKeyThreshold, 1)(params).map(p => q"hotKeyMethod = HotKeyMethod.Threshold($p)")
+        val hotKeyMethod = findParam(ParamHotKeyThreshold, 1)(params).map(p =>
+          q"hotKeyMethod = HotKeyMethod.Threshold($p)"
+        )
         val cmsEps = findParam(ParamEps, 2)(params).map(p => q"cmsEps = $p")
         val cmsDelta = findParam(ParamDelta, 4)(params).map(p => q"cmsDelta = $p")
         val cmsSeed = findParam(ParamSeed, 3)(params).map(p => q"cmsSeed = $p")
-        val sampleFraction = findParam(ParamSampleFraction, 5)(params).map(p => q"sampleFraction = $p")
-        val sampleWithReplacement = findParam(ParamWithReplacement, 6)(params).map(p => q"sampleWithReplacement = $p")
+        val sampleFraction =
+          findParam(ParamSampleFraction, 5)(params).map(p => q"sampleFraction = $p")
+        val sampleWithReplacement =
+          findParam(ParamWithReplacement, 6)(params).map(p => q"sampleWithReplacement = $p")
 
-        val updated = (rhs ++ hotKeyMethod ++ cmsEps ++ cmsDelta ++ cmsSeed ++ sampleFraction ++ sampleWithReplacement).toList
+        val updated =
+          (rhs ++ hotKeyMethod ++ cmsEps ++ cmsDelta ++ cmsSeed ++ sampleFraction ++ sampleWithReplacement).toList
         Patch.addGlobalImport(HotKeyMethodImport) + Patch.replaceTree(t, q"$fn(..$updated)".syntax)
     }.asPatch
   }
