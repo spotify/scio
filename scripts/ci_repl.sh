acd19804c2b36f81b9b97ef0911a49a13d52a704
Michel Davit <micheld@spotify.com>
2023-08-30 15:29:02 +0200

    Fix ci-repl test

--- scripts/ci_repl.sh
+++ scripts/ci_repl.sh
@@ -4,11 +4,11 @@ set -e
 
 echo "Running REPL integration tests for Scala $SCALA_VERSION"
 echo "Test scripts:"
-find ./scio-repl/src/it/resources -type f
+find ./scio-repl/src/test/resources -type f
 
 sbt -v "++$SCALA_VERSION scio-repl/assembly"
 
 REPL_JAR="./scio-repl/target/scala-${SCALA_VERSION%.*}/scio-repl.jar"
 
-find ./scio-repl/src/it/resources -type f -exec sh -c "cat {} | java -jar $REPL_JAR" \; | tee repl-$SCALA_VERSION.log
+find ./scio-repl/src/test/resources -type f -exec sh -c "cat {} | java -jar $REPL_JAR" \; | tee repl-$SCALA_VERSION.log
 grep 'SUCCESS: \[scio\]' repl-$SCALA_VERSION.log
