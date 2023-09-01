7290bee5ca8e8c1aa21e529dbaac6931c6f0e50a
Patrick McGee <pmcgee@spotify.com>
2023-09-01 16:19:33 -0400

    copyrights

--- scio-extra/src/it/scala/com/spotify/scio/extra/voyager/VoyagerIT.scala
+++ scio-extra/src/it/scala/com/spotify/scio/extra/voyager/VoyagerIT.scala
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2023 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.spotify.scio.extra.voyager
 
 import com.spotify.scio.testing.PipelineSpec
 import com.spotify.scio.testing.util.ItUtils
-import com.spotify.scio.values.{SCollection, SideInput}
+import com.spotify.scio.values.SideInput
 import org.apache.beam.sdk.io.FileSystems
 
 import scala.jdk.CollectionConverters._
+
 class VoyagerIT extends PipelineSpec {
   val dim: Int = 2
   val storageType: VoyagerStorageType = E4M3
