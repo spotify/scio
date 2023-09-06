dc8c8c0842eeacd2a82d656ebe9e24b2603675d8
Karim Mansour <karim@mansour.id>
2023-09-06 13:51:24 +0200

    Remove old comment as it's no longer relevant

--- scio-core/src/main/java/com/spotify/scio/transforms/FutureHandlers.java
+++ scio-core/src/main/java/com/spotify/scio/transforms/FutureHandlers.java
@@ -44,7 +44,6 @@ public class FutureHandlers {
     @Override
     default void waitForFutures(Iterable<ListenableFuture<V>> futures)
         throws InterruptedException, ExecutionException {
-      // Futures#allAsList only works if all futures succeed
       Futures.successfulAsList(futures).get();
     }
 
