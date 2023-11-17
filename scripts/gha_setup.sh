5778aa233a9f749ec3fd02fd120719831ff8c6ab
Michel Davit <micheld@spotify.com>
2023-11-17 13:52:22 +0100

    Rely on env vars exposter by google-github-action/auth (#5079)

--- scripts/gha_setup.sh
+++ scripts/gha_setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Configure SBT options
-[[ -z "${BEAM_RUNNERS}" ]]                   || echo "-DbeamRunners=$BEAM_RUNNERS" >> .sbtopts
-[[ -z "${GOOGLE_PROJECT_ID}" ]]              || echo "-Dbigquery.project=$GOOGLE_PROJECT_ID" >> .sbtopts
-[[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]] || echo "-Dbigquery.secret=$BEAM_RUNNERS" >> .sbtopts
-[[ -z "${CLOUDSQL_SQLSERVER_PASSWORD}" ]]    || echo "-Dcloudsql.sqlserver.password=$BEAM_RUNNERS" >> .sbtopts
+[[ -z "${BEAM_RUNNERS}" ]]                   || echo "-DbeamRunners=$BEAM_RUNNERS"                                >> .sbtopts
+[[ -z "${GOOGLE_CLOUD_PROJECT}" ]]           || echo "-Dbigquery.project=$GOOGLE_CLOUD_PROJECT"                   >> .sbtopts
+[[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]] || echo "-Dbigquery.secret=$GOOGLE_APPLICATION_CREDENTIALS"          >> .sbtopts
+[[ -z "${CLOUDSQL_SQLSERVER_PASSWORD}" ]]    || echo "-Dcloudsql.sqlserver.password=$CLOUDSQL_SQLSERVER_PASSWORD" >> .sbtopts
