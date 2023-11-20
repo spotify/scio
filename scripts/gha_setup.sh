92b963a9b353aaafffb82b6ffd849b263bef7469
Michel Davit <micheld@spotify.com>
2023-11-20 11:20:30 +0100

    Increase BQ read timeout for integration test (#5087)
    Some tntegration test fail compilation due to BQ macro reaching timeout
    when generating case classes.
    Increase timeout to pass the step on the CI

--- scripts/gha_setup.sh
+++ scripts/gha_setup.sh
@@ -6,4 +6,6 @@ set -e
 [[ -z "${BEAM_RUNNERS}" ]]                   || echo "-DbeamRunners=$BEAM_RUNNERS"                                >> .sbtopts
 [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]           || echo "-Dbigquery.project=$GOOGLE_CLOUD_PROJECT"                   >> .sbtopts
 [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]] || echo "-Dbigquery.secret=$GOOGLE_APPLICATION_CREDENTIALS"          >> .sbtopts
+[[ -z "${BQ_CONNECT_TIMEOUT}" ]]             || echo "-Dbigquery.connect_timeout=$BQ_CONNECT_TIMEOUT"             >> .sbtopts
+[[ -z "${BQ_READ_TIMEOUT}" ]]                || echo "-Dbigquery.read_timeout=$BQ_READ_TIMEOUT"                   >> .sbtopts
 [[ -z "${CLOUDSQL_SQLSERVER_PASSWORD}" ]]    || echo "-Dcloudsql.sqlserver.password=$CLOUDSQL_SQLSERVER_PASSWORD" >> .sbtopts
