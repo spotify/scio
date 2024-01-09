164f96317bf8c8372df19afe7f8271fd9cf52f2e
Michel Davit <micheld@spotify.com>
2024-01-09 15:46:09 +0100

    Relax versioning regex (#5139)

--- scio-core/src/main/scala/com/spotify/scio/VersionUtil.scala
+++ scio-core/src/main/scala/com/spotify/scio/VersionUtil.scala
@@ -31,9 +31,12 @@ import scala.collection.mutable
 import scala.util.Try
 
 private[scio] object VersionUtil {
+
+  private val oderSemVer: Ordering[SemVer] =
+    Ordering.by(v => (v.major, v.minor, v.rev, v.suffix.toUpperCase()))
+
   case class SemVer(major: Int, minor: Int, rev: Int, suffix: String) extends Ordered[SemVer] {
-    def compare(that: SemVer): Int =
-      Ordering[(Int, Int, Int, String)].compare(SemVer.unapply(this).get, SemVer.unapply(that).get)
+    def compare(that: SemVer): Int = oderSemVer.compare(this, that)
   }
 
   private[this] val Timeout = 3000
@@ -41,9 +44,9 @@ private[scio] object VersionUtil {
 
   /**
    * example versions: version = "0.10.0-beta1+42-828dca9a-SNAPSHOT" version = "0.10.0-beta1"
-   * version = "0.10.0-SNAPSHOT"
+   * version = "0.10.0-SNAPSHOT" version = "0.10-e135ed2-SNAPSHOT"
    */
-  private[this] val Pattern = """v?(\d+)\.(\d+).(\d+)((-\w+)?(\+\d+-\w+(\+\d+-\d+)?(-\w+)?)?)?""".r
+  private[this] val Pattern = """^(0|[1-9]\d*)\.(0|[1-9]\d*)(?:\.(0|[1-9]\d*))?(?:-(.+))?$""".r
   private[this] val Logger = LoggerFactory.getLogger(this.getClass)
 
   private[this] val MessagePattern: (String, String) => String = (version, url) => s"""
@@ -69,16 +72,20 @@ private[scio] object VersionUtil {
       .execute()
       .parseAs(classOf[java.util.List[java.util.Map[String, AnyRef]]])
     response.asScala
-      .filter(node => !node.get("prerelease").asInstanceOf[Boolean])
-      .find(node => !node.get("draft").asInstanceOf[Boolean])
-      .map(latestNode => latestNode.get("tag_name").asInstanceOf[String])
+      .filterNot(_.get("prerelease").asInstanceOf[Boolean])
+      .filterNot(_.get("draft").asInstanceOf[Boolean])
+      .map(_.get("tag_name").asInstanceOf[String])
+      .collectFirst { case tag if tag.head == 'v' => tag.tail }
   }.toOption.flatten
 
   private def parseVersion(version: String): SemVer = {
     val m = Pattern.findFirstMatchIn(version).get
-    // higher value for no "-SNAPSHOT"
-    val snapshot = if (!m.group(4).isEmpty()) m.group(4).toUpperCase else "\uffff"
-    SemVer(m.group(1).toInt, m.group(2).toInt, m.group(3).toInt, snapshot)
+    // use max value for pre-release if not defined
+    val preRelease = Option(m.group(4)).getOrElse(new String(Array(Char.MaxValue)))
+    val major = m.group(1).toInt
+    val minor = m.group(2).toInt
+    val tiny = Option(m.group(3)).map(_.toInt).getOrElse(0)
+    SemVer(major, minor, tiny, preRelease)
   }
 
   private[scio] def ignoreVersionCheck: Boolean =
@@ -118,7 +125,7 @@ private[scio] object VersionUtil {
     } else {
       val buffer = mutable.Buffer.empty[String]
       val v1 = parseVersion(current)
-      if (v1.suffix.contains("-SNAPSHOT")) {
+      if (v1.suffix.contains("SNAPSHOT")) {
         buffer.append(s"Using a SNAPSHOT version of Scio: $current")
       }
       latestOverride.orElse(latest).foreach { v =>
