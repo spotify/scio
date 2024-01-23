e638e362e756028e9a99ec6a83374891376627d4
Michel Davit <micheld@spotify.com>
2024-01-23 16:33:01 +0100

    Add kryo serializer got GAX api exceptions

--- scio-core/src/main/scala/com/spotify/scio/coders/instances/kryo/GrpcSerializer.scala
+++ scio-core/src/main/scala/com/spotify/scio/coders/instances/kryo/GrpcSerializer.scala
@@ -19,6 +19,9 @@ package com.spotify.scio.coders.instances.kryo
 
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{Input, Output}
+import com.google.api.gax.grpc.GrpcStatusCode
+import com.google.api.gax.httpjson.HttpJsonStatusCode
+import com.google.api.gax.rpc.{ApiException, ApiExceptionFactory}
 import com.twitter.chill.KSerializer
 import io.grpc.{Metadata, Status, StatusRuntimeException}
 
@@ -42,7 +45,7 @@ private[coders] class StatusSerializer extends KSerializer[Status] {
 }
 
 private[coders] class StatusRuntimeExceptionSerializer extends KSerializer[StatusRuntimeException] {
-  lazy val statusSer = new StatusSerializer()
+  private lazy val statusSer = new StatusSerializer()
 
   override def write(kryo: Kryo, output: Output, e: StatusRuntimeException): Unit = {
     kryo.writeObject(output, e.getStatus, statusSer)
@@ -58,5 +61,53 @@ private[coders] class StatusRuntimeExceptionSerializer extends KSerializer[Statu
     val trailers = kryo.readObjectOrNull(input, classOf[Metadata])
 
     new StatusRuntimeException(status, trailers)
+  }
+}
+
+private[coders] class GaxApiExceptionSerializer extends KSerializer[ApiException] {
+  private lazy val statusSer = new StatusSerializer()
+  override def write(kryo: Kryo, output: Output, e: ApiException): Unit = {
+    kryo.writeObject(output, e.getMessage)
+    kryo.writeClassAndObject(output, e.getCause)
+    e.getStatusCode match {
+      case grpc: GrpcStatusCode =>
+        kryo.writeClass(output, classOf[GrpcStatusCode])
+        kryo.writeObject(output, grpc.getTransportCode.toStatus, statusSer)
+      case http: HttpJsonStatusCode =>
+        kryo.writeClass(output, classOf[HttpJsonStatusCode])
+        kryo.writeObject(output, http.getTransportCode)
+      case statusCode =>
+        kryo.writeClass(output, statusCode.getClass)
+    }
+    kryo.writeObject(output, e.isRetryable)
+    // kryo.writeObjectOrNull(output, e.getErrorDetails, classOf[ErrorDetails])
+  }
+
+  override def read(
+    kryo: Kryo,
+    input: Input,
+    `type`: Class[ApiException]
+  ): ApiException = {
+    val message = kryo.readObject(input, classOf[String])
+    val cause = kryo.readClassAndObject(input)
+    val codeClass = kryo.readClass(input).getType
+    val code = if (codeClass == classOf[GrpcStatusCode]) {
+      val status = kryo.readObject(input, classOf[Status], statusSer)
+      GrpcStatusCode.of(status.getCode)
+    } else if (codeClass == classOf[HttpJsonStatusCode]) {
+      val status = kryo.readObject(input, classOf[Integer])
+      HttpJsonStatusCode.of(status)
+    } else {
+      null
+    }
+    val retryable = kryo.readObjectOrNull(input, classOf[Boolean])
+    // val errorDetails = kryo.readObjectOrNull(input, classOf[ErrorDetails])
+
+    ApiExceptionFactory.createException(
+      message,
+      cause.asInstanceOf[Throwable],
+      code,
+      retryable
+    )
   }
 }
--- scio-google-cloud-platform/src/main/scala/com/spotify/scio/coders/instances/kryo/GcpSerializer.scala
+++ scio-google-cloud-platform/src/main/scala/com/spotify/scio/coders/instances/kryo/GcpSerializer.scala
@@ -17,6 +17,8 @@
 package com.spotify.scio.coders.instances.kryo
 
 import com.esotericsoftware.kryo.serializers.DefaultSerializers.StringSerializer
+import com.google.api.gax.rpc.ApiException
+import com.google.cloud.bigtable.data.v2.models.MutateRowsException
 import com.google.cloud.bigtable.grpc.scanner.BigtableRetriesExhaustedException
 import com.twitter.chill._
 
@@ -39,5 +41,36 @@ private[coders] class BigtableRetriesExhaustedExceptionSerializer
     val message = kryo.readObject(input, classOf[String], stringSerializer)
     val cause = kryo.readObject(input, classOf[Throwable], statusExceptionSerializer)
     new BigtableRetriesExhaustedException(message, cause)
+  }
+}
+
+private[coders] class MutateRowsExceptionSerializer extends KSerializer[MutateRowsException] {
+  private lazy val apiExceptionSer = new GaxApiExceptionSerializer()
+  override def write(kryo: Kryo, output: Output, e: MutateRowsException): Unit = {
+    kryo.writeClassAndObject(output, e.getCause)
+    val failedMutations = e.getFailedMutations
+    kryo.writeObject(output, failedMutations.size())
+    failedMutations.forEach { fm =>
+      kryo.writeObject(output, fm.getIndex)
+      kryo.writeObject(output, fm.getError, apiExceptionSer)
+    }
+    kryo.writeObject(output, e.isRetryable)
+  }
+
+  override def read(
+    kryo: Kryo,
+    input: Input,
+    `type`: Class[MutateRowsException]
+  ): MutateRowsException = {
+    val cause = kryo.readClassAndObject(input).asInstanceOf[Throwable]
+    val size = kryo.readObject(input, classOf[Integer])
+    val failedMutations = new _root_.java.util.ArrayList[MutateRowsException.FailedMutation](size)
+    (0 until size).foreach { _ =>
+      val index = kryo.readObject(input, classOf[Integer])
+      val error = kryo.readObject(input, classOf[ApiException], apiExceptionSer)
+      failedMutations.add(MutateRowsException.FailedMutation.create(index, error))
+    }
+    val retryable = kryo.readObject(input, classOf[Boolean])
+    new MutateRowsException(cause, failedMutations, retryable)
   }
 }
