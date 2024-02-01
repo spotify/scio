c35e66786e62fcbfe09ecd636e4f5022eeaf5f12
Claire McGinty <clairem@spotify.com>
2024-02-01 04:09:26 -0500

    Add counter for SMB Predicate filtering (#5221)

--- scio-smb/src/main/java/org/apache/beam/sdk/extensions/smb/SortedBucketTransform.java
+++ scio-smb/src/main/java/org/apache/beam/sdk/extensions/smb/SortedBucketTransform.java
@@ -46,7 +46,6 @@ import org.apache.beam.sdk.io.BoundedSource;
 import org.apache.beam.sdk.io.Read;
 import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.metrics.Counter;
-import org.apache.beam.sdk.metrics.Distribution;
 import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.schemas.transforms.Group;
@@ -124,8 +123,6 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
             filenamePolicy.forDestination(),
             sourceSpec.hashType);
 
-    final Distribution keyGroupSize = Metrics.distribution(getName(), getName() + "-KeyGroupSize");
-    final Counter recordsWritten = Metrics.counter(getName(), getName() + "-RecordsWritten");
     if (transformFn != null) {
       this.doFn =
           ParDo.of(
@@ -136,8 +133,7 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
                   fileAssignment,
                   fileOperations,
                   transformFn,
-                  keyGroupSize,
-                  recordsWritten));
+                  getName()));
     } else {
       this.doFn =
           ParDo.of(
@@ -148,8 +144,7 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
                       fileAssignment,
                       fileOperations,
                       sideInputTransformFn,
-                      keyGroupSize,
-                      recordsWritten))
+                      getName()))
               .withSideInputs(sides);
     }
   }
@@ -414,8 +409,7 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
     protected final SMBFilenamePolicy.FileAssignment fileAssignment;
     protected final FileOperations<FinalValueT> fileOperations;
     protected final List<SortedBucketSource.BucketedInput<?>> sources;
-    protected final Distribution keyGroupSize;
-    protected final Counter recordsWritten;
+    protected final String metricsPrefix;
     private final Function<SortedBucketIO.ComparableKeyBytes, FinalKeyT> keyFn;
     private final Comparator<SortedBucketIO.ComparableKeyBytes> keyComparator;
 
@@ -425,15 +419,13 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
         Comparator<SortedBucketIO.ComparableKeyBytes> keyComparator,
         SMBFilenamePolicy.FileAssignment fileAssignment,
         FileOperations<FinalValueT> fileOperations,
-        Distribution keyGroupSize,
-        Counter recordsWritten) {
+        String metricsPrefix) {
       this.fileAssignment = fileAssignment;
       this.fileOperations = fileOperations;
       this.sources = sources;
-      this.keyGroupSize = keyGroupSize;
+      this.metricsPrefix = metricsPrefix;
       this.keyFn = keyFn;
       this.keyComparator = keyComparator;
-      this.recordsWritten = recordsWritten;
     }
 
     protected abstract void outputTransform(
@@ -464,6 +456,8 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
       ResourceId dst =
           fileAssignment.forBucket(BucketShardId.of(bucketId, 0), effectiveParallelism, 1);
       OutputCollector<FinalValueT> outputCollector;
+      final Counter recordsWritten =
+          Metrics.counter(metricsPrefix, metricsPrefix + "-RecordsWritten");
       try {
         outputCollector = new OutputCollector<>(fileOperations.createWriter(dst), recordsWritten);
       } catch (IOException err) {
@@ -480,7 +474,7 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
               coGbkResultSchema(),
               someArbitraryBucketMetadata,
               keyComparator,
-              keyGroupSize,
+              metricsPrefix,
               false,
               bucketId,
               effectiveParallelism,
@@ -523,16 +517,8 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
         SMBFilenamePolicy.FileAssignment fileAssignment,
         FileOperations<FinalValueT> fileOperations,
         TransformFn<FinalKeyT, FinalValueT> transformFn,
-        Distribution keyGroupSize,
-        Counter recordsWritten) {
-      super(
-          sources,
-          keyFn,
-          keyComparator,
-          fileAssignment,
-          fileOperations,
-          keyGroupSize,
-          recordsWritten);
+        String metricsPrefix) {
+      super(sources, keyFn, keyComparator, fileAssignment, fileOperations, metricsPrefix);
       this.transformFn = transformFn;
     }
 
@@ -557,16 +543,8 @@ public class SortedBucketTransform<FinalKeyT, FinalValueT> extends PTransform<PB
         SMBFilenamePolicy.FileAssignment fileAssignment,
         FileOperations<FinalValueT> fileOperations,
         TransformFnWithSideInputContext<FinalKeyT, FinalValueT> transformFn,
-        Distribution keyGroupSize,
-        Counter recordsWritten) {
-      super(
-          sources,
-          keyFn,
-          keyComparator,
-          fileAssignment,
-          fileOperations,
-          keyGroupSize,
-          recordsWritten);
+        String metricsPrefix) {
+      super(sources, keyFn, keyComparator, fileAssignment, fileOperations, metricsPrefix);
       this.transformFn = transformFn;
     }
 
