cfb9bbcf630165f8f9c0d8754730f954c06a31ac
Claire McGinty <clairem@spotify.com>
2024-03-12 11:16:15 -0400

    Allow String key type to transform SMB sources with CharSequence key

--- scio-smb/src/test/scala/com/spotify/scio/smb/SmbVersionParityTest.scala
+++ scio-smb/src/test/scala/com/spotify/scio/smb/SmbVersionParityTest.scala
@@ -30,7 +30,7 @@ import org.apache.beam.sdk.values.TupleTag
 import java.nio.file.Files
 
 class SmbVersionParityTest extends PipelineSpec {
-  private def testRoundtrip(
+  private def testReadRoundtrip(
     write: SortedBucketIO.Write[CharSequence, _, Account],
     read: SortedBucketIO.Read[Account]
   ): Unit = {
@@ -64,11 +64,49 @@ class SmbVersionParityTest extends PipelineSpec {
       .toSeq should contain theSameElementsAs accounts
   }
 
+  private def testTransformRoundtrip(
+    write: SortedBucketIO.Write[CharSequence, Void, Account],
+    read: SortedBucketIO.Read[Account],
+    transform: SortedBucketIO.TransformOutput[String, Void, Account]
+  ): Unit = {
+    val accounts = (1 to 10).map { i =>
+      Account
+        .newBuilder()
+        .setId(i)
+        .setName(i.toString)
+        .setAmount(i.toDouble)
+        .setType(s"type$i")
+        .setAccountStatus(AccountStatus.Active)
+        .build()
+    }
+
+    {
+      val sc = ScioContext()
+      sc.parallelize(accounts)
+        .saveAsSortedBucket(write)
+      sc.run()
+    }
+
+    // Read data
+    val sc = ScioContext()
+    val tap = sc
+      .sortMergeTransform(classOf[String], read)
+      .to(transform)
+      .via { case (_, records, outputCollector) =>
+        records.foreach(outputCollector.accept(_))
+      }
+
+    tap
+      .get(sc.run().waitUntilDone())
+      .value
+      .toSeq should contain theSameElementsAs accounts
+  }
+
   "SortedBucketSource" should "be able to read CharSequence-keyed Avro sources written before 0.14" in {
     val output = Files.createTempDirectory("smb-version-test-avro").toFile
     output.deleteOnExit()
 
-    testRoundtrip(
+    testReadRoundtrip(
       AvroSortedBucketIO
         .write(classOf[CharSequence], "name", classOf[Account])
         .to(output.getAbsolutePath)
@@ -80,11 +118,33 @@ class SmbVersionParityTest extends PipelineSpec {
     )
   }
 
+  it should "be able to transform CharSequence-keyed Avro sources written before 0.14" in {
+    val tmpDir = Files.createTempDirectory("smb-version-test-avro-tfx").toFile
+    tmpDir.deleteOnExit()
+
+    val writeOutput = tmpDir.toPath.resolve("write")
+    val tfxOutput = tmpDir.toPath.resolve("tfx")
+
+    testTransformRoundtrip(
+      AvroSortedBucketIO
+        .write(classOf[CharSequence], "name", classOf[Account])
+        .to(writeOutput.toString)
+        .withNumBuckets(1)
+        .withNumShards(1),
+      AvroSortedBucketIO
+        .read(new TupleTag[Account], classOf[Account])
+        .from(writeOutput.toString),
+      AvroSortedBucketIO
+        .transformOutput(classOf[String], "name", classOf[Account])
+        .to(tfxOutput.toString)
+    )
+  }
+
   it should "be able to read CharSequence-keyed Parquet sources written before 0.14" in {
     val output = Files.createTempDirectory("smb-version-test-parquet").toFile
     output.deleteOnExit()
 
-    testRoundtrip(
+    testReadRoundtrip(
       ParquetAvroSortedBucketIO
         .write(classOf[CharSequence], "name", classOf[Account])
         .to(output.getAbsolutePath)
@@ -93,6 +153,28 @@ class SmbVersionParityTest extends PipelineSpec {
       ParquetAvroSortedBucketIO
         .read(new TupleTag[Account], classOf[Account])
         .from(output.getAbsolutePath)
+    )
+  }
+
+  it should "be able to transform CharSequence-keyed Parquet sources written before 0.14" in {
+    val tmpDir = Files.createTempDirectory("smb-version-test-parquet-tfx").toFile
+    tmpDir.deleteOnExit()
+
+    val writeOutput = tmpDir.toPath.resolve("write")
+    val tfxOutput = tmpDir.toPath.resolve("tfx")
+
+    testTransformRoundtrip(
+      ParquetAvroSortedBucketIO
+        .write(classOf[CharSequence], "name", classOf[Account])
+        .to(writeOutput.toString)
+        .withNumBuckets(1)
+        .withNumShards(1),
+      ParquetAvroSortedBucketIO
+        .read(new TupleTag[Account], classOf[Account])
+        .from(writeOutput.toString),
+      ParquetAvroSortedBucketIO
+        .transformOutput(classOf[String], "name", classOf[Account])
+        .to(tfxOutput.toString)
     )
   }
 }
