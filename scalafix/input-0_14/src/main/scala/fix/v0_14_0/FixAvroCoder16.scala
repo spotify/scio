66f08648123a0c26d4b165630eb8d0520a278129
Claire McGinty <clairem@spotify.com>
2024-05-02 10:22:08 +0100

    Update FixAvroCoder to match transformed Avro SCollections

---
+++ scalafix/input-0_14/src/main/scala/fix/v0_14_0/FixAvroCoder16.scala
@@ -0,0 +1,13 @@
+/*
+rule = FixAvroCoder
+ */
+package fix.v0_14_0
+
+import com.spotify.scio.values.SCollection
+
+object FixAvroCoder16 {
+
+  def someMethod(data: SCollection[A]): SCollection[A] = {
+    data.map(r => ("foo", r))
+  }
+}
