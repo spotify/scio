82f80dc62d18922cd2594b20a3cdc5d816f0de60
Claire McGinty <clairem@spotify.com>
2024-05-02 11:27:44 +0100

    fixup

--- scalafix/input-0_14/src/main/scala/fix/v0_14_0/FixAvroCoder16.scala
+++ scalafix/input-0_14/src/main/scala/fix/v0_14_0/FixAvroCoder16.scala
@@ -7,7 +7,7 @@ import com.spotify.scio.values.SCollection
 
 object FixAvroCoder16 {
 
-  def someMethod(data: SCollection[A]): SCollection[A] = {
+  def someMethod(data: SCollection[A]): SCollection[(String, A)] = {
     data.map(r => ("foo", r))
   }
 }
