93bfb7a7ce0335e2fa624b17aee2d68a4913c3af
Kellen Dye <kellend@spotify.com>
2025-11-25 00:08:16 -0500

    cleanup

--- scio-parquet/src/main/scala/com/spotify/scio/parquet/ParquetMetadataDoFn.scala
+++ scio-parquet/src/main/scala/com/spotify/scio/parquet/ParquetMetadataDoFn.scala
@@ -17,15 +17,10 @@
 
 package com.spotify.scio.parquet
 
-import org.apache.beam.sdk.io.FileSystems
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.DoFn._
-import org.apache.parquet.format.converter.ParquetMetadataConverter
 import org.apache.parquet.hadoop.ParquetFileReader
 
-import java.io.ByteArrayInputStream
-import java.nio.{ByteBuffer, ByteOrder}
-import java.nio.channels.{ReadableByteChannel, SeekableByteChannel}
 import scala.jdk.CollectionConverters._
 import scala.util.Try
 
@@ -48,7 +43,6 @@ object ParquetMetadataDoFn {
 /** Reads a parquet file and emits its metadata */
 class ParquetMetadataDoFn extends DoFn[String, (String, Try[ParquetMetadataDoFn.ParquetMetadata])] {
   import ParquetMetadataDoFn._
-  private val MagicBytes = "PAR1".getBytes
 
   @ProcessElement
   def processElement(
@@ -62,42 +56,22 @@ class ParquetMetadataDoFn extends DoFn[String, (String, Try[ParquetMetadataDoFn.
       reader = ParquetFileReader.open(BeamInputFile.of(filePath))
       val md = reader.getFooter
       ParquetMetadata(
-        schema = md.getFileMetaData.getSchema.toString,
-        blocks = md.getBlocks.asScala.map { b =>
+        md.getFileMetaData.getSchema.toString,
+        md.getBlocks.asScala.map { b =>
           BlockMetadata(
-            rowCount = b.getRowCount,
-            totalByteSize = b.getTotalByteSize,
-            numColumns = b.getColumns.size()
+            b.getRowCount,
+            b.getTotalByteSize,
+            b.getColumns.size()
           )
         }.toSeq,
-        createdBy = md.getFileMetaData.getCreatedBy,
-        numRows = md.getBlocks.asScala.map(_.getRowCount).sum,
-        keyValueMetaData = Option(md.getFileMetaData.getKeyValueMetaData)
+        md.getFileMetaData.getCreatedBy,
+        md.getBlocks.asScala.map(_.getRowCount).sum,
+        Option(md.getFileMetaData.getKeyValueMetaData)
           .map(_.asScala.toMap)
           .getOrElse(Map.empty[String, String])
       )
     } finally {
       if (reader != null) reader.close()
     }
-  }
-
-  private def readBytesAt(
-    channel: SeekableByteChannel,
-    position: Long,
-    length: Int
-  ): Array[Byte] = {
-    channel.position(position)
-    val buffer = ByteBuffer.allocate(length)
-    var bytesRead = 0
-    while (bytesRead < length) {
-      val read = channel.read(buffer)
-      if (read == -1) {
-        throw new IllegalStateException(
-          s"Unexpected EOF at position $position, expected $length bytes but got $bytesRead"
-        )
-      }
-      bytesRead += read
-    }
-    buffer.array()
   }
 }
