d3e13ca8229523b12a289c2cab3a263d113a981d
Kellen Dye <kellend@spotify.com>
2026-06-25 12:04:57 +0200

    wip

--- scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
+++ scio-core/src/main/scala/com/spotify/scio/ScioContext.scala
@@ -461,6 +461,7 @@ class ScioContext private[scio] (
     import com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystemConfiguration._
     import com.google.cloud.hadoop.fs.gcs.HadoopConfigurationProperty
     import com.google.cloud.hadoop.gcsio.GoogleCloudStorageReadOptions
+    import com.google.cloud.hadoop.gcsio.GoogleCloudStorageReadOptions.{Builder, Fadvise}
 
     try {
       // If Hadoop is on the classpath, try to parse default gcs-connector options
@@ -469,53 +470,65 @@ class ScioContext private[scio] (
       val o = optionsAs[GcsOptions]
 
       type Getter[T] = java.util.function.BiFunction[String, T, T]
-      val fadvise: Getter[GoogleCloudStorageReadOptions.Fadvise] = (k, d) => config.getEnum(k, d)
+      val fadvise: Getter[Fadvise] = (k, d) => config.getEnum(k, d)
       val bool: Getter[java.lang.Boolean] = (k, d) => config.getBoolean(k, d)
       val longBytes: Getter[java.lang.Long] = (k, d) => config.getLongBytes(k, d)
       val long: Getter[java.lang.Long] = (k, d) => config.getLong(k, d)
       val int: Getter[java.lang.Integer] = (k, d) => config.getInt(k, d)
 
-      type Builder = GoogleCloudStorageReadOptions.Builder
       implicit class PropOps[T](val prop: HadoopConfigurationProperty[T]) {
-        def ifSet(getter: Getter[T])(setter: Builder => T => Builder)(b: Builder): Builder =
-          if (config.get(prop.getKey) != null) setter(b)(prop.get(config, getter)) else b
-        def ifSetDuration(setter: Builder => java.time.Duration => Builder)(b: Builder): Builder =
-          if (config.get(prop.getKey) != null) setter(b)(prop.getTimeDuration(config)) else b
+        def ifSet(getter: Getter[T], setter: (Builder, T) => Builder)(b: Builder): Builder =
+          if (config.get(prop.getKey) != null) setter(b, prop.get(config, getter)) else b
+        def ifSetDuration(setter: (Builder, java.time.Duration) => Builder)(b: Builder): Builder =
+          if (config.get(prop.getKey) != null) setter(b, prop.getTimeDuration(config)) else b
       }
 
       o.setGoogleCloudStorageReadOptions(
         GoogleCloudStorageReadOptions
           .builder()
-          .pipe(GCS_INPUT_STREAM_FADVISE.ifSet(fadvise)(_.setFadvise))
           .pipe(
-            GCS_INPUT_STREAM_FAST_FAIL_ON_NOT_FOUND_ENABLE.ifSet(bool)(b =>
-              v => b.setFastFailOnNotFoundEnabled(v)
-            )
+            GCS_INPUT_STREAM_FADVISE
+              .ifSet(fadvise, (b, v) => b.setFadvise(v))
           )
           .pipe(
-            GCS_INPUT_STREAM_SUPPORT_GZIP_ENCODING_ENABLE.ifSet(bool)(b =>
-              v => b.setGzipEncodingSupportEnabled(v)
-            )
+            GCS_INPUT_STREAM_FAST_FAIL_ON_NOT_FOUND_ENABLE
+              .ifSet(bool, (b, v) => b.setFastFailOnNotFoundEnabled(v))
           )
           .pipe(
-            GCS_INPUT_STREAM_INPLACE_SEEK_LIMIT.ifSet(longBytes)(b => v => b.setInplaceSeekLimit(v))
+            GCS_INPUT_STREAM_SUPPORT_GZIP_ENCODING_ENABLE
+              .ifSet(bool, (b, v) => b.setGzipEncodingSupportEnabled(v))
           )
           .pipe(
-            GCS_INPUT_STREAM_MIN_RANGE_REQUEST_SIZE.ifSet(longBytes)(b =>
-              v => b.setMinRangeRequestSize(v)
-            )
-          )
-          .pipe(GCS_GRPC_CHECKSUMS_ENABLE.ifSet(bool)(b => v => b.setGrpcChecksumsEnabled(v)))
-          .pipe(GCS_GRPC_READ_TIMEOUT.ifSetDuration(b => v => b.setGrpcReadTimeout(v)))
-          .pipe(
-            GCS_GRPC_READ_MESSAGE_TIMEOUT.ifSetDuration(b => v => b.setGrpcReadMessageTimeout(v))
+            GCS_INPUT_STREAM_INPLACE_SEEK_LIMIT
+              .ifSet(longBytes, (b, v) => b.setInplaceSeekLimit(v))
           )
           .pipe(
-            GCS_GRPC_READ_ZEROCOPY_ENABLE.ifSet(bool)(b => v => b.setGrpcReadZeroCopyEnabled(v))
+            GCS_INPUT_STREAM_MIN_RANGE_REQUEST_SIZE
+              .ifSet(longBytes, (b, v) => b.setMinRangeRequestSize(v))
           )
-          .pipe(BLOCK_SIZE.ifSet(long)(b => v => b.setBlockSize(v)))
           .pipe(
-            GCS_FADVISE_REQUEST_TRACK_COUNT.ifSet(int)(b => v => b.setFadviseRequestTrackCount(v))
+            GCS_GRPC_CHECKSUMS_ENABLE
+              .ifSet(bool, (b, v) => b.setGrpcChecksumsEnabled(v))
+          )
+          .pipe(
+            GCS_GRPC_READ_TIMEOUT
+              .ifSetDuration((b, v) => b.setGrpcReadTimeout(v))
+          )
+          .pipe(
+            GCS_GRPC_READ_MESSAGE_TIMEOUT
+              .ifSetDuration((b, v) => b.setGrpcReadMessageTimeout(v))
+          )
+          .pipe(
+            GCS_GRPC_READ_ZEROCOPY_ENABLE
+              .ifSet(bool, (b, v) => b.setGrpcReadZeroCopyEnabled(v))
+          )
+          .pipe(
+            BLOCK_SIZE
+              .ifSet(long, (b, v) => b.setBlockSize(v))
+          )
+          .pipe(
+            GCS_FADVISE_REQUEST_TRACK_COUNT
+              .ifSet(int, (b, v) => b.setFadviseRequestTrackCount(v))
           )
           .build()
       )
