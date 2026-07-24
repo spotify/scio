ca2dd9e1282c021413b230acf6d861d75fd8ed8c
Claire McGinty <clairem@spotify.com>
2026-07-24 13:59:40 -0400

    Integration test

--- integration/src/test/scala/com/spotify/scio/iceberg/IcebergIOIT.scala
+++ integration/src/test/scala/com/spotify/scio/iceberg/IcebergIOIT.scala
@@ -17,6 +17,7 @@
 package com.spotify.scio.iceberg
 
 import com.dimafeng.testcontainers.{ForAllTestContainer, GenericContainer}
+import com.spotify.scio.parquet.BeamInputFile
 import com.spotify.scio.testing.PipelineSpec
 import magnolify.beam._
 import org.apache.iceberg.catalog.{Namespace, TableIdentifier}
@@ -28,7 +29,15 @@ import org.apache.iceberg.types.Types.{
   StringType,
   StructType
 }
-import org.apache.iceberg.{CatalogProperties, CatalogUtil, PartitionSpec, Schema}
+import org.apache.iceberg.{
+  CatalogProperties,
+  CatalogUtil,
+  NullOrder,
+  PartitionSpec,
+  Schema,
+  SortDirection
+}
+import org.apache.parquet.hadoop.ParquetFileReader
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 
 import java.time.Duration
@@ -65,12 +74,15 @@ class IcebergIOIT extends PipelineSpec with ForAllTestContainer {
 
   lazy val uri = s"http://${container.containerIpAddress}:${container.mappedPort(ContainerPort)}"
 
-  override def afterStart(): Unit = {
+  lazy val catalog: RESTCatalog = {
     val cat = new RESTCatalog()
     cat.initialize(CatalogName, Map("uri" -> uri).asJava)
+    cat
+  }
 
-    cat.createNamespace(Namespace.of(NamespaceName))
-    cat.createTable(
+  override def afterStart(): Unit = {
+    catalog.createNamespace(Namespace.of(NamespaceName))
+    catalog.createTable(
       TableIdentifier.parse(TableName),
       new Schema(
         NestedField.required(0, "a", IntegerType.get()),
@@ -102,6 +114,106 @@ class IcebergIOIT extends PipelineSpec with ForAllTestContainer {
         TableName,
         catalogProperties = catalogProperties
       ) should containInAnyOrder(elements)
+    }
+  }
+
+  it should "write with sort order" in {
+    val sortedTableName = s"${NamespaceName}.sorted_records"
+    val catalogProperties = Map(
+      CatalogUtil.ICEBERG_CATALOG_TYPE -> CatalogUtil.ICEBERG_CATALOG_TYPE_REST,
+      CatalogProperties.URI -> uri
+    )
+    val elements = 1.to(10).map(i => IcebergIOITRecord(i, s"$i", Nested(i % 2 == 0)))
+
+    runWithRealContext() { sc =>
+      sc.parallelize(elements)
+        .saveAsIceberg(
+          sortedTableName,
+          catalogProperties = catalogProperties,
+          sortFields = List("a asc nulls first")
+        )
+    }
+
+    val table = catalog.loadTable(TableIdentifier.parse(sortedTableName))
+    val sortOrder = table.sortOrder()
+    sortOrder.isSorted shouldBe true
+    sortOrder.fields().size() shouldBe 1
+    sortOrder.fields().get(0).direction() shouldBe SortDirection.ASC
+    sortOrder.fields().get(0).nullOrder() shouldBe NullOrder.NULLS_FIRST
+  }
+
+  it should "write with partition spec" in {
+    val partitionedTableName = s"${NamespaceName}.partitioned_records"
+    val catalogProperties = Map(
+      CatalogUtil.ICEBERG_CATALOG_TYPE -> CatalogUtil.ICEBERG_CATALOG_TYPE_REST,
+      CatalogProperties.URI -> uri
+    )
+    val elements = 1.to(10).map(i => IcebergIOITRecord(i, s"$i", Nested(i % 2 == 0)))
+
+    runWithRealContext() { sc =>
+      sc.parallelize(elements)
+        .saveAsIceberg(
+          partitionedTableName,
+          catalogProperties = catalogProperties,
+          partitionFields = List("bucket(b, 2)")
+        )
+    }
+
+    val table = catalog.loadTable(TableIdentifier.parse(partitionedTableName))
+    val spec = table.spec()
+    spec.isPartitioned shouldBe true
+    spec.fields().size() shouldBe 1
+    spec.fields().get(0).name() shouldBe "b_bucket"
+  }
+
+  it should "propagate Iceberg writeProperties" in {
+    val bfTableName = s"${NamespaceName}.bloom_filter_records"
+    val catalogProperties = Map(
+      CatalogUtil.ICEBERG_CATALOG_TYPE -> CatalogUtil.ICEBERG_CATALOG_TYPE_REST,
+      CatalogProperties.URI -> uri
+    )
+    val elements = 1.to(100).map(i => IcebergIOITRecord(i, s"value_$i", Nested(i % 2 == 0)))
+
+    runWithRealContext() { sc =>
+      sc.parallelize(elements)
+        .saveAsIceberg(
+          bfTableName,
+          catalogProperties = catalogProperties,
+          writeProperties = Map("write.parquet.bloom-filter-enabled.column.b" -> "true")
+        )
+    }
+
+    val table = catalog.loadTable(TableIdentifier.parse(bfTableName))
+    val tasks = table.newScan().planFiles()
+    try {
+      val dataFiles = tasks.iterator().asScala.map(_.file().path().toString).toSeq
+      dataFiles should not be empty
+
+      dataFiles.foreach { path =>
+        val reader = ParquetFileReader.open(BeamInputFile.of(path))
+        try {
+          reader.getFooter.getBlocks.asScala.foreach { block =>
+            block.getColumns.asScala.foreach { col =>
+              val hasBloom = col.getBloomFilterOffset > 0
+              col.getPath.toDotString match {
+                case "b" =>
+                  // flip assertion once https://github.com/apache/beam/pull/39250/ is release in Beam 2.76
+                  withClue(
+                    "Iceberg writeProperties are not supported in Beam versions <= 2.76. Once Beam is upgraded, flip this assertion to `true`."
+                  ) {
+                    hasBloom shouldBe false
+                  }
+                case _ =>
+                  hasBloom shouldBe false
+              }
+            }
+          }
+        } finally {
+          reader.close()
+        }
+      }
+    } finally {
+      tasks.close()
     }
   }
 }
